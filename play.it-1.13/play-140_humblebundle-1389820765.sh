a1ba1b51a8707788220b9c6be5b837c4c128c217
vv221 <vv221@dotslashplay.it>
2016-04-09 16:00:20 +0200

    1.13 - 140 (Humble Bundle) minor tweaks to improve readability better dependencies handling

--- play.it-1.13/play-140_humblebundle-1389820765.sh
+++ play.it-1.13/play-140_humblebundle-1389820765.sh
@@ -29,14 +29,12 @@
 
 ###
 # conversion script for the 140 archive sold on Humble Bundle
-# build a .deb package from the .zip archive
-# tested on Debian, should work on any .deb-based distribution
+# build .deb packages from the .zip archive
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
 
-script_version=20160212.1
+script_version=20160331.1
 
 # Set game-specific variables
 
@@ -51,13 +49,13 @@ GAME_ARCHIVE1_MD5='e78c09a2a9f47d89a4bb1e4e97911e79'
 GAME_ARCHIVE_FULLSIZE='92000'
 PKG_REVISION='humble1389820765'
 
-INSTALLER_GAME_ARCH1='./140.x86 140_Data/Mono/x86 140_Data/Plugins/x86'
-INSTALLER_GAME_ARCH2='./140.x86_64 140_Data/Mono/x86_64 140_Data/Plugins/x86_64'
-INSTALLER_GAME='./140_Data'
+INSTALLER_GAME_PKG1='./140.x86 140_Data/Mono/x86 140_Data/Plugins/x86'
+INSTALLER_GAME_PKG2='./140.x86_64 140_Data/Mono/x86_64 140_Data/Plugins/x86_64'
+INSTALLER_GAME_PKG3='./140_Data'
 
 APP1_ID="${GAME_ID}"
-APP1_EXE_ARCH1='./140.x86'
-APP1_EXE_ARCH2='./140.x86_64'
+APP1_EXE_PKG1='./140.x86'
+APP1_EXE_PKG2='./140.x86_64'
 APP1_ICON='./140_Data/Resources/UnityPlayer.png'
 APP1_ICON_RES='128x128'
 APP1_NAME="${GAME_NAME}"
@@ -67,23 +65,22 @@ APP1_CAT='Game'
 PKG_ID="${GAME_ID}"
 PKG_VERSION='1.0'
 PKG_DEPS='libglu1-mesa | libglu1, libasound2-plugins, libxcursor1'
-PKG_RECS=''
 PKG_DESC="${GAME_NAME}
- package built from HumbleBundle.com installer
+ package built from HumbleBundle.com .zip archive
  ./play.it script version ${script_version}"
 
 PKG1_ID="${PKG_ID}"
 PKG1_ARCH='i386'
 PKG1_VERSION="${PKG_VERSION}"
 PKG1_DEPS="${PKG_DEPS}"
-PKG1_RECS="${PKG_RECS}"
+PKG1_RECS=''
 PKG1_DESC="${PKG_DESC}"
 
 PKG2_ID="${PKG_ID}"
 PKG2_ARCH='amd64'
 PKG2_VERSION="${PKG_VERSION}"
 PKG2_DEPS="${PKG_DEPS}"
-PKG2_RECS="${PKG_RECS}"
+PKG2_RECS=''
 PKG2_DESC="${PKG_DESC}"
 
 PKG3_ID="${GAME_ID}-common"
@@ -93,11 +90,14 @@ PKG3_CONFLICTS=''
 PKG3_DEPS=''
 PKG3_RECS=''
 PKG3_DESC="${GAME_NAME} - arch-independant data
- package built from HumbleBundle.com installer
+ package built from HumbleBundle.com .zip archive
  ./play.it script version ${script_version}"
 
 PKG1_CONFLICTS="${PKG2_ID}:${PKG2_ARCH}"
 PKG2_CONFLICTS="${PKG1_ID}:${PKG1_ARCH}"
+
+PKG1_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG1_DEPS}"
+PKG2_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG2_DEPS}"
 
 # Load common functions
 
@@ -145,17 +145,13 @@ game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" 
 game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG3_DIR' "${PKG3_ID}_${PKG3_VERSION}-${PKG_REVISION}_${PKG3_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
-printf '\n'
-
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON="/usr/local/share/icons/hicolor/${APP1_ICON_RES}/apps"
+PATH_ICON_BASE="/usr/local/share/icons/hicolor"
 
 printf '\n'
-
 set_target '1' 'humblebundle.com'
-
 printf '\n'
 
 # Check target files integrity
@@ -166,49 +162,46 @@ fi
 
 # Extract game data
 
-build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DESK}" "${PATH_GAME}" "${PATH_ICON}"
+PATH_ICON="${PATH_ICON_BASE}/${APP1_ICON_RES}/apps"
+build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DESK}" "${PATH_GAME}"
 rm -rf "${PKG3_DIR}"
 mkdir -p "${PKG3_DIR}/DEBIAN" "${PKG3_DIR}${PATH_GAME}" "${PKG3_DIR}${PATH_ICON}"
-
 print wait
 
 extract_data 'zip' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
 
-for file in ${INSTALLER_GAME_ARCH1}; do
+for file in ${INSTALLER_GAME_PKG1}; do
 	mkdir -p "${PKG1_DIR}${PATH_GAME}/${file%/*}"
 	mv "${PKG_TMPDIR}/${file}" "${PKG1_DIR}${PATH_GAME}/${file%/*}"
 done
 
-for file in ${INSTALLER_GAME_ARCH2}; do
+for file in ${INSTALLER_GAME_PKG2}; do
 	mkdir -p "${PKG2_DIR}${PATH_GAME}/${file%/*}"
 	mv "${PKG_TMPDIR}/${file}" "${PKG2_DIR}${PATH_GAME}/${file%/*}"
 done
 
-for file in ${INSTALLER_GAME}; do
+for file in ${INSTALLER_GAME_PKG3}; do
 	mv "${PKG_TMPDIR}"/${file} "${PKG3_DIR}${PATH_GAME}"
 done
 
-chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE_ARCH1}"
-chmod 755 "${PKG2_DIR}${PATH_GAME}/${APP1_EXE_ARCH2}"
+chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE_PKG1}"
+chmod 755 "${PKG2_DIR}${PATH_GAME}/${APP1_EXE_PKG2}"
 
 rm -rf "${PKG_TMPDIR}"
-
 print done
 
 # Write launchers
 
-write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_ARCH1}" '' '' '' "${APP1_NAME} (${PKG1_ARCH})"
-write_bin_native "${PKG2_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_ARCH2}" '' '' '' "${APP1_NAME} (${PKG2_ARCH})"
+write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_PKG1}" '' '' '' "${APP1_NAME} (${PKG1_ARCH})"
+write_bin_native "${PKG2_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_PKG2}" '' '' '' "${APP1_NAME} (${PKG2_ARCH})"
 
 write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" ''
 cp -l "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${PKG2_DIR}${PATH_DESK}/${APP1_ID}.desktop"
-
 printf '\n'
 
 # Build packages
 
 printf '%s…\n' "$(l10n 'build_pkgs')"
-
 print wait
 
 write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}" 'arch'
@@ -216,35 +209,29 @@ write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "$
 write_pkg_debian "${PKG3_DIR}" "${PKG3_ID}" "${PKG3_VERSION}-${PKG_REVISION}" "${PKG3_ARCH}" "${PKG3_CONFLICTS}" "${PKG3_DEPS}" "${PKG3_RECS}" "${PKG3_DESC}" ''
 
 file="${PKG3_DIR}/DEBIAN/postinst"
-
 cat > "${file}" <<- EOF
 #!/bin/sh -e
 ln -s "${PATH_GAME}/${APP1_ICON}" "${PATH_ICON}/${GAME_ID}.png"
 exit 0
 EOF
-
 chmod 755 "${file}"
 
 file="${PKG3_DIR}/DEBIAN/prerm"
-
 cat > "${file}" <<- EOF
 #!/bin/sh -e
 rm "${PATH_ICON}/${GAME_ID}.png"
 exit 0
 EOF
-
 chmod 755 "${file}"
 
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG1_ARCH}"
 build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG2_ARCH}"
 build_pkg "${PKG3_DIR}" "${PKG3_DESC}" "${PKG_COMPRESSION}" 'quiet' ''
-
 print done
 
 print_instructions "$(printf '%s' "${PKG1_DESC}" | head -n1) (${PKG1_ARCH})" "${PKG3_DIR}" "${PKG1_DIR}"
 printf '\n'
 print_instructions "$(printf '%s' "${PKG2_DESC}" | head -n1) (${PKG2_ARCH})" "${PKG3_DIR}" "${PKG2_DIR}"
-
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
 exit 0
