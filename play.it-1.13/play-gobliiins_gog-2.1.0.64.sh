bece2e6fb4efd21e8692a73318acd25f4740324d
vv221 <vv221@dotslashplay.it>
2016-04-09 18:04:15 +0200

    1.13 - Gobliiins (GOG) full rewrite

--- play.it-1.13/play-gobliiins_gog-2.1.0.64.sh
+++ play.it-1.13/play-gobliiins_gog-2.1.0.64.sh
@@ -30,69 +30,86 @@
 ###
 # conversion script for the Gobliiins installer sold on GOG.com
 # build a .deb package from the Windows installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20151127.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160331.1
 
 # Set game-specific variables
 
+SCRIPT_DEPS_HARD='fakeroot innoextract fakeroot'
+SCRIPT_DEPS_SOFT='icotool'
+
 GAME_ID='gobliiins'
-GAME_ID_SHOR='gob1'
-SCUMMVM_ID='gob'
+GAME_ID_PKG1="${GAME_ID}-floppy"
+GAME_ID_PKG2="${GAME_ID}-cd"
+GAME_ID_SHORT='gob1'
 GAME_NAME='Gobliiins'
+GAME_NAME_PKG1="${GAME_NAME} - floppy version"
+GAME_NAME_PKG2="${GAME_NAME} - CD version"
 
 GAME_ARCHIVE1='setup_gobliiins_2.1.0.64.exe'
 GAME_ARCHIVE1_MD5='e587f246c7dedb84b30ee09e6f1c5462'
 GAME_ARCHIVE_FULLSIZE='94000'
-PKG_ORIGIN='gog'
-PKG_REVISION='2.1.0.64'
+PKG_REVISION='gog2.1.0.64'
 
-#APP1_ICON='./goggame-1207662273.ico'
-#APP1_ICON_RES='16x16 32x32 48x48 256x256'
+INSTALLER_DOC='app/*.pdf tmp/gog_eula.txt'
+INSTALLER_GAME_PKG1='app/goggame-1207662273.ico app/fdd/*'
+INSTALLER_GAME_PKG2='app/gob.lic app/intro.stk app/track1.mp3'
+
+APP1_ID_PKG1="${GAME_ID_PKG1}"
+APP1_ID_PKG2="${GAME_ID_PKG2}"
+APP1_SCUMMID='gob'
+APP1_ICON='./goggame-1207662273.ico'
+APP1_ICON_RES='16x16 32x32 48x48 256x256'
+APP1_NAME_PKG1="${GAME_NAME_PKG1}"
+APP1_NAME_FR_PKG1="${GAME_NAME} - version disquette"
+APP1_NAME_PKG2="${GAME_NAME_PKG2}"
+APP1_NAME_FR_PKG2="${GAME_NAME} - version CD"
 APP1_CAT='Game'
 
-PKG1_ID="${GAME_ID}-floppy"
-PKG2_ID="${GAME_ID}-cd"
+PKG_ARCH='all'
+PKG_VERSION='1.0'
+PKG_DEPS='scummvm'
 
-PKG1_APP1_ID="${PKG1_ID}"
-PKG1_APP1_NAME="${GAME_NAME} (floppy version)"
-PKG1_APP1_NAME_FR="${GAME_NAME} (version disquette)"
-
-PKG2_APP1_ID="${PKG2_ID}"
-PKG2_APP1_NAME="${GAME_NAME} (CD version)"
-PKG2_APP1_NAME_FR="${GAME_NAME} (version CD)"
-
-PKG1_VERSION='1.0'
-PKG1_ARCH='all'
+PKG1_ID="${GAME_ID_PKG1}"
+PKG1_ARCH="${PKG_ARCH}"
+PKG1_VERSION="${PKG_VERSION}"
 PKG1_CONFLICTS=''
-PKG1_DEPS='scummvm'
+PKG1_DEPS="${PKG_DEPS}"
 PKG1_RECS=''
-PKG1_DESC="${GAME_NAME} (floppy version)"
+PKG1_DESC="${GAME_NAME_PKG1}
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
 
-PKG2_VERSION="${PKG1_VERSION}"
-PKG2_ARCH="${PKG1_ARCH}"
-PKG2_CONFLICTS="${PKG1_CONFLICTS}"
-PKG2_DEPS="${PKG1_DEPS}"
-PKG2_RECS="${PKG1_RECS}"
-PKG2_DESC="${GAME_NAME} (CD version)"
+PKG2_ID="${GAME_ID_PKG2}"
+PKG2_ARCH="${PKG_ARCH}"
+PKG2_VERSION="${PKG_VERSION}"
+PKG2_CONFLICTS=''
+PKG2_DEPS="${PKG_DEPS}"
+PKG2_RECS=''
+PKG2_DESC="${GAME_NAME_PKG2}
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
 TARGET_LIB_VERSION='1.13'
+
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
 fi
+
 if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'play-anything.sh not found.\n'
 	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
+
 LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'Wrong version of play-anything.\n'
@@ -100,37 +117,37 @@ if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt
 	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
+
 . "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
-GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT='en'
-WITH_MOVIES_DEFAULT=''
+NO_ICON=0
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+
 fetch_args "$@"
-check_deps 'fakeroot innoextract'
-NO_ICON='1'
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
-set_lang
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
+PATH_DOC_PKG1="${PKG_PREFIX}/share/doc/${GAME_ID_PKG1}"
+PATH_DOC_PKG2="${PKG_PREFIX}/share/doc/${GAME_ID_PKG2}"
+PATH_GAME_PKG1="${PKG_PREFIX}/share/games/${GAME_ID_PKG1}"
+PATH_GAME_PKG2="${PKG_PREFIX}/share/games/${GAME_ID_PKG2}"
 PATH_ICON_BASE='/usr/local/share/icons/hicolor'
-PKG1_PATH_DOC="${PKG_PREFIX}/share/doc/${PKG1_ID}"
-PKG2_PATH_DOC="${PKG_PREFIX}/share/doc/${PKG2_ID}"
-PKG1_PATH_GAME="${PKG_PREFIX}/share/games/${PKG1_ID}"
-PKG2_PATH_GAME="${PKG_PREFIX}/share/games/${PKG2_ID}"
-
 
 printf '\n'
 set_target '1' 'gog.com'
@@ -144,43 +161,68 @@ fi
 
 # Extract game data
 
-for dir in "${PATH_BIN}" "${PATH_DESK}" "${PKG1_PATH_DOC}" "${PKG1_PATH_GAME}" '/DEBIAN/'; do
+printf '%s…\n' "$(l10n 'build_pkg_dirs')"
+rm -rf "${PKG1_DIR}" "${PKG2_DIR}"
+for dir in "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC_PKG1}" "${PATH_GAME_PKG1}" "${PATH_ICON_BASE}" '/DEBIAN/'; do
 	mkdir -p "${PKG1_DIR}${dir}"
 done
-for dir in "${PATH_BIN}" "${PATH_DESK}" "${PKG2_PATH_DOC}" "${PKG2_PATH_GAME}" '/DEBIAN/'; do
+for dir in "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC_PKG2}" "${PATH_GAME_PKG2}" "${PATH_ICON_BASE}" '/DEBIAN/'; do
 	mkdir -p "${PKG2_DIR}${dir}"
 done
-extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'defaults'
-for file in 'app/*.pdf' 'tmp/gog_eula.txt'; do
-	cp -l "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PKG1_PATH_DOC}"
-	cp -l "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PKG2_PATH_DOC}"
+print wait
+
+extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
+
+for file in ${INSTALLER_DOC}; do
+	cp -rl "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC_PKG1}"
+	cp -rl "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_DOC_PKG2}"
 done
-mv "${PKG_TMPDIR}/app/fdd"/* "${PKG1_DIR}${PKG1_PATH_GAME}"
-for file in './gob.lic' './intro.stk' './track1.mp3'; do
-	mv "${PKG_TMPDIR}/app"/${file} "${PKG2_DIR}${PKG2_PATH_GAME}"
+
+for file in ${INSTALLER_GAME_PKG1}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME_PKG1}"
 done
+
+for file in ${INSTALLER_GAME_PKG2}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_GAME_PKG2}"
+done
+
+if [ "${NO_ICON}" = '0' ]; then
+	PATH_GAME="${PATH_GAME_PKG1}"
+	extract_icons "${APP1_ID_PKG1}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
+	cp -rl "${PKG1_DIR}${PATH_ICON_BASE}"/* "${PKG2_DIR}${PATH_ICON_BASE}"
+	find "${PKG2_DIR}${PATH_ICON_BASE}" -name "${APP1_ID_PKG1}.png" | while read file; do
+		mv "${file}" "${file%/*}/${APP1_ID_PKG2}.png"
+	done
+fi
+
+rm "${PKG1_DIR}${PATH_GAME_PKG1}/${APP1_ICON}"
+
 rm -rf "${PKG_TMPDIR}"
+print done
 
 # Write launchers
-PATH_GAME="${PKG1_PATH_GAME}"
-write_bin_scummvm "${PKG1_DIR}${PATH_BIN}/${PKG1_APP1_ID}" "${SCUMMVM_ID}" '' '' "${PKG1_APP1_NAME}"
-PATH_GAME="${PKG2_PATH_GAME}"
-write_bin_scummvm "${PKG2_DIR}${PATH_BIN}/${PKG2_APP1_ID}" "${SCUMMVM_ID}" '' '' "${PKG2_APP1_NAME}"
-write_desktop "${PKG1_APP1_ID}" "${PKG1_APP1_NAME}" "${PKG1_APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${PKG1_APP1_ID}.desktop" "${APP1_CAT}" 'scummvm'
-write_desktop "${PKG2_APP1_ID}" "${PKG2_APP1_NAME}" "${PKG2_APP1_NAME_FR}" "${PKG2_DIR}${PATH_DESK}/${PKG2_APP1_ID}.desktop" "${APP1_CAT}" 'scummvm'
-if [ "${GAME_LANG}" = 'fr' ]; then
-	sed -i 's/Exec=.\+/& -q fr/' "${PKG2_DIR}${PATH_DESK}/${PKG2_APP1_ID}.desktop"
-fi
+
+PATH_GAME="${PATH_GAME_PKG1}"
+write_bin_scummvm "${PKG1_DIR}${PATH_BIN}/${APP1_ID_PKG1}" "${APP1_SCUMMID}" '' '' "${APP1_NAME_PKG1}"
+PATH_GAME="${PATH_GAME_PKG2}"
+write_bin_scummvm "${PKG1_DIR}${PATH_BIN}/${APP1_ID_PKG2}" "${APP1_SCUMMID}" '-q ${LANG%%_*}' '' "${APP1_NAME_PKG2}"
+
+write_desktop "${APP1_ID_PKG1}" "${APP1_NAME_PKG1}" "${APP1_NAME_FR_PKG1}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID_PKG1}.desktop" "${APP1_CAT}" 'scummvm'
+write_desktop "${APP1_ID_PKG2}" "${APP1_NAME_PKG2}" "${APP1_NAME_FR_PKG2}" "${PKG2_DIR}${PATH_DESK}/${APP1_ID_PKG2}.desktop" "${APP1_CAT}" 'scummvm'
 printf '\n'
 
 # Build packages
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
-write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_ORIGIN}${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}"
+
 printf '%s…\n' "$(l10n 'build_pkgs')"
 print wait
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}"
+
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet'
 build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet'
 print done
+
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n'
 print_instructions "${PKG2_DESC}" "${PKG2_DIR}"
