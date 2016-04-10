37c4d5322ad2a307fdf52c788d3a83bc065014c9
vv221 <vv221@dotslashplay.it>
2016-04-10 20:55:36 +0200

    1.13 - Icewind Dale (GOG) full rewrite

--- play.it-1.13/play-icewind-dale_gog-2.0.0.11.sh
+++ play.it-1.13/play-icewind-dale_gog-2.0.0.11.sh
@@ -30,26 +30,29 @@
 ###
 # conversion script for the Icewind Dale installer sold on GOG.com
 # build a .deb package from the Windows installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20160304.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
 
+script_version=20160331.1
+
 # Set game-specific variables
+
+SCRIPT_DEPS_HARD='fakeroot realpath innoextract'
+SCRIPT_DEPS_SOFT='icotool wrestool'
 
 GAME_ID='icewind-dale'
 GAME_ID_SHORT='iwd1'
 GAME_NAME='Icewind Dale'
-GAME_NAME_PKG='Icewind Dale + Heart of Winter'
 
 GAME_ARCHIVE1='setup_icewind_dale_complete_2.0.0.11.exe'
 GAME_ARCHIVE1_MD5='b1395109232aac8d7f8455dad418b084'
 GAME_ARCHIVE_FULLSIZE='2100000'
-PKG_ORIGIN='gog'
-PKG_REVISION='2.0.0.11'
+PKG_REVISION='gog2.0.0.11'
+
+INSTALLER_JUNK='app/gameuxinstallhelper.dll app/gfw_high.ico app/gog.ico app/goggame.dll app/*ddrawfix* app/temp'
+INSTALLER_DOC='app/manual.pdf app/*.txt tmp/gog_eula.txt'
+INSTALLER_GAME='app/*'
 
 TRADFR_URL='http://www.dotslashplay.it/ressources/icewind-dale/'
 TRADFR_ARCHIVE1='iwd1fr-full.7z'
@@ -73,7 +76,7 @@ APP_COMMON_ID="${GAME_ID_SHORT}-common.sh"
 
 APP1_ID="${GAME_ID}"
 APP1_EXE='./idmain.exe'
-APP1_ICON="${APP1_EXE}"
+APP1_ICON='./idmain.exe'
 APP1_ICON_RES='16x16 32x32'
 APP1_NAME="${GAME_NAME}"
 APP1_NAME_FR="${GAME_NAME}"
@@ -81,10 +84,10 @@ APP1_CAT='Game'
 
 APP2_ID="${GAME_ID}_config"
 APP2_EXE='./config.exe'
-APP2_ICON="${APP2_EXE}"
+APP2_ICON='./config.exe'
 APP2_ICON_RES='16x16 32x32'
-APP2_NAME="${GAME_NAME} (settings)"
-APP2_NAME_FR="${GAME_NAME} (réglages)"
+APP2_NAME="${GAME_NAME} - settings"
+APP2_NAME_FR="${GAME_NAME} - réglages"
 APP2_CAT='Settings'
 
 PKG1_ID="${GAME_ID}"
@@ -93,21 +96,27 @@ PKG1_ARCH='i386'
 PKG1_CONFLICTS=''
 PKG1_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
 PKG1_RECS=''
-PKG1_DESC="${GAME_NAME_PKG}"
+PKG1_DESC="${GAME_NAME}
+ package built from GOG.com Windows installer
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
 TARGET_LIB_VERSION='1.13'
+
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
 fi
+
 if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'play-anything.sh not found.\n'
 	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
+
 LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'Wrong version of play-anything.\n'
@@ -115,35 +124,40 @@ if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt
 	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
+
 . "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
-GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT='en'
-WITH_MOVIES_DEFAULT=''
+NO_ICON=0
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+GAME_LANG_DEFAULT='en'
+
 fetch_args "$@"
-check_deps 'fakeroot innoextract realpath' 'icotool wrestool'
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
 set_lang
+
 if [ "${GAME_LANG}" = 'fr' ]; then
-	check_deps_7z
+	SCRIPT_DEPS_HARD="${SCRIPT_DEPS_HARD} 7z"
 fi
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
 PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON_BASE='/usr/local/share/icons/hicolor'
+PATH_ICON_BASE="/usr/local/share/icons/hicolor"
 
 printf '\n'
 set_target '1' 'gog.com'
@@ -152,12 +166,12 @@ if [ "${GAME_LANG}" = 'fr' ]; then
 fi
 printf '\n'
 
-# Check target file integrity
+# Check target files integrity
 
 if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
 	printf '%s…\n' "$(l10n 'checksum_multiple')"
 	print wait
-	checksum "${GAME_ARCHIVE}" 'quiet' "${GAME_ARCHIVE1_MD5}" "${GAME_ARCHIVE2_MD5}"
+	checksum "${GAME_ARCHIVE}" 'quiet' "${GAME_ARCHIVE1_MD5}"
 	if [ "${GAME_LANG}" = 'fr' ]; then
 		checksum "${TRADFR_ARCHIVE}" 'quiet' "${TRADFR_ARCHIVE1_MD5}" "${TRADFR_ARCHIVE2_MD5}" "${TRADFR_ARCHIVE3_MD5}"
 	fi
@@ -168,29 +182,42 @@ fi
 
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}"
 print wait
+
 extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
+
 if [ "${GAME_LANG}" = 'fr' ]; then
-	rm -r "${PKG_TMPDIR}/app/characters"
-	rm -r "${PKG_TMPDIR}/app/scripts"
-	if [ "${TRADFR_ARCHIVE##*/}" = "${TRADFR_ARCHIVE1}" ] || [ "${TRADFR_ARCHIVE##*/}" = "${TRADFR_ARCHIVE2}" ]; then
-		rm -r "${PKG_TMPDIR}/app/sounds"
-	fi
+	INSTALLER_JUNK="${INSTALLER_JUNK} app/characters app/scripts"
+	[ "${TRADFR_ARCHIVE##*/}" = "${TRADFR_ARCHIVE1}" ] && INSTALLER_JUNK="${INSTALLER_JUNK} app/sounds"
+	[ "${TRADFR_ARCHIVE##*/}" = "${TRADFR_ARCHIVE2}" ] && INSTALLER_JUNK="${INSTALLER_JUNK} app/sounds"
+fi
+
+for file in ${INSTALLER_JUNK}; do
+	rm -Rf "${PKG_TMPDIR}"/${file}
+done
+
+if [ "${GAME_LANG}" = 'fr' ]; then
 	extract_data '7z' "${TRADFR_ARCHIVE}" "${PKG_TMPDIR}/app" 'force,quiet'
 fi
-for file in 'tmp/gog_eula.txt' 'app/manual.pdf' 'app/*.txt' ; do
+
+for file in ${INSTALLER_DOC}; do
 	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
 done
-mv "${PKG_TMPDIR}/app"/* "${PKG1_DIR}${PATH_GAME}"
-ini_file="${PKG1_DIR}${PATH_GAME}/icewind.ini"
-for drive in 'HD0' 'CD1'; do
-	sed -i "s/${drive}:=.\+/${drive}:=C:\\\\${GAME_ID}\\\\/" "${ini_file}"
+
+for file in ${INSTALLER_GAME}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
 done
+
+ini_file="${PKG1_DIR}${PATH_GAME}/icewind.ini"
+sed -i "s/HD0:=.\+/HD0:=C:\\\\${GAME_ID}\\\\/" "${ini_file}"
+sed -i "s/CD1:=.\+/CD1:=C:\\\\${GAME_ID}\\\\/" "${ini_file}"
 sed -i "s/CD2:=.\+/CD2:=C:\\\\${GAME_ID}\\\\cd2\\\\/" "${ini_file}"
 sed -i "s/CD3:=.\+/CD3:=C:\\\\${GAME_ID}\\\\cd3\\\\/" "${ini_file}"
+
 if [ "${NO_ICON}" = '0' ]; then
 	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
 	extract_icons "${APP2_ID}" "${APP2_ICON}" "${APP2_ICON_RES}" "${PKG_TMPDIR}"
 fi
+
 rm -rf "${PKG_TMPDIR}"
 print done
 
@@ -200,14 +227,17 @@ write_bin_wine_common "${PKG1_DIR}${PATH_BIN}/${APP_COMMON_ID}"
 write_bin_wine_cfg "${PKG1_DIR}${PATH_BIN}/${GAME_ID_SHORT}-winecfg"
 write_bin_wine "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' "${APP1_NAME}"
 write_bin_wine "${PKG1_DIR}${PATH_BIN}/${APP2_ID}" "${APP2_EXE}" '' '' "${APP2_NAME}"
+
 write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" 'wine'
 write_desktop "${APP2_ID}" "${APP2_NAME}" "${APP2_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP2_ID}.desktop" "${APP2_CAT}" 'wine'
 printf '\n'
 
 # Build package
 
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
-build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'defaults'
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
