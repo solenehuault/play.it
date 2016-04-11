e5cae2054594117fc37ac262825ebac34cb19af4
vv221 <vv221@dotslashplay.it>
2016-04-11 14:47:57 +0200

    1.13 - new game: Duet (Humble)

---
+++ play.it-1.13/play-duet_humble-2016-03-03.sh
@@ -0,0 +1,190 @@
+#!/bin/sh -e
+
+###
+# Copyright (c) 2015-2016, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# conversion script for the Duet archive sold on HumbleBundle.com
+# build a .deb package from the .zip archive
+#
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20160411.1
+
+# Set game-specific variables
+
+SCRIPT_DEPS_HARD='fakeroot realpath unzip'
+
+GAME_ID='duet'
+GAME_ID_SHORT='duet'
+GAME_NAME='Duet'
+
+GAME_ARCHIVE1='Duet-Build1006023-Linux64.zip'
+GAME_ARCHIVE1_MD5='b9c34c29da94c199ee75a5e71272a1eb'
+GAME_ICON1='logo.png'
+GAME_ICON1_URL='http://www.kumobius.com/presskits/duet/images/logo.png'
+GAME_ARCHIVE_FULLSIZE='210000'
+PKG_REVISION='humble160303'
+
+INSTALLER_GAME='./Duet ./libsfml-*.so.2.3 ./libsteam_api.so ./Media ./steam_appid.txt'
+
+APP1_ID="${GAME_ID}"
+APP1_EXE='./Duet'
+APP1_ICON='./logo.png'
+APP1_ICON_RES='256x256'
+APP1_NAME="${GAME_NAME}"
+APP1_NAME_FR="${GAME_NAME}"
+APP1_CAT='Game'
+
+PKG1_ID="${GAME_ID}"
+PKG1_VERSION='1.0'
+PKG1_ARCH='amd64'
+PKG1_CONFLICTS=''
+PKG1_DEPS='libc6, libstdc++6, libgl1-mesa-glx | libgl1, libasound2-plugins, libfreetype6, libjpeg8, libopenal1, libudev1'
+PKG1_RECS=''
+PKG1_DESC="${GAME_NAME}
+ package built from HumbleBundle.com archive
+ ./play.it script version ${script_version}"
+
+# Load common functions
+
+TARGET_LIB_VERSION='1.13'
+
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+
+if ! [ -e "${PLAYIT_LIB}" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
+	exit 1
+fi
+
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
+if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	exit 1
+fi
+
+. "${PLAYIT_LIB}"
+
+# Set extra variables
+
+NO_ICON=0
+
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+
+fetch_args "$@"
+
+set_checksum
+set_compression
+set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+
+PATH_BIN="${PKG_PREFIX}/games"
+PATH_DESK='/usr/local/share/applications'
+PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
+PATH_ICON_BASE="/usr/local/share/icons/hicolor"
+
+printf '\n'
+set_target '1' 'humblebundle.com'
+set_target_optional 'GAME_ICON' "${GAME_ICON1}"
+[ -n "${GAME_ICON}" ] || NO_ICON=1
+printf '\n'
+
+# Check target files integrity
+
+if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}"
+fi
+
+# Extract game data
+
+PATH_ICON="${PATH_ICON_BASE}/${APP1_ICON_RES}/apps"
+build_pkg_dirs '1' "${PATH_BIN}" "${PATH_GAME}" "${PATH_DESK}" "${PATH_ICON}"
+[ "${NO_ICON}" = '0' ] && mkdir -p "${PKG1_DIR}${PATH_ICON}"
+print wait
+
+extract_data 'zip' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
+
+for file in ${INSTALLER_GAME}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+done
+
+if [ "${NO_ICON}" = '0' ]; then
+	cp "${GAME_ICON}" "${PKG1_DIR}${PATH_ICON}/${APP1_ID}.png"
+fi
+
+chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE}"
+
+rm -rf "${PKG_TMPDIR}"
+print done
+
+# Write launchers
+
+write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' '' "${APP1_NAME}"
+write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" ''
+printf '\n'
+
+# Build package
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+
+file="${PKG1_DIR}/DEBIAN/postinst"
+cat > "${file}" << EOF
+#!/bin/sh -e
+ln -s /lib/x86_64-linux-gnu/libudev.so.1 "${PATH_GAME}/libudev.so.0"
+exit 0
+EOF
+chmod 755 "${file}"
+
+file="${PKG1_DIR}/DEBIAN/prerm"
+cat > "${file}" << EOF
+#!/bin/sh -e
+rm "${PATH_GAME}/libudev.so.0"
+exit 0
+EOF
+chmod 755 "${file}"
+
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
+print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
+printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
+
+exit 0
