cf5cc2de25b4992c49ff0ede77cc4812a71d8a98
vv221 <vv221@dotslashplay.it>
2016-04-13 16:05:51 +0200

    1.13 - new game: Wing Commander 3 (GOG)

---
+++ play.it-1.13/play-wing-commander-3_gog-2.1.0.7.sh
@@ -0,0 +1,194 @@
+#!/bin/sh -e
+
+###
+# Copyright (c) 2015-2016, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# conversion script for the Wing Commander 3 installer sold on GOG.com
+# build a .deb package from the Windows installer
+#
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20160413.1
+
+# Set game-specific variables
+
+SCRIPT_DEPS_HARD='fakeroot innoextract realpath'
+SCRIPT_DEPS_SOFT='icotool'
+
+GAME_ID='wing-commander-3'
+GAME_ID_SHORT='wc3'
+GAME_NAME='Wing Commander 3'
+
+GAME_ARCHIVE1='setup_wing_commander3_2.1.0.7.exe'
+GAME_ARCHIVE1_MD5='c9c9b539e6e1f0b0509b6f777878d91e'
+GAME_ARCHIVE_FULLSIZE='1900000'
+PKG_REVISION='gog2.1.0.7'
+
+INSTALLER_DOC='app/*.pdf'
+INSTALLER_GAME='app/data.dat app/objects.tre app/wc3 app/goggame-1207658966.ico'
+
+GAME_IMAGE='./data.dat'
+
+GAME_CACHE_DIRS=''
+GAME_CACHE_FILES=''
+GAME_CACHE_FILES_POST='./SWAPFILE.*'
+GAME_CONFIG_DIRS='./wc3'
+GAME_CONFIG_FILES=''
+GAME_CONFIG_FILES_POST=''
+GAME_DATA_DIRS=''
+GAME_DATA_FILES=''
+GAME_DATA_FILES_POST='./*.WSG'
+
+APP_COMMON_ID="${GAME_ID_SHORT}-common.sh"
+
+APP1_ID="${GAME_ID}"
+APP1_EXE='./wc3.exe'
+APP1_ICON='./goggame-1207658966.ico'
+APP1_ICON_RES='16x16 32x32 48x48 256x256'
+APP1_NAME="${GAME_NAME}"
+APP1_NAME_FR="${GAME_NAME}"
+APP1_CAT='Game'
+
+PKG1_ID="${GAME_ID}"
+PKG1_ARCH='all'
+PKG1_VERSION='1.0'
+PKG1_CONFLICTS=''
+PKG1_DEPS='dosbox'
+PKG1_RECS=''
+PKG1_DESC="${GAME_NAME}
+ package built from GOG.com Windows installer
+ ./play.it script version ${script_version}"
+
+# Load common functions
+
+TARGET_LIB_VERSION='1.13'
+
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+
+if ! [ -e "${PLAYIT_LIB}" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
+	exit 1
+fi
+
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
+if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	exit 1
+fi
+
+. "${PLAYIT_LIB}"
+
+# Set extra variables
+
+NO_ICON=0
+
+PKG_PREFIX_DEFAULT='/usr/local'
+PKG_COMPRESSION_DEFAULT='none'
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+
+fetch_args "$@"
+
+set_checksum
+set_compression
+set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+
+PATH_BIN="${PKG_PREFIX}/games"
+PATH_DESK='/usr/local/share/applications'
+PATH_DESK_DIR='/usr/local/share/desktop-directories'
+PATH_DESK_MERGED='/etc/xdg/menus/applications-merged'
+PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
+PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
+PATH_ICON_BASE='/usr/local/share/icons/hicolor'
+
+printf '\n'
+set_target '1' 'gog.com'
+printf '\n'
+
+# Check target files integrity
+
+if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}"
+fi
+
+# Extract game data
+
+build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_DESK_DIR}" "${PATH_DESK_MERGED}" "${PATH_GAME}"
+print wait
+
+extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
+
+for file in ${INSTALLER_DOC}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+done
+
+for file in ${INSTALLER_GAME}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+done
+
+if [ "${NO_ICON}" = '0' ]; then
+	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
+fi
+rm "${PKG1_DIR}${PATH_GAME}/${APP1_ICON}"
+
+rm -rf "${PKG_TMPDIR}"
+print done
+
+# Write launchers
+
+write_bin_dosbox_common "${PKG1_DIR}${PATH_BIN}/${APP_COMMON_ID}"
+write_bin_dosbox "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' "${APP1_NAME}"
+
+sed -i 's/${GAME_EXE/d:\n&/' "${PKG1_DIR}${PATH_BIN}/${APP1_ID}"
+
+write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" 'dosbox'
+printf '\n'
+
+# Build package
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
+print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
+printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
+
+exit 0
