6fec999b7b1025757667354495df366640460e96
vv221 <vv221@dotslashplay.it>
2016-04-14 13:58:10 +0200

    1.13 - Shadowrun Hong Kong (GOG) updated to work on the new installer (2.8.0.10)

---
+++ play.it-1.13/play-shadowrun-hong-kong_gog-2.8.0.10.sh
@@ -0,0 +1,204 @@
+#!/bin/sh -e
+
+###
+# Copyright (c) 2015-2016, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# conversion script for the Shadowrun: Hong Kong installer sold on GOG.com
+# build a .deb package from the .sh MojoSetup installer
+# tested on Debian, should work on any .deb-based distribution
+#
+# script version 20160414.1
+#
+# send your bug reports to vv221@dotslashplay.it
+# start the e-mail subject by "./play.it" to avoid it being flagged as spam
+###
+
+# Set game-specific variables
+
+GAME_ID='shadowrun-hong-kong'
+GAME_ID_SHORT='srhk'
+GAME_NAME='Shadowrun: Hong Kong'
+
+GAME_ARCHIVE1='gog_shadowrun_hong_kong_extended_edition_2.8.0.10.sh'
+GAME_ARCHIVE1_MD5='a6a6859556306c5fb5e165b4fcefccce'
+GAME_ARCHIVE_FULLSIZE='12000000'
+PKG_REVISION='gog2.8.0.10'
+
+INSTALLER_DOC='data/noarch/docs/*'
+INSTALLER_GAME_PKG1='data/noarch/game/*'
+INSTALLER_GAME_PKG2='data/noarch/game/SRHK_Data/StreamingAssets/standalone'
+
+GAME_CACHE_DIRS=''
+GAME_CACHE_FILES=''
+GAME_CACHE_FILES_POST=''
+GAME_CONFIG_DIRS=''
+GAME_CONFIG_FILES=''
+GAME_CONFIG_FILES_POST=''
+GAME_DATA_DIRS='./DumpBox ./logs'
+GAME_DATA_FILES='./SRHK ./ShadowrunEditor ./SRHK.sh'
+GAME_DATA_FILES_POST=''
+
+APP_COMMON_ID="${GAME_ID_SHORT}-common.sh"
+
+APP1_ID="${GAME_ID}"
+APP1_EXE='./SRHK'
+APP1_ICON='./SRHK_Data/Resources/UnityPlayer.png'
+APP1_ICON_RES='128x128'
+APP1_NAME="${GAME_NAME}"
+APP1_NAME_FR="${GAME_NAME}"
+APP1_CAT='Game'
+
+PKG1_ID="${GAME_ID}"
+PKG1_VERSION='3.1.2'
+PKG1_ARCH='i386'
+PKG1_CONFLICTS=''
+PKG1_RECS=''
+PKG1_DESC="${GAME_NAME}"
+
+PKG2_ID="${GAME_ID}-data"
+PKG2_VERSION="${PKG1_VERSION}"
+PKG2_ARCH="${PKG1_ARCH}"
+PKG2_CONFLICTS=''
+PKG2_RECS=''
+PKG2_DESC="${GAME_NAME} (data)"
+
+PKG1_DEPS="${PKG2_ID} (= ${PKG2_VERSION}-${PKG_REVISION}), libglu1-mesa | libglu1, libqtgui4, libqt4-network, libxcursor1, libxrandr2"
+PKG2_DEPS=''
+
+# Load common functions
+
+TARGET_LIB_VERSION='1.13'
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+if ! [ -e "${PLAYIT_LIB}" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
+	exit 1
+fi
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	exit 1
+fi
+. "${PLAYIT_LIB}"
+
+# Set extra variables
+
+PKG_PREFIX_DEFAULT='/usr/local'
+PKG_COMPRESSION_DEFAULT='none'
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+GAME_LANG_DEFAULT=''
+WITH_MOVIES_DEFAULT=''
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+fetch_args "$@"
+printf '\n'
+check_deps 'fakeroot realpath unzip'
+printf '\n'
+set_checksum
+set_compression
+set_prefix
+
+PATH_BIN="${PKG_PREFIX}/games"
+PATH_DESK='/usr/local/share/applications'
+PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
+PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
+PATH_ICON="/usr/local/share/icons/hicolor/${APP1_ICON_RES}/apps"
+
+printf '\n'
+set_target '1' 'gog.com'
+printf '\n'
+
+# Check target file integrity
+
+if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}" "${GAME_ARCHIVE2_MD5}"
+fi
+
+# Extract game data
+
+build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON}"
+mkdir -p "${PKG2_DIR}${PATH_GAME}/SRHK_Data/StreamingAssets" "${PKG2_DIR}/DEBIAN"
+print wait
+extract_data 'mojo' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
+for file in ${INSTALLER_DOC}; do
+		mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+done
+for file in ${INSTALLER_GAME_PKG2}; do
+		mv "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_GAME}/SRHK_Data/StreamingAssets"
+done
+for file in ${INSTALLER_GAME_PKG1}; do
+		mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+done
+for file in "${APP1_EXE}" './ShadowrunEditor' './SRHK.sh'; do
+	chmod 755 "${PKG1_DIR}${PATH_GAME}/${file}"
+done
+print done
+
+# Write launchers
+
+write_bin_native_prefix_common "${PKG1_DIR}${PATH_BIN}/${APP_COMMON_ID}"
+write_bin_native_prefix "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '-logFile ./logs/${GAME_EXE}_$(date +%F-%R).log' '' '' "${APP1_NAME}"
+sed -i 's#exit 0#rmdir --ignore-fail-on-non-empty -p "${HOME}/Documents/Shadowrun Hong Kong/ContentPacks"\n&#' "${PKG1_DIR}${PATH_BIN}/${APP1_ID}"
+write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}"
+printf '\n'
+
+# Build packages
+
+printf '%s…\n' "$(l10n 'build_pkgs')"
+print wait
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+file="${PKG1_DIR}/DEBIAN/postinst"
+cat > "${file}" << EOF
+#!/bin/sh -e
+ln -s "${PATH_GAME}/${APP1_ICON}" "${PATH_ICON}/${GAME_ID}.png"
+exit 0
+EOF
+chmod 755 "${file}"
+file="${PKG1_DIR}/DEBIAN/prerm"
+cat > "${file}" << EOF
+#!/bin/sh -e
+rm "${PATH_ICON}/${GAME_ID}.png"
+exit 0
+EOF
+chmod 755 "${file}"
+write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}"
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet'
+build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet'
+print done
+print_instructions "${PKG1_DESC}" "${PKG2_DIR}" "${PKG1_DIR}"
+printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
+
+exit 0
