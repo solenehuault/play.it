b64eb1420587862d9b58ec7116cd5c5e60354048
vv221 <vv221@dotslashplay.it>
2016-04-14 20:39:51 +0200

    1.13 - Faster Than Light (GOG & Humble) arch-independant data has a dedicated package icon extraction is done via a dedicated function

--- play.it-1.13/play-faster-than-light_humble-1.5.13.sh
+++ play.it-1.13/play-faster-than-light_humble-1.5.13.sh
@@ -36,7 +36,7 @@
 # start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
 
-script_version=20160310.1
+script_version=20160414.1
 
 # Set game-specific variables
 
@@ -53,13 +53,13 @@ GAME_ARCHIVE_FULLSIZE='220000'
 PKG_REVISION='humble140602'
 
 INSTALLER_DOC='FTL/FTL_README.html FTL/data/licenses'
-INSTALLER_GAME_ARCH1='FTL/data/x86'
-INSTALLER_GAME_ARCH2='FTL/data/amd64'
-INSTALLER_GAME='FTL/data/*'
+INSTALLER_GAME_PKG1='FTL/data/x86'
+INSTALLER_GAME_PKG2='FTL/data/amd64'
+INSTALLER_GAME_PKG3='FTL/data/*'
 
 APP1_ID="${GAME_ID}"
-APP1_EXE_ARCH1='x86/bin/FTL'
-APP1_EXE_ARCH2='amd64/bin/FTL'
+APP1_EXE_PKG1='x86/bin/FTL'
+APP1_EXE_PKG2='amd64/bin/FTL'
 APP1_ICON1='./exe_icon.bmp'
 APP1_ICON1_RES='64x64'
 APP1_ICON2='resources/exe_icon.bmp'
@@ -71,27 +71,39 @@ APP1_CAT='Game'
 PKG_ID="${GAME_ID}"
 PKG_VERSION='1.5.13'
 PKG_DEPS='libglu1-mesa | libglu1, libsdl1.2debian'
-PKG_RECS=''
 PKG_DESC="${GAME_NAME}
- package built from HumbleBundle.com installer
+ package built from HumbleBundle.com archive
  ./play.it script version ${script_version}"
 
 PKG1_ID="${PKG_ID}"
 PKG1_ARCH='i386'
 PKG1_VERSION="${PKG_VERSION}"
 PKG1_DEPS="${PKG_DEPS}"
-PKG1_RECS="${PKG_RECS}"
+PKG1_RECS=''
 PKG1_DESC="${PKG_DESC}"
 
 PKG2_ID="${PKG_ID}"
 PKG2_ARCH='amd64'
 PKG2_VERSION="${PKG_VERSION}"
 PKG2_DEPS="${PKG_DEPS}"
-PKG2_RECS="${PKG_RECS}"
+PKG2_RECS=''
 PKG2_DESC="${PKG_DESC}"
+
+PKG3_ID="${GAME_ID}-common"
+PKG3_ARCH='all'
+PKG3_VERSION="${PKG_VERSION}"
+PKG3_CONFLICTS=''
+PKG3_DEPS=''
+PKG3_RECS=''
+PKG3_DESC="${GAME_NAME} - arch-independant data
+ package built from HumbleBundle.com archive
+ ./play.it script version ${script_version}"
 
 PKG1_CONFLICTS="${PKG2_ID}:${PKG2_ARCH}"
 PKG2_CONFLICTS="${PKG1_ID}:${PKG1_ARCH}"
+
+PKG1_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG1_DEPS}"
+PKG2_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG2_DEPS}"
 
 # Load common functions
 
@@ -120,6 +132,17 @@ fi
 
 . "${PLAYIT_LIB}"
 
+# Set script-specific functions
+
+extract_icon_ftl() {
+	local PKG1_DIR="${PKG3_DIR}"
+	local res="$3"
+	extract_icons "$@"
+	PATH_ICON="${PATH_ICON_BASE}/${res}/apps"
+	mkdir -p "${PKG3_DIR}${PATH_ICON}"
+	mv "${PKG_TMPDIR}/${APP1_ID}.png" "${PKG3_DIR}${PATH_ICON}"
+}
+
 # Set extra variables
 
 NO_ICON=0
@@ -140,6 +163,7 @@ check_deps_soft ${SCRIPT_DEPS_SOFT}
 game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG3_DIR' "${PKG3_ID}_${PKG3_VERSION}-${PKG_REVISION}_${PKG3_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
@@ -148,9 +172,7 @@ PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
 PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 
 printf '\n'
-
 set_target '1' 'humblebundle.com'
-
 printf '\n'
 
 # Check target files integrity
@@ -161,90 +183,69 @@ fi
 
 # Extract game data
 
-build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON_BASE}"
-
+build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DESK}" "${PATH_GAME}"
+rm -rf "${PKG3_DIR}"
+mkdir -p "${PKG3_DIR}/DEBIAN" "${PKG3_DIR}${PATH_DOC}" "${PKG3_DIR}${PATH_GAME}"
 print wait
 
 extract_data 'tar' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
 
 for file in ${INSTALLER_DOC}; do
-	cp -rl "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
-	cp -rl "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_DOC}"
+	mv "${PKG_TMPDIR}"/${file} "${PKG3_DIR}${PATH_DOC}"
 done
 
-for file in ${INSTALLER_GAME_ARCH1}; do
+for file in ${INSTALLER_GAME_PKG1}; do
 	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
 done
 
-for file in ${INSTALLER_GAME_ARCH2}; do
+for file in ${INSTALLER_GAME_PKG2}; do
 	mv "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_GAME}"
 done
 
-for file in ${INSTALLER_GAME}; do
-	cp -rl "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
-	cp -rl "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_GAME}"
+for file in ${INSTALLER_GAME_PKG3}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG3_DIR}${PATH_GAME}"
 done
 
-chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE_ARCH1}"
-chmod 755 "${PKG2_DIR}${PATH_GAME}/${APP1_EXE_ARCH2}"
+chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE_PKG1}"
+chmod 755 "${PKG2_DIR}${PATH_GAME}/${APP1_EXE_PKG2}"
 
 if [ "${NO_ICON}" = '0' ]; then
-
-	extract_icons "${APP1_ID}" "${APP1_ICON1}" "${APP1_ICON1_RES}" "${PKG_TMPDIR}" "${APP1_ICON1_RES}"
-
-	for pkg_dir in "${PKG1_DIR}" "${PKG2_DIR}"; do
-		path_icon="${pkg_dir}${PATH_ICON_BASE}/${APP1_ICON1_RES}/apps"
-		mkdir -p "${path_icon}"
-		cp -l "${PKG_TMPDIR}/${APP1_ID}.png" "${path_icon}"
-	done
-
-	rm "${PKG_TMPDIR}/${APP1_ID}.png"
-
-	extract_icons "${APP1_ID}" "${APP1_ICON2}" "${APP1_ICON2_RES}" "${PKG_TMPDIR}" "${APP1_ICON2_RES}"
-
-	for pkg_dir in "${PKG1_DIR}" "${PKG2_DIR}"; do
-		path_icon="${pkg_dir}${PATH_ICON_BASE}/${APP1_ICON2_RES}/apps"
-		mkdir -p "${path_icon}"
-		cp -l "${PKG_TMPDIR}/${APP1_ID}.png" "${path_icon}"
-	done
-
+	extract_icon_ftl "${APP1_ID}" "${APP1_ICON1}" "${APP1_ICON1_RES}" "${PKG_TMPDIR}" "${APP1_ICON1_RES}"
+	extract_icon_ftl "${APP1_ID}" "${APP1_ICON2}" "${APP1_ICON2_RES}" "${PKG_TMPDIR}" "${APP1_ICON2_RES}"
 fi
 
 rm -rf "${PKG_TMPDIR}"
-
 print done
 
 # Write launchers
 
-write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_ARCH1}" '' 'x86/lib' '' "${APP1_NAME} (${PKG1_ARCH})"
-write_bin_native "${PKG2_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_ARCH2}" '' 'amd64/lib' '' "${APP1_NAME} (${PKG2_ARCH})"
+write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_PKG1}" '' 'x86/lib' '' "${APP1_NAME} (${PKG1_ARCH})"
+write_bin_native "${PKG2_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_PKG2}" '' 'amd64/lib' '' "${APP1_NAME} (${PKG2_ARCH})"
 
 sed -i 's|cd "${GAME_EXE_PATH%/\*}"|cd "${GAME_PATH}"|' "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${PKG2_DIR}${PATH_BIN}/${APP1_ID}"
 sed -i 's|./"${GAME_EXE_PATH##\*/}"|./"${GAME_EXE}"|' "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${PKG2_DIR}${PATH_BIN}/${APP1_ID}"
 
 write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" ''
 cp -l "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${PKG2_DIR}${PATH_DESK}/${APP1_ID}.desktop"
-
 printf '\n'
 
 # Build packages
 
 printf '%s…\n' "$(l10n 'build_pkgs')"
-
 print wait
 
 write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}" 'arch'
 write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}" 'arch'
+write_pkg_debian "${PKG3_DIR}" "${PKG3_ID}" "${PKG3_VERSION}-${PKG_REVISION}" "${PKG3_ARCH}" "${PKG3_CONFLICTS}" "${PKG3_DEPS}" "${PKG3_RECS}" "${PKG3_DESC}"
 
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG1_ARCH}"
 build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG2_ARCH}"
-
+build_pkg "${PKG3_DIR}" "${PKG3_DESC}" "${PKG_COMPRESSION}" 'quiet'
 print done
 
-print_instructions "$(printf '%s' "${PKG1_DESC}" | head -n1) (${PKG1_ARCH})" "${PKG1_DIR}"
+print_instructions "$(printf '%s' "${PKG1_DESC}" | head -n1) (${PKG1_ARCH})" "${PKG3_DIR}" "${PKG1_DIR}"
 printf '\n'
-print_instructions "$(printf '%s' "${PKG2_DESC}" | head -n1) (${PKG2_ARCH})" "${PKG2_DIR}"
-
+print_instructions "$(printf '%s' "${PKG2_DESC}" | head -n1) (${PKG2_ARCH})" "${PKG3_DIR}" "${PKG2_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
 exit 0
