748bef943872412eaeb3afcc9b7e93803f2ada0d
vv221 <vv221@dotslashplay.it>
2016-04-22 16:06:04 +0200

    1.13 - Lure of the Temptress (GOG) full rewrite

--- play.it-1.13/play-lure-of-the-temptress_gog-2.0.0.5.sh
+++ play.it-1.13/play-lure-of-the-temptress_gog-2.0.0.5.sh
@@ -30,13 +30,11 @@
 ###
 # conversion script for the Lure of the Temptress installer sold on GOG.com
 # build a .deb package from the .sh MojoSetup installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20160103.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160422.1
 
 # Set game-specific variables
 
@@ -51,7 +49,7 @@ GAME_ARCHIVE1_MD5='8d7431b120e62ca1f5c397c03f61557e'
 GAME_ARCHIVE_FULLSIZE='60000'
 PKG_REVISION='gog2.0.0.5'
 
-INSTALLER_DOC='data/noarch/docs/* data/noarch/data/License_and_support.txt'
+INSTALLER_DOC='data/noarch/docs/* data/noarch/data/*.txt'
 INSTALLER_GAME='data/noarch/data/*'
 
 APP1_ID="${GAME_ID}"
@@ -70,71 +68,99 @@ PKG1_DEPS='scummvm'
 PKG1_RECS=''
 PKG1_DESC="${GAME_NAME}
  package built from GOG.com installer
- ./play.it script version 20160103.1"
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
 TARGET_LIB_VERSION='1.13'
-if ! [ -e './play-anything.sh' ]; then
+
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+
+if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'play-anything.sh not found.\nIt must be placed in the same directory than this script.\n\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
-LIB_VERSION="$(grep '^# library version' './play-anything.sh' | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'Wrong version of play-anything.\nIt must be at least %s but lower than %s.\n\n' "${TARGET_LIB_VERSION}" "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
-. './play-anything.sh'
+
+. "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
 GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT=''
-WITH_MOVIES_DEFAULT=''
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 fetch_args "$@"
-check_deps "${SCRIPT_DEPS_HARD}"
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
 PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON="/usr/local/share/icons/hicolor/${APP1_ICON_RES}/apps"
+PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 
 printf '\n'
 set_target '1' 'gog.com'
 printf '\n'
 
-# Check target files integrity
+# Check target file integrity
 
 if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
-	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}"
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}" "${GAME_ARCHIVE2_MD5}"
 fi
 
 # Extract game data
 
+PATH_ICON="${PATH_ICON_BASE}/${APP1_ICON_RES}/apps"
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON}"
-extract_data 'mojo' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
+print wait
+
+extract_data 'mojo' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet,tolower'
+
 for file in ${INSTALLER_DOC}; do
 	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
 done
+
 for file in ${INSTALLER_GAME}; do
 	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
 done
-mv "${PKG_TMPDIR}"/${APP1_ICON} "${PKG1_DIR}${PATH_ICON}/${APP1_ID}.png"
+
+mv "${PKG_TMPDIR}/${APP1_ICON}" "${PKG1_DIR}${PATH_ICON}/${APP1_ID}.png"
+
 rm -rf "${PKG_TMPDIR}"
+print done
+
+# Write launchers
+
+write_bin_scummvm "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_SCUMMID}" '' '' "${APP1_NAME}"
+write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}"
+printf '\n'
+
+# Build package
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+
 file="${PKG1_DIR}/etc/apt/preferences.d/${GAME_ID}"
 mkdir -p "${file%/*}"
 cat > "${file}" << EOF
@@ -142,18 +168,9 @@ Package: ${GAME_ID}
 Pin: release o=Debian
 Pin-Priority: -1
 EOF
-print done
 
-# Write launchers
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
 
-write_bin_scummvm "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_SCUMMID}" '' '' "${APP1_NAME}"
-write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" ''
-printf '\n'
-
-# Build package
-
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
-build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'defaults'
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
