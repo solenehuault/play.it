2a1b161453144b461f738dcd2210253dbd3dbacb
vv221 <vv221@dotslashplay.it>
2016-04-27 17:51:08 +0200

    1.13 - Neverwinter Nights (GOG 2.0.0.15) full rewrite

--- play.it-1.13/play-neverwinter-nights_gog-2.0.0.15.sh
+++ play.it-1.13/play-neverwinter-nights_gog-2.0.0.15.sh
@@ -14,29 +14,27 @@
 # this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
 ###
 
 ###
 # conversion script for the Neverwinter Nights installer sold on GOG.com
 # build a .deb package from the Windows installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20160123.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160427.1
 
 # Set game-specific variables
 
@@ -78,6 +76,7 @@ CLIENT_ES_ARCHIVE2_MD5='cc63d00327ea5426c5a2322075cafba7'
 CLIENT_ES_ARCHIVE3='Spanish_linuxclient168_xp2.tar.gz'
 CLIENT_ES_ARCHIVE3_URL='http://files.bioware.com/neverwinternights/updates/linux/168/Spanish_linuxclient168_xp2.tar.gz'
 CLIENT_ES_ARCHIVE3_MD5='04719199f69f19277f5c068826eee72c'
+
 CLIENT_ARCHIVE_MOVIES='nwmovies-mpv.tar.gz'
 CLIENT_ARCHIVE_MOVIES_URL='https://sites.google.com/site/gogdownloader/nwmovies-mpv.tar.gz'
 CLIENT_ARCHIVE_MOVIES_MD5='71f3d88db1cd75665b62b77f7604dce1'
@@ -85,10 +84,11 @@ CLIENT_ARCHIVE_MOVIES_MD5='71f3d88db1cd75665b62b77f7604dce1'
 GAME_ARCHIVE_FULLSIZE='6500000'
 PKG_REVISION='gog2.0.0.15'
 
-INSTALLER_DOC='app/docs'
-INSTALLER_GAME='app/nwn.exe app/ambient app/data app/dmvault app/hak app/localvault app/modules app/movies app/music app/nwm app/override app/texturepacks app/*.key app/*.tlk'
+INSTALLER_PATH='app'
+INSTALLER_DOC='docs'
+INSTALLER_GAME='ambient data dmvault hak localvault modules movies music nwm override texturepacks nwn.exe *.key *.tlk'
 
-NWMOVIES_BUILD_REQ_32="gcc libelf-dev:i386 libsdl1.2-dev"
+NWMOVIES_BUILD_REQ_32='gcc libelf-dev:i386 libsdl1.2-dev'
 NWMOVIES_BUILD_REQ_64="${NWMOVIES_BUILD_REQ_32} gcc-multilib"
 
 GAME_CACHE_DIRS='/temp ./tempclient'
@@ -110,36 +110,45 @@ APP1_NAME="${GAME_NAME}"
 APP1_NAME_FR="${GAME_NAME}"
 APP1_CAT='Game'
 
-PKG_EN_VERSION='1.69.8109'
-PKG_ES_VERSION='1.68.8099'
-
 PKG1_ID="${GAME_ID}"
-PKG1_VERSION='1.68.8099'
+PKG1_EN_VERSION='1.69.8109'
+PKG1_ES_VERSION='1.68.8099'
 PKG1_ARCH='i386'
 PKG1_CONFLICTS=''
 PKG1_DEPS='libglu1-mesa | ligblu1, libsdl1.2debian'
 PKG1_RECS=''
 PKG1_DESC="${GAME_NAME}
  package built from GOG.com installer
- ./play.it script version 20160123.1"
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
 TARGET_LIB_VERSION='1.13'
-if ! [ -e './play-anything.sh' ]; then
+
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+
+if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'play-anything.sh not found.\nIt must be placed in the same directory than this script.\n\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
-LIB_VERSION="$(grep '^# library version' './play-anything.sh' | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'Wrong version of play-anything.\nIt must be at least %s but lower than %s.\n\n' "${TARGET_LIB_VERSION}" "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
-. './play-anything.sh'
 
-# Define script-specific functions
+. "${PLAYIT_LIB}"
+
+# Set script-specific functions
 
 help() {
 	printf '%s %s' "$0" '[<archive>] [--checksum=md5|none] [--compression=none|gzip|xz] [--prefix=dir] [--lang=en|es] [--with-movies]'
@@ -152,7 +161,9 @@ help() {
 }
 
 set_lang() {
-if [ -z "${GAME_LANG}" ]; then export GAME_LANG="${GAME_LANG_DEFAULT}"; fi
+if [ -z "${GAME_LANG}" ]; then
+	GAME_LANG="${GAME_LANG_DEFAULT}"
+fi
 printf '%s: %s\n' "$(l10n 'set_lang')" "${GAME_LANG}"
 if [ -z "$(printf '#en#es#' | grep "#${GAME_LANG}#")" ]; then
 	print error
@@ -166,7 +177,7 @@ fi
 
 set_nwmovies() {
 if [ -z "${WITH_MOVIES}" ]; then
-	export WITH_NWMOVIE="${WITH_MOVIES_DEFAULT}"
+	WITH_NWMOVIE="${WITH_MOVIES_DEFAULT}"
 fi
 if [ "${WITH_MOVIES}" = '1' ]; then
 	printf '%s.\n' "$(l10n 'movies_enabled')"
@@ -176,11 +187,11 @@ if [ "${WITH_MOVIES}" = '1' ]; then
 		NWMOVIES_BUILD_REQ="${NWMOVIES_BUILD_REQ_32}"
 	fi
 	for pkg in ${NWMOVIES_BUILD_REQ}; do
-		if ! dpkg -s "${pkg}" > /dev/null 2>&1; then
-			print error
-			printf "%s:\n%s\n" "$(l10n 'movies_build_deps')" "${NWMOVIES_BUILD_REQ}"
-			exit 1
-		fi
+	if ! dpkg -s "${pkg}" > /dev/null 2>&1; then
+		print error
+		printf "%s:\n%s\n" "$(l10n 'movies_build_deps')" "${NWMOVIES_BUILD_REQ}"
+		exit 1
+	fi
 	done
 else
 	print warning
@@ -189,6 +200,9 @@ fi
 }
 
 # Set extra variables
+
+NO_ICON=0
+
 GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
 PKG_COMPRESSION_DEFAULT='none'
 PKG_PREFIX_DEFAULT='/usr/local'
@@ -196,53 +210,52 @@ GAME_LANG_DEFAULT='en'
 WITH_MOVIES_DEFAULT='0'
 
 fetch_args "$@"
-printf '\n'
-check_deps "${SCRIPT_DEPS_HARD}" "${SCRIPT_DEPS_SOFT}"
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
-set_nwmovies
 set_lang
+set_nwmovies
 
-if [ "${GAME_LANG}" = 'en' ]; then
-	GAME_ARCHIVE1="${GAME_EN_ARCHIVE1}"
-	GAME_ARCHIVE1_MD5="${GAME_EN_ARCHIVE1_MD5}"
-	GAME_ARCHIVE2="${GAME_EN_ARCHIVE2}"
-	GAME_ARCHIVE2_MD5="${GAME_EN_ARCHIVE2_MD5}"
-	GAME_ARCHIVE3="${GAME_EN_ARCHIVE3}"
-	GAME_ARCHIVE3_MD5="${GAME_EN_ARCHIVE3_MD5}"
-	CLIENT_ARCHIVE2="${CLIENT_EN_ARCHIVE2}"
-	CLIENT_ARCHIVE2_URL="${CLIENT_EN_ARCHIVE2_URL}"
-	CLIENT_ARCHIVE2_MD5="${CLIENT_EN_ARCHIVE2_MD5}"
-	CLIENT_ARCHIVE3="${CLIENT_EN_ARCHIVE3}"
-	CLIENT_ARCHIVE3_URL="${CLIENT_EN_ARCHIVE3_URL}"
-	CLIENT_ARCHIVE3_MD5="${CLIENT_EN_ARCHIVE3_MD5}"
-	PKG1_VERSION="${PKG_EN_VERSION}"
-elif [ "${GAME_LANG}" = 'es' ]; then
-	GAME_ARCHIVE1="${GAME_ES_ARCHIVE1}"
-	GAME_ARCHIVE1_MD5="${GAME_ES_ARCHIVE1_MD5}"
-	GAME_ARCHIVE2="${GAME_ES_ARCHIVE2}"
-	GAME_ARCHIVE2_MD5="${GAME_ES_ARCHIVE2_MD5}"
-	GAME_ARCHIVE3="${GAME_ES_ARCHIVE3}"
-	GAME_ARCHIVE3_MD5="${GAME_ES_ARCHIVE3_MD5}"
-	CLIENT_ARCHIVE2="${CLIENT_ES_ARCHIVE2}"
-	CLIENT_ARCHIVE2_URL="${CLIENT_ES_ARCHIVE2_URL}"
-	CLIENT_ARCHIVE2_MD5="${CLIENT_ES_ARCHIVE2_MD5}"
-	CLIENT_ARCHIVE3="${CLIENT_ES_ARCHIVE3}"
-	CLIENT_ARCHIVE3_URL="${CLIENT_ES_ARCHIVE3_URL}"
-	CLIENT_ARCHIVE3_MD5="${CLIENT_ES_ARCHIVE3_MD5}"
-	PKG1_VERSION="${PKG_ES_VERSION}"
-fi
+case "${GAME_LANG}" in
+	'en') GAME_ARCHIVE1="${GAME_EN_ARCHIVE1}"
+		GAME_ARCHIVE1_MD5="${GAME_EN_ARCHIVE1_MD5}"
+		GAME_ARCHIVE2="${GAME_EN_ARCHIVE2}"
+		GAME_ARCHIVE2_MD5="${GAME_EN_ARCHIVE2_MD5}"
+		GAME_ARCHIVE3="${GAME_EN_ARCHIVE3}"
+		GAME_ARCHIVE3_MD5="${GAME_EN_ARCHIVE3_MD5}"
+		CLIENT_ARCHIVE2="${CLIENT_EN_ARCHIVE2}"
+		CLIENT_ARCHIVE2_URL="${CLIENT_EN_ARCHIVE2_URL}"
+		CLIENT_ARCHIVE2_MD5="${CLIENT_EN_ARCHIVE2_MD5}"
+		CLIENT_ARCHIVE3="${CLIENT_EN_ARCHIVE3}"
+		CLIENT_ARCHIVE3_URL="${CLIENT_EN_ARCHIVE3_URL}"
+		CLIENT_ARCHIVE3_MD5="${CLIENT_EN_ARCHIVE3_MD5}"
+		PKG1_VERSION="${PKG1_EN_VERSION}" ;;
+	'es') GAME_ARCHIVE1="${GAME_ES_ARCHIVE1}"
+		GAME_ARCHIVE1_MD5="${GAME_ES_ARCHIVE1_MD5}"
+		GAME_ARCHIVE2="${GAME_ES_ARCHIVE2}"
+		GAME_ARCHIVE2_MD5="${GAME_ES_ARCHIVE2_MD5}"
+		GAME_ARCHIVE3="${GAME_ES_ARCHIVE3}"
+		GAME_ARCHIVE3_MD5="${GAME_ES_ARCHIVE3_MD5}"
+		CLIENT_ARCHIVE2="${CLIENT_ES_ARCHIVE2}"
+		CLIENT_ARCHIVE2_URL="${CLIENT_ES_ARCHIVE2_URL}"
+		CLIENT_ARCHIVE2_MD5="${CLIENT_ES_ARCHIVE2_MD5}"
+		CLIENT_ARCHIVE3="${CLIENT_ES_ARCHIVE3}"
+		CLIENT_ARCHIVE3_URL="${CLIENT_ES_ARCHIVE3_URL}"
+		CLIENT_ARCHIVE3_MD5="${CLIENT_ES_ARCHIVE3_MD5}"
+		PKG1_VERSION="${PKG1_ES_VERSION}";;
+esac
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
 
 game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
-PATH_DESK='/usr/local/share/applications'
-PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 PATH_BIN="${PKG_PREFIX}/games"
+PATH_DESK='/usr/local/share/applications'
 PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
+PATH_ICON_BASE="/usr/local/share/icons/hicolor"
 
 printf '\n'
 set_target '1' 'gog.com'
@@ -252,7 +265,7 @@ set_target_extra 'CLIENT_ARCHIVE1' "${CLIENT_ARCHIVE1_URL}" "${CLIENT_ARCHIVE1}"
 set_target_extra 'CLIENT_ARCHIVE2' "${CLIENT_ARCHIVE2_URL}" "${CLIENT_ARCHIVE2}"
 set_target_extra 'CLIENT_ARCHIVE3' "${CLIENT_ARCHIVE3_URL}" "${CLIENT_ARCHIVE3}"
 if [ "$WITH_MOVIES" = '1' ]; then
-	set_target_extra 'CLIENT_ARCHIVE_MOVIES' "${CLIENT_ARCHIVE_MOVIES_URL}" "${CLIENT_ARCHIVE_MOVIES}" 
+	set_target_extra 'CLIENT_ARCHIVE_MOVIES' "${CLIENT_ARCHIVE_MOVIES_URL}" "${CLIENT_ARCHIVE_MOVIES}"
 fi
 printf '\n'
 
@@ -267,9 +280,7 @@ if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
 	checksum "${CLIENT_ARCHIVE1}" 'quiet' "${CLIENT_ARCHIVE1_MD5}"
 	checksum "${CLIENT_ARCHIVE2}" 'quiet' "${CLIENT_ARCHIVE2_MD5}"
 	checksum "${CLIENT_ARCHIVE3}" 'quiet' "${CLIENT_ARCHIVE3_MD5}"
-	if [ "$WITH_MOVIES" = '1' ]; then
-		checksum "${CLIENT_ARCHIVE_MOVIES}" 'quiet' "${CLIENT_ARCHIVE_MOVIES_MD5}"
-	fi
+	[ "$WITH_MOVIES" = '1' ] && checksum "${CLIENT_ARCHIVE_MOVIES}" 'quiet' "${CLIENT_ARCHIVE_MOVIES_MD5}"
 	print done
 fi
 
@@ -277,53 +288,71 @@ fi
 
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}"
 print wait
+
 extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
+
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
 for file in ${INSTALLER_DOC}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
 done
+
 for file in ${INSTALLER_GAME}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}"
 done
-for dir in 'portraits' 'saves' 'servervault'; do
-	mkdir "${PKG1_DIR}${PATH_GAME}/${dir}"
-done
-extract_data 'tar' "${CLIENT_ARCHIVE1}" "${PKG1_DIR}${PATH_GAME}" 'quiet,fix_rights'
-extract_data 'tar' "${CLIENT_ARCHIVE2}" "${PKG1_DIR}${PATH_GAME}" 'quiet,fix_rights'
-extract_data 'tar' "${CLIENT_ARCHIVE3}" "${PKG1_DIR}${PATH_GAME}" 'quiet,fix_rights'
-rm "${PKG1_DIR}${PATH_GAME}/lib"/*
-mv "${PKG1_DIR}${PATH_GAME}"/*.txt "${PKG1_DIR}${PATH_DOC}"
-mv "${PKG_TMPDIR}/app/nwncdkey.ini" "${PKG1_DIR}${PATH_GAME}"
-touch "${PKG1_DIR}${PATH_GAME}/lib/libtxc_dxtn.so"
+cd - > /dev/null
+
+cd "${PKG1_DIR}${PATH_GAME}"
+extract_data 'tar' "${CLIENT_ARCHIVE1}" . 'quiet,fix_rights'
+extract_data 'tar' "${CLIENT_ARCHIVE2}" . 'quiet,fix_rights'
+extract_data 'tar' "${CLIENT_ARCHIVE3}" . 'quiet,fix_rights'
+
+mv "${PKG_TMPDIR}/app/nwncdkey.ini" .
+mv *.txt "${PKG1_DIR}${PATH_DOC}"
+
+rm lib/*
+touch 'lib/libtxc_dxtn.so'
+
 for file in 'nwmain' 'dmclient' 'nwn' 'nwserver' 'fixinstall'; do
-	chmod 755 "${PKG1_DIR}${PATH_GAME}/${file}"
+	chmod 755 "${file}"
 done
+
+for dir in 'portraits' 'saves' 'servervault'; do
+	mkdir "${dir}"
+done
+cd - > /dev/null
+
 if [ "${NO_ICON}" = '0' ]; then
 	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
 fi
 rm "${PKG1_DIR}${PATH_GAME}/${APP1_ICON}"
-rm -Rf "${PKG_TMPDIR}"
 
 # Building nwmovies
 
 if [ "${WITH_MOVIES}" = '1' ]; then
 	extract_data 'tar' "${CLIENT_ARCHIVE_MOVIES}" "${PKG1_DIR}${PATH_GAME}" 'quiet'
 	cd "${PKG1_DIR}${PATH_GAME}"
+
 	# Build nwmovies
 	printf '%s NWMovies…\n' "$(l10n 'build_generic')"
 	./nwmovies_install.pl build > /dev/null 2>&1
 	sed -i 's/mpv /mpv --fs --no-osc /' ./nwmovies/nwplaymovie
+
 	# Cleanup what is no longer required
 	for file in '*.c' '*.h' '*.S' '*.pl' '*.map'; do
 		rm -f ./${file} nwmovies/${file} nwmovies/libdis/${file}
 	done
+
 	# Patch the launcher
 	mv ./nwn ./nwn.real
-	sed 's#./nwmain $@#export LD_PRELOAD=./nwmovies.so\n./nwmain $@#' ./nwn.real > ./nwn.nwmovies
+	sed 's|./nwmain $@|export LD_PRELOAD=./nwmovies.so\n./nwmain $@|' ./nwn.real > ./nwn.nwmovies
 	chmod 755 ./nwn.nwmovies
 	ln -s ./nwn.nwmovies ./nwn
+
 	cd - > /dev/null
 	PKG1_DEPS="${PKG1_DEPS}, mpv:amd64|mpv"
 fi
+
+rm -Rf "${PKG_TMPDIR}"
 print done
 
 # Write launchers
@@ -337,6 +366,7 @@ printf '\n'
 
 write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
