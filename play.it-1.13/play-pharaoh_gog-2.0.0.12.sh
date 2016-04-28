c1ccaa300e09ebac7250f71b67f7f33b2f515730
vv221 <vv221@dotslashplay.it>
2016-04-28 18:14:05 +0200

    1.13 - Pharaoh (GOG) full rewrite

--- play.it-1.13/play-pharaoh_gog-2.0.0.12.sh
+++ play.it-1.13/play-pharaoh_gog-2.0.0.12.sh
@@ -30,30 +30,41 @@
 ###
 # conversion script for the Pharaoh + Cleopatra installer sold on GOG.com
 # build a .deb package from the Windows installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20151127.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160428.1
 
 # Set game-specific variables
 
+SCRIPT_DEPS_HARD='fakeroot realpath innoextract'
+SCRIPT_DEPS_SOFT='icotool wrestool'
+
 GAME_ID='pharaoh'
-GAME_ID_SHORT="${GAME_ID}"
+GAME_ID_SHORT='pharaoh'
+GAME_NAME='Pharaoh'
 
 GAME_ARCHIVE1='setup_pharaoh_gold_2.0.0.12.exe'
 GAME_ARCHIVE1_MD5='04631d3942f7e16fd5d96bf2238c0528'
 GAME_ARCHIVE_FULLSIZE='810000'
-PKG_ORIGIN='gog'
-PKG_REVISION='2.0.0.12'
+PKG_REVISION='gog2.0.0.12'
+
+GAME_TRADFR_ARCHIVE1='pharaonfr-textes.7z'
+GAME_TRADFR_ARCHIVE1_MD5='609fd40b7cb7202583698478deb320c8'
+GAME_TRADFR_ARCHIVE2='pharaonfr-voix.7z'
+GAME_TRADFR_ARCHIVE2_MD5='0eed85eeebf06e7d62a371d1f4dd9a59'
+GAME_TRADFR_ARCHIVE3='pharaonfr-videos.7z'
+GAME_TRADFR_ARCHIVE3_MD5='561954174d2a4e918babf4a4d12cad1b'
+
+INSTALLER_DOC='app/mission?editor?guide.txt app/readme.txt app/*.pdf tmp/gog_eula.txt tmp/eula.txt'
+INSTALLER_GAME='app/*'
 
 GAME_CACHE_DIRS=''
 GAME_CACHE_FILES=''
 GAME_CACHE_FILES_POST=''
 GAME_CONFIG_DIRS=''
-GAME_CONFIG_FILES='./pharaoh.ini'
+GAME_CONFIG_FILES='./*.ini'
 GAME_CONFIG_FILES_POST=''
 GAME_DATA_DIRS='./save'
 GAME_DATA_FILES='./*.inf ./*.txt'
@@ -65,7 +76,7 @@ APP1_ID="${GAME_ID}"
 APP1_EXE='./pharaoh.exe'
 APP1_ICON='./pharaoh.exe'
 APP1_ICON_RES='16x16 32x32'
-APP1_NAME='Pharaoh'
+APP1_NAME="${GAME_NAME}"
 APP1_NAME_FR='Pharaon'
 APP1_CAT='Game'
 
@@ -75,29 +86,27 @@ PKG1_ARCH='i386'
 PKG1_CONFLICTS=''
 PKG1_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
 PKG1_RECS=''
-PKG1_DESC='Pharaoh + Cleopatra'
-
-TRADFR_URL='http://www.dotslashplay.it/ressources/pharaoh/'
-TRADFR_ARCHIVE1='pharaonfr-full.7z'
-TRADFR_ARCHIVE1_MD5='ed8287056cf9c321f91dcac81ab79c96'
-TRADFR_ARCHIVE2='pharaonfr-light.7z'
-TRADFR_ARCHIVE2_MD5='d3232bddc2b129ac610ba0387a3317a9'
-TRADFR_ARCHIVE3='pharaonfr-ultralight.7z'
-TRADFR_ARCHIVE3_MD5='588c04debd0afd698701556ffebbca9b'
+PKG1_DESC="${GAME_NAME}
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
 TARGET_LIB_VERSION='1.13'
+
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
 fi
+
 if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'play-anything.sh not found.\n'
 	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
+
 LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'Wrong version of play-anything.\n'
@@ -105,29 +114,34 @@ if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt
 	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
+
 . "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
-GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT='en'
-WITH_MOVIES_DEFAULT=''
+NO_ICON=0
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+GAME_LANG_DEFAULT='en'
+
 fetch_args "$@"
-check_deps 'fakeroot innoextract' 'wrestool icotool'
-if [ "${GAME_LANG}" = 'fr' ]; then
-	check_deps_7z
-fi
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
 set_lang
+
+if [ "${GAME_LANG}" = 'fr' ]; then
+	SCRIPT_DEPS_HARD="${SCRIPT_DEPS_HARD} 7z"
+fi
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
@@ -138,7 +152,9 @@ PATH_ICON_BASE="/usr/local/share/icons/hicolor"
 printf '\n'
 set_target '1' 'gog.com'
 if [ "${GAME_LANG}" = 'fr' ]; then
-	set_target_extra 'TRADFR_ARCHIVE' "${TRADFR_URL}" "${TRADFR_ARCHIVE1}" "${TRADFR_ARCHIVE2}" "${TRADFR_ARCHIVE3}"
+	set_target_optional 'TRADFR_ARCHIVE1' "${GAME_TRADFR_ARCHIVE1}"
+	set_target_optional 'TRADFR_ARCHIVE2' "${GAME_TRADFR_ARCHIVE2}"
+	set_target_optional 'TRADFR_ARCHIVE3' "${GAME_TRADFR_ARCHIVE3}"
 fi
 printf '\n'
 
@@ -149,7 +165,9 @@ if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
 	print wait
 	checksum "${GAME_ARCHIVE}" 'quiet' "${GAME_ARCHIVE1_MD5}"
 	if [ "${GAME_LANG}" = 'fr' ]; then
-		checksum "${TRADFR_ARCHIVE}" 'quiet' "${TRADFR_ARCHIVE1_MD5}" "${TRADFR_ARCHIVE2_MD5}" "${TRADFR_ARCHIVE3_MD5}"
+		[ -n "${TRADFR_ARCHIVE1}" ] && checksum "${TRADFR_ARCHIVE1}" 'quiet' "${GAME_TRADFR_ARCHIVE1_MD5}"
+		[ -n "${TRADFR_ARCHIVE2}" ] && checksum "${TRADFR_ARCHIVE2}" 'quiet' "${GAME_TRADFR_ARCHIVE2_MD5}"
+		[ -n "${TRADFR_ARCHIVE3}" ] && checksum "${TRADFR_ARCHIVE3}" 'quiet' "${GAME_TRADFR_ARCHIVE3_MD5}"
 	fi
 	print done
 fi
@@ -157,19 +175,30 @@ fi
 # Extract game data
 
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_GAME}"
-printf '\n%s…\n' "$(l10n 'extract_data_generic')"
 print wait
+
 extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
+
 if [ "${GAME_LANG}" = 'fr' ]; then
-	extract_data '7z' "${TRADFR_ARCHIVE}" "${PKG_TMPDIR}/app" 'force,quiet'
+	[ -n "${TRADFR_ARCHIVE1}" ] && extract_data '7z' "${TRADFR_ARCHIVE1}" "${PKG_TMPDIR}/app" 'force,quiet'
+	[ -n "${TRADFR_ARCHIVE2}" ] && extract_data '7z' "${TRADFR_ARCHIVE2}" "${PKG_TMPDIR}/app" 'force,quiet'
+	[ -n "${TRADFR_ARCHIVE3}" ] && extract_data '7z' "${TRADFR_ARCHIVE3}" "${PKG_TMPDIR}/app" 'force,quiet'
 fi
-for file in 'app/mission*editor*guide.txt' 'app/readme.txt' 'app/*.pdf' 'tmp/gog_eula.txt' 'tmp/eula.txt'; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+
+cd "${PKG_TMPDIR}"
+for file in ${INSTALLER_DOC}; do
+	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
 done
-mv "${PKG_TMPDIR}/app"/* "${PKG1_DIR}${PATH_GAME}"
+
+for file in ${INSTALLER_GAME}; do
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
+
 if [ "${NO_ICON}" = '0' ]; then
 	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
 fi
+
 rm -rf "${PKG_TMPDIR}"
 print done
 
@@ -183,8 +212,9 @@ printf '\n'
 
 # Build package
 
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
