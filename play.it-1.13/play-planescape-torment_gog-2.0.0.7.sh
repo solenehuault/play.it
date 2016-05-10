6d2de31f8c9c5a6faab0042bca8a749c37a55112
vv221 <vv221@dotslashplay.it>
2016-05-10 21:51:07 +0200

    1.13 - Planescape Torment (GOG) full rewrite build separate packages for language-specific data

--- play.it-1.13/play-planescape-torment_gog-2.0.0.7.sh
+++ play.it-1.13/play-planescape-torment_gog-2.0.0.7.sh
@@ -30,17 +30,15 @@
 ###
 # conversion script for the Planescape: Torment installer sold on GOG.com
 # build a .deb package from the .sh MojoSetup installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20151224.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160510.1
 
 # Setting game-specific variables
 
-SCRIPT_DEPS_HARD='fakeroot realpath innoextract'
+SCRIPT_DEPS_HARD='fakeroot realpath unzip'
 SCRIPT_DEPS_SOFT='icotool wrestool'
 
 GAME_ID='planescape-torment'
@@ -54,9 +52,12 @@ GAME_ARCHIVE2='gog_planescape_torment_french_2.0.0.7.sh'
 GAME_ARCHIVE2_MD5='1086f8a44d766c243d33438f9bd9ff40'
 GAME_ARCHIVE_FULLSIZE='2500000'
 
-INSTALLER_JUNK='data/noarch/prefix/drive_c/gog?games/*/*ddraw* data/noarch/prefix/drive_c/gog?games/*/gog_planescape_torment.sdb'
-INSTALLER_DOC='data/noarch/docs/*'
-INSTALLER_GAME='data/noarch/prefix/drive_c/gog?games/*/*'
+INSTALLER_DOC_PATH='data/noarch/docs'
+INSTALLER_DOC='*'
+INSTALLER_GAME_PATH='data/noarch/prefix/drive_c/gog?games/*'
+INSTALLER_GAME_JUNK='*ddraw* gog_planescape_torment.sdb torment.err torment.log'
+INSTALLER_GAME_PKG1='./*.tlk ./cachemos.bif ./chitin.key ./crefiles.bif ./cs_0404.bif ./interface.bif ./sound.bif ./torment.ini ./voice.bif data/genmova.bif data/movies2.bif data/movies4.bif'
+INSTALLER_GAME_PKG2='*'
 
 GAME_CACHE_DIRS='./cache'
 GAME_CACHE_FILES=''
@@ -78,27 +79,47 @@ APP1_NAME="${GAME_NAME}"
 APP1_NAME_FR="${GAME_NAME}"
 APP1_CAT='Game'
 
+PKG_VERSION='1.1'
+
 PKG1_ID="${GAME_ID}"
-PKG1_VERSION='1.1'
+PKG1_VERSION="${PKG_VERSION}"
 PKG1_ARCH='i386'
 PKG1_CONFLICTS=''
 PKG1_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
 PKG1_RECS=''
-PKG1_DESC="${GAME_NAME}"
+PKG1_DESC="${GAME_NAME}
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
+
+PKG2_ID="${GAME_ID}-common"
+PKG2_VERSION="${PKG_VERSION}"
+PKG2_ARCH='all'
+PKG2_CONFLICTS=''
+PKG2_DEPS=''
+PKG2_RECS=''
+PKG2_DESC="${GAME_NAME} - common data
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
+
+PKG1_DEPS="${PKG2_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG1_DEPS}"
 
 # Load common functions
 
 TARGET_LIB_VERSION='1.13'
+
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
 fi
+
 if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'play-anything.sh not found.\n'
 	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
+
 LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'Wrong version of play-anything.\n'
@@ -106,34 +127,42 @@ if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt
 	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
+
 . "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
-GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT=''
-WITH_MOVIES_DEFAULT=''
+NO_ICON=0
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+
 fetch_args "$@"
-check_deps "${SCRIPT_DEPS_HARD}" "${SCRIPT_DEPS_SOFT}"
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
 PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON_BASE="/usr/local/share/icons/hicolor"
+PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 
 printf '\n'
 set_target '2' 'gog.com'
+case "$(basename ${GAME_ARCHIVE})" in
+	"${GAME_ARCHIVE1}") PKG1_DIR="${PKG1_DIR%/*}/${PKG1_ID}-en_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" ;;
+	"${GAME_ARCHIVE2}") PKG1_DIR="${PKG1_DIR%/*}/${PKG1_ID}-fr_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" ;;
+esac
 printf '\n'
 
 # Check target files integrity
@@ -144,24 +173,43 @@ fi
 
 # Extract game data
 
-build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_GAME}"
+build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_GAME}"
 print wait
+
 extract_data 'mojo' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet,tolower'
-for file in ${INSTALLER_JUNK}; do
-	rm -rf "${PKG_TMPDIR}"/${file}
-done
+
+cd "${PKG_TMPDIR}/${INSTALLER_DOC_PATH}"
 for file in ${INSTALLER_DOC}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+	mv "${file}" "${PKG2_DIR}${PATH_DOC}"
 done
-for file in ${INSTALLER_GAME}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+cd - > /dev/null
+
+cd "${PKG_TMPDIR}"/${INSTALLER_GAME_PATH}
+for file in ${INSTALLER_GAME_JUNK}; do
+	rm -rf "${file}"
 done
+
+for file in ${INSTALLER_GAME_PKG1}; do
+	mkdir -p "${PKG1_DIR}${PATH_GAME}/${file%/*}"
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}/${file}"
+done
+
+for file in ${INSTALLER_GAME_PKG2}; do
+	mv "${file}" "${PKG2_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
+
+if [ "${NO_ICON}" = '0' ]; then
+	PKG1_DIR_REAL="${PKG1_DIR}"
+	PKG1_DIR="${PKG2_DIR}"
+	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
+	PKG1_DIR="${PKG1_DIR_REAL}"
+fi
+
 file="${PKG1_DIR}${PATH_GAME}/torment.ini"
 sed -i 's/HD0:=.\+/HD0:=C:\\planescape-torment\\/' "${file}"
 sed -i 's/CD\([1-5]\):=.\+/CD\1:=C:\\planescape-torment\\data\\/' "${file}"
-if [ "${NO_ICON}" = '0' ]; then
-	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
-fi
+
 rm -rf "${PKG_TMPDIR}"
 print done
 
@@ -175,9 +223,17 @@ printf '\n'
 
 # Build package
 
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
-build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
-print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
+printf '%s…\n' "$(l10n 'build_pkgs')"
+print wait
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}"
+
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet'
+build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet'
+print done
+
+print_instructions "${PKG1_DESC}" "${PKG2_DIR}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
 exit 0
