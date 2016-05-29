5bbfbb671b585089376957b92d118bd0fdda7068
vv221 <vv221@dotslashplay.it>
2016-05-30 00:15:58 +0200

    1.13 - Don’t Starve: Reign of Giants (GOG) - common arch-independant package

--- play.it-1.13/play-dont-starve-reign-of-giants_gog-2.0.0.3.sh
+++ play.it-1.13/play-dont-starve-reign-of-giants_gog-2.0.0.3.sh
@@ -28,37 +28,38 @@
 ###
 
 ###
-# conversion script for the Don’t Starve:  Reign Of Giants installer sold on GOG.com
-# build a .deb package from the .sh MojoSetup installer
-# tested on Debian, should work on any .deb-based distribution
+# conversion script for the the Don’t Starve:  Reign Of Giants installer sold on GOG.com
+# build a .deb package from the MojoSetup (.sh) installer
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
 
-script_version=20160210.1
+script_version=20160530.1
 
 # Set game-specific variables
 
 SCRIPT_DEPS_HARD='fakeroot realpath unzip'
 
 GAME_ID='dont-starve'
-GAME_ID_SHORT='dontstarve'
-GAME_NAME='Don’t Starve'
+GAME_ID_SHORT='dstarve'
+GAME_NAME='Don’t Starve - Reign Of Giants'
 
 GAME_ARCHIVE1='gog_don_t_starve_reign_of_giants_dlc_2.0.0.3.sh'
 GAME_ARCHIVE1_MD5='bd505adc70ed478a92669bc8c1c3a127'
 GAME_ARCHIVE_FULLSIZE='400000'
 PKG_REVISION='gog2.0.0.3'
 
-INSTALLER_DOC='data/noarch/docs/*'
-INSTALLER_GAME_ARCH1='data/noarch/game/dontstarve32/*'
-INSTALLER_GAME_ARCH2='data/noarch/game/dontstarve64/*'
+INSTALLER_PATH='data/noarch'
+INSTALLER_DOC='docs/*'
+INSTALLER_GAME_PKG1='game/dontstarve32/manifest_dlc0001.json'
+INSTALLER_GAME_PKG2='game/dontstarve64/manifest_dlc0001.json'
+INSTALLER_GAME_PKG3='game/dontstarve32/data'
+
+APP_COMMON_ID="${GAME_ID_SHORT}-common.sh"
 
 PKG_ID="${GAME_ID}-dlc1"
 PKG_VERSION='1'
-PKG_DEPS="${GAME_ID} (>= 2015.04.28)"
-PKG_RECS=''
+PKG_DEPS="${GAME_ID} (>= 2016.04.01)"
 PKG_DESC="${GAME_NAME}
  package built from GOG.com installer
  ./play.it script version ${script_version}"
@@ -66,19 +67,34 @@ PKG_DESC="${GAME_NAME}
 PKG1_ID="${PKG_ID}"
 PKG1_ARCH='i386'
 PKG1_VERSION="${PKG_VERSION}"
+PKG1_CONFLICTS=''
 PKG1_DEPS="${PKG_DEPS}"
-PKG1_RECS="${PKG_RECS}"
+PKG1_RECS=''
 PKG1_DESC="${PKG_DESC}"
 
 PKG2_ID="${PKG_ID}"
 PKG2_ARCH='amd64'
 PKG2_VERSION="${PKG_VERSION}"
+PKG2_CONFLICTS=''
 PKG2_DEPS="${PKG_DEPS}"
-PKG2_RECS="${PKG_RECS}"
+PKG2_RECS=''
 PKG2_DESC="${PKG_DESC}"
+
+PKG3_ID="${PKG_ID}-common"
+PKG3_ARCH='all'
+PKG3_VERSION="${PKG_VERSION}"
+PKG3_CONFLICTS=''
+PKG3_DEPS=''
+PKG3_RECS=''
+PKG3_DESC="${GAME_NAME} - arch-independant data
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
 
 PKG1_CONFLICTS="${PKG2_ID}:${PKG2_ARCH}"
 PKG2_CONFLICTS="${PKG1_ID}:${PKG1_ARCH}"
+
+PKG1_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG1_DEPS}"
+PKG2_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG2_DEPS}"
 
 # Load common functions
 
@@ -124,17 +140,16 @@ check_deps_hard ${SCRIPT_DEPS_HARD}
 game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG3_DIR' "${PKG3_ID}_${PKG3_VERSION}-${PKG_REVISION}_${PKG3_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
-PATH_BIN="${PKG_PREFIX}/games"
-PATH_DESK='/usr/local/share/applications'
-PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
+#PATH_BIN="${PKG_PREFIX}/games"
+PATH_DOC="/usr/local/share/doc/${GAME_ID}"
+#PATH_DESK='/usr/local/share/applications'
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON="/usr/local/share/icons/hicolor/${APP1_ICON_RES}/apps"
+#PATH_ICON_BASE="/usr/local/share/icons/hicolor"
 
 printf '\n'
-
 set_target '1' 'gog.com'
-
 printf '\n'
 
 # Check target files integrity
@@ -145,45 +160,52 @@ fi
 
 # Extract game data
 
-build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON}"
+#PATH_ICON="${PATH_ICON_BASE}/${APP1_ICON_RES}/apps"
+build_pkg_dirs '2' "${PATH_GAME}"
+rm -rf "${PKG3_DIR}"
+mkdir -p "${PKG3_DIR}/DEBIAN" "${PKG3_DIR}${PATH_DOC}" "${PKG3_DIR}${PATH_GAME}"
+print wait
 
 extract_data 'mojo' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
 
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
 for file in ${INSTALLER_DOC}; do
-	cp -rl "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
-	cp -rl "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_DOC}"
+	mv "${file}" "${PKG3_DIR}${PATH_DOC}"
 done
 
-for file in ${INSTALLER_GAME_ARCH1}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+for file in ${INSTALLER_GAME_PKG1}; do
+  mv "${file}" "${PKG1_DIR}${PATH_GAME}"
 done
 
-for file in ${INSTALLER_GAME_ARCH2}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG2_DIR}${PATH_GAME}"
+for file in ${INSTALLER_GAME_PKG2}; do
+  mv "${file}" "${PKG2_DIR}${PATH_GAME}"
 done
+
+for file in ${INSTALLER_GAME_PKG3}; do
+	mv "${file}" "${PKG3_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
 
 rm -rf "${PKG_TMPDIR}"
-
 print done
 
-# Build packages
+# Build package
 
 printf '%s…\n' "$(l10n 'build_pkgs')"
-
 print wait
 
 write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}" 'arch'
 write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}" 'arch'
+write_pkg_debian "${PKG3_DIR}" "${PKG3_ID}" "${PKG3_VERSION}-${PKG_REVISION}" "${PKG3_ARCH}" "${PKG3_CONFLICTS}" "${PKG3_DEPS}" "${PKG3_RECS}" "${PKG3_DESC}"
 
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG1_ARCH}"
 build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG2_ARCH}"
-
+build_pkg "${PKG3_DIR}" "${PKG3_DESC}" "${PKG_COMPRESSION}" 'quiet'
 print done
 
-print_instructions "$(printf '%s' "${PKG1_DESC}" | head -n1) (${PKG1_ARCH})" "${PKG1_DIR}"
+print_instructions "$(printf '%s' "${PKG1_DESC}" | head -n1) (${PKG1_ARCH})" "${PKG3_DIR}" "${PKG1_DIR}"
 printf '\n'
-print_instructions "$(printf '%s' "${PKG2_DESC}" | head -n1) (${PKG2_ARCH})" "${PKG2_DIR}"
-
+print_instructions "$(printf '%s' "${PKG2_DESC}" | head -n1) (${PKG2_ARCH})" "${PKG3_DIR}" "${PKG2_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
 exit 0
