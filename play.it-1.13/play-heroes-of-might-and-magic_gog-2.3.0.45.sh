7da7fa0e9c717b20274273759752fa9a773cb46e
vv221 <vv221@dotslashplay.it>
2016-06-25 13:48:34 +0200

    1.13 - Heroes of Might and Magic (GOG) - new installer + dedicated package for language-specific files

--- play.it-1.13/play-heroes-of-might-and-magic_gog-2.3.0.45.sh
+++ play.it-1.13/play-heroes-of-might-and-magic_gog-2.3.0.45.sh
@@ -34,7 +34,7 @@
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20160331.1
+script_version=20160625.1
 
 # Set game-specific variables
 
@@ -46,14 +46,17 @@ GAME_ID_SHORT='homm1'
 GAME_NAME='Heroes of Might and Magic'
 GAME_NAME_SHORT='HoMM'
 
-GAME_ARCHIVE1='setup_heroes_of_might_and_magic_2.1.0.38.exe'
-GAME_ARCHIVE1_MD5='c6eca5782226db2960cbdc9a03038b06'
-GAME_ARCHIVE_FULLSIZE='540000'
-PKG_REVISION='gog2.1.0.38'
+GAME_ARCHIVE1='setup_heroes_of_might_and_magic_2.3.0.45.exe'
+GAME_ARCHIVE1_MD5='2cae1821085090e30e128cd0a76b0d21'
+GAME_ARCHIVE2='setup_heroes_of_might_and_magic_french_2.3.0.45.exe'
+GAME_ARCHIVE2_MD5='9ec736a2a1b97dc36257f583f42864ac'
+GAME_ARCHIVE_FULLSIZE='530000'
+PKG_REVISION='gog2.3.0.45'
 
-INSTALLER_JUNK='app/dosbox* app/gameuxinstallhelper.dll app/goggame*.dll app/goggame*.info app/gog.ico app/__support app/support.ico app/webcache.zip'
-INSTALLER_DOC='app/help app/*.pdf app/*.txt tmp/gog_eula.txt tmp/eula.txt'
-INSTALLER_GAME='app/*'
+INSTALLER_PATH='app'
+INSTALLER_DOC='help *.pdf *.txt ../tmp/gog_eula.txt ../tmp/eula.txt'
+INSTALLER_GAME_PKG1='data/campaign.hs data/heroes.agg data/standard.hs ./*.exe ./games ./maps'
+INSTALLER_GAME_PKG2='data goggame-1207658748.ico heroes.cfg homm1.gog wail32.dll ../sys/wing32.dll'
 
 GAME_CACHE_DIRS=''
 GAME_CACHE_FILES=''
@@ -75,7 +78,7 @@ MENU_CAT='Games'
 
 APP1_ID="${GAME_ID}"
 APP1_EXE='./heroes.exe'
-APP1_ICON='./gfw_high.ico'
+APP1_ICON='./goggame-1207658748.ico'
 APP1_ICON_RES='16x16 32x32 48x48 256x256'
 APP1_NAME="${GAME_NAME_SHORT} - A Strategic Quest"
 APP1_NAME_FR="${APP1_NAME}"
@@ -85,15 +88,30 @@ APP2_EXE='./editor.exe'
 APP2_NAME="${GAME_NAME_SHORT} - editor"
 APP2_NAME_FR="${GAME_NAME_SHORT} - éditeur"
 
+PKG_VERSION='1.0'
+PKG_ARCH='all'
+
 PKG1_ID="${GAME_ID}"
-PKG1_VERSION='1.0'
-PKG1_ARCH='all'
+PKG1_VERSION="${PKG_VERSION}"
+PKG1_ARCH="${PKG_ARCH}"
 PKG1_CONFLICTS=''
 PKG1_DEPS='dosbox'
 PKG1_RECS=''
 PKG1_DESC="${GAME_NAME}
- package built from GOG.com Windows installer
+ package built from GOG.com installer
  ./play.it script version ${script_version}"
+
+PKG2_ID="${GAME_ID}-common"
+PKG2_VERSION="${PKG_VERSION}"
+PKG2_ARCH="${PKG_ARCH}"
+PKG2_CONFLICTS=''
+PKG2_DEPS=''
+PKG2_RECS=''
+PKG2_DESC="${GAME_NAME} - common data
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
+
+PKG1_DEPS="${PKG2_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG1_DEPS}"
 
 # Load common functions
 
@@ -141,6 +159,7 @@ check_deps_soft ${SCRIPT_DEPS_SOFT}
 
 game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
@@ -151,36 +170,48 @@ PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
 PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 
 printf '\n'
-set_target '1' 'gog.com'
+set_target '2' 'gog.com'
+case "$(basename ${GAME_ARCHIVE})" in
+	"${GAME_ARCHIVE1}") PKG1_DIR="${PKG1_DIR%/*}/${PKG1_ID}-en_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" ;;
+	"${GAME_ARCHIVE2}") PKG1_DIR="${PKG1_DIR%/*}/${PKG1_ID}-fr_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" ;;
+esac
 printf '\n'
 
 # Check target file integrity
 
 if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
-	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}"
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}" "${GAME_ARCHIVE2_MD5}"
 fi
 
 # Extract game data
 
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_DESK_DIR}" "${PATH_DESK_MERGED}" "${PATH_GAME}"
+rm -rf "${PKG2_DIR}"
+mkdir -p "${PKG2_DIR}/DEBIAN" "${PKG2_DIR}${PATH_GAME}" "${PKG2_DIR}${PATH_ICON_BASE}"
 print wait
 
 extract_data 'inno' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
 
-for file in ${INSTALLER_JUNK}; do
-	rm -rf "${PKG_TMPDIR}"/${file}
-done
-
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
 for file in ${INSTALLER_DOC}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
 done
 
-for file in ${INSTALLER_GAME}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+for file in ${INSTALLER_GAME_PKG1}; do
+	mkdir -p "${PKG1_DIR}${PATH_GAME}/${file%/*}"
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}/${file}"
 done
+
+for file in ${INSTALLER_GAME_PKG2}; do
+	mv "${file}" "${PKG2_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
 
 if [ "${NO_ICON}" = '0' ]; then
+	PKG1_DIR_REAL="${PKG1_DIR}"
+	PKG1_DIR="${PKG2_DIR}"
 	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
+	PKG1_DIR="${PKG1_DIR_REAL}"
 fi
 
 rm -rf "${PKG_TMPDIR}"
@@ -195,15 +226,16 @@ write_bin_dosbox "${PKG1_DIR}${PATH_BIN}/${APP2_ID}" "${APP2_EXE}" '' '' "${APP2
 write_menu "${GAME_ID}" "${MENU_NAME}" "${MENU_NAME_FR}" "${MENU_CAT}" "${PKG1_DIR}${PATH_DESK_DIR}/${GAME_ID}.directory" "${PKG1_DIR}${PATH_DESK_MERGED}/${GAME_ID}.menu" 'dosbox' "${APP1_ID}" "${APP2_ID}"
 write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" '' 'dosbox'
 write_desktop "${APP2_ID}" "${APP2_NAME}" "${APP2_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP2_ID}.desktop" '' 'dosbox'
-
 printf '\n'
 
 # Build package
 
 write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}"
 
 if [ "${NO_ICON}" = '0' ]; then
-	file="${PKG1_DIR}/DEBIAN/postinst"
+
+	file="${PKG2_DIR}/DEBIAN/postinst"
 	cat > "${file}" <<- EOF
 	#!/bin/sh -e
 	for res in ${APP1_ICON_RES}; do
@@ -212,10 +244,11 @@ if [ "${NO_ICON}" = '0' ]; then
 	done
 	exit 0
 	EOF
+
 	sed -i 's/  /\t/' "${file}"
 	chmod 755 "${file}"
 
-	file="${PKG1_DIR}/DEBIAN/prerm"
+	file="${PKG2_DIR}/DEBIAN/prerm"
 	cat > "${file}" <<- EOF
 	#!/bin/sh -e
 	for res in ${APP1_ICON_RES}; do
@@ -224,13 +257,17 @@ if [ "${NO_ICON}" = '0' ]; then
 	done
 	exit 0
 	EOF
+
 	sed -i 's/  /\t/' "${file}"
 	chmod 755 "${file}"
+
 fi
 
-build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'defaults'
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet'
+build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet'
+print done
 
-print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
+print_instructions "${PKG1_DESC}" "${PKG2_DIR}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
 exit 0
