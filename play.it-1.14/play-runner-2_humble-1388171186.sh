21e7289505c39dc20883f00d34bad55974873f0c
vv221 <vv221@dotslashplay.it>
2016-07-17 16:28:37 +0200

    1.14 - Runner 2 (Humble) - merge scripts for multiple architectures

--- play.it-1.14/play-runner-2_humble-1388171186.sh
+++ play.it-1.14/play-runner-2_humble-1388171186.sh
@@ -30,31 +30,30 @@
 ###
 # conversion script for the Runner 2 archive sold on HumbleBundle.com
 # build a .deb package from the MojoSetup installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20151122.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160717.1
 
 # Set game-specific variables
 
 SCRIPT_DEPS_HARD='fakeroot realpath'
-SCRIPT_DEPS_SOFT=''
 
 GAME_ID='runner-2'
 GAME_ID_SHORT='runner2'
-GAME_NAME='Runner2'
+GAME_NAME='Runner2: Future Legend of Rhythm Alien'
 
 GAME_ARCHIVE1='runner2_i386_1388171186.tar.gz'
 GAME_ARCHIVE1_MD5='ea105bdcd486879fb99889b87e90eed5'
+GAME_ARCHIVE2='runner2_amd64_1388171186.tar.gz'
+GAME_ARCHIVE2_MD5='2f7ccdb675a63a5fc152514682e97480'
 GAME_ARCHIVE_FULLSIZE='770000'
 PKG_REVISION='humble1388171186'
 
-INSTALLER_JUNK=''
-INSTALLER_DOC='runner2-1.0/README*'
-INSTALLER_GAME='runner2-1.0/runner2/*'
+INSTALLER_PATH='runner2-1.0/runner2'
+INSTALLER_DOC='../README*'
+INSTALLER_GAME='./*'
 
 APP1_ID="${GAME_ID}"
 APP1_EXE='./runner2'
@@ -66,26 +65,30 @@ APP1_CAT='Game'
 
 PKG1_ID="${GAME_ID}"
 PKG1_VERSION='1.0'
-PKG1_ARCH='i386'
 PKG1_CONFLICTS=''
 PKG1_RECS=''
-PKG1_DEPS='libc6, libstdc++6, libgcc1, zlib1g, libsdl1.2debian, libgl1-mesa-glx'
+PKG1_DEPS='libc6, libstdc++6, libgcc1, zlib1g, libsdl1.2debian, libgl1-mesa-glx | libgl1'
 PKG1_DESC="${GAME_NAME}
- package built from the HumbleBundle.com archive"
+ package built from HumbleBundle.com archive
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
-TARGET_LIB_VERSION='1.13'
+TARGET_LIB_VERSION='1.14'
+
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
 fi
+
 if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'play-anything.sh not found.\n'
 	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
+
 LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'Wrong version of play-anything.\n'
@@ -93,58 +96,66 @@ if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt
 	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
+
 . "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
 GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT=''
-WITH_MOVIES_DEFAULT=''
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 fetch_args "$@"
-check_deps "${SCRIPT_DEPS_HARD}" "${SCRIPT_DEPS_SOFT}"
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
 PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON="/usr/local/share/icons/hicolor/${APP1_ICON_RES}/apps"
+PATH_ICON_BASE="/usr/local/share/icons/hicolor"
 
 printf '\n'
-set_target '1' 'humblebundle.com'
+set_target '2' 'humblebundle.com'
+case "$(basename ${GAME_ARCHIVE})" in
+	"${GAME_ARCHIVE1}") PKG1_ARCH='i386' ;;
+	"${GAME_ARCHIVE2}") PKG1_ARCH='amd64' ;;
+esac
 printf '\n'
 
 # Check target file integrity
 
 if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
-	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}"
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}" "${GAME_ARCHIVE2_MD5}"
 fi
 
 # Extract game data
 
+PATH_ICON="${PATH_ICON_BASE}/${APP1_ICON_RES}/apps"
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON}"
 print wait
+
 extract_data 'tar' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
-for file in ${INSTALLER_JUNK}; do
-	rm -rf "${PKG_TMPDIR}"/${file}
-done
+
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
 for file in ${INSTALLER_DOC}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
 done
+
 for file in ${INSTALLER_GAME}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}"
 done
+cd - > /dev/null
+
 chmod 755 "${PKG1_DIR}${PATH_GAME}"/${APP1_EXE}
-ln -s "${PATH_GAME}/${APP1_ICON}" "${PKG1_DIR}${PATH_ICON}/${APP1_ID}.png"
+
 rm -rf "${PKG_TMPDIR}"
 print done
 
@@ -157,7 +168,25 @@ printf '\n'
 # Build package
 
 write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+
+file="${PKG1_DIR}/DEBIAN/postinst"
+cat > "${file}" << EOF
+#!/bin/sh -e
+ln -s "${PATH_GAME}/${APP1_ICON}" "${PATH_ICON}/${GAME_ID}.png"
+exit 0
+EOF
+chmod 755 "${file}"
+
+file="${PKG1_DIR}/DEBIAN/prerm"
+cat > "${file}" << EOF
+#!/bin/sh -e
+rm "${PATH_ICON}/${GAME_ID}.png"
+exit 0
+EOF
+chmod 755 "${file}"
+
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
