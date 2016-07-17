bdcc6d3deede3f239b15b032ad510632c14da8b6
vv221 <vv221@dotslashplay.it>
2016-07-17 16:16:12 +0200

    1.14 - Runner 2 (Humble) - import scripts from branch 1.13 import done from commit ac812b6ae7a81db43ec00d0018b386a2cc936202

---
+++ play.it-1.14/play-runner-2_humble-1388171186_amd64.sh
@@ -0,0 +1,164 @@
+#!/bin/sh -e
+
+###
+# Copyright (c) 2015-2016, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# conversion script for the Runner 2 archive sold on HumbleBundle.com
+# build a .deb package from the MojoSetup installer
+# tested on Debian, should work on any .deb-based distribution
+#
+# script version 20151122.1
+#
+# send your bug reports to vv221@dotslashplay.it
+# start the e-mail subject by "./play.it" to avoid it being flagged as spam
+###
+
+# Set game-specific variables
+
+SCRIPT_DEPS_HARD='fakeroot realpath'
+SCRIPT_DEPS_SOFT=''
+
+GAME_ID='runner-2'
+GAME_ID_SHORT='runner2'
+GAME_NAME='Runner2'
+
+GAME_ARCHIVE1='runner2_amd64_1388171186.tar.gz'
+GAME_ARCHIVE1_MD5='2f7ccdb675a63a5fc152514682e97480'
+GAME_ARCHIVE_FULLSIZE='770000'
+PKG_REVISION='humble1388171186'
+
+INSTALLER_JUNK=''
+INSTALLER_DOC='runner2-1.0/README*'
+INSTALLER_GAME='runner2-1.0/runner2/*'
+
+APP1_ID="${GAME_ID}"
+APP1_EXE='./runner2'
+APP1_ICON='./Runner2.png'
+APP1_ICON_RES='48x48'
+APP1_NAME="${GAME_NAME}"
+APP1_NAME_FR="${GAME_NAME}"
+APP1_CAT='Game'
+
+PKG1_ID="${GAME_ID}"
+PKG1_VERSION='1.0'
+PKG1_ARCH='amd64'
+PKG1_CONFLICTS=''
+PKG1_RECS=''
+PKG1_DEPS='libc6, libstdc++6, libgcc1, zlib1g, libsdl1.2debian, libgl1-mesa-glx'
+PKG1_DESC="${GAME_NAME}
+ package built from the HumbleBundle.com archive"
+
+# Load common functions
+
+TARGET_LIB_VERSION='1.13'
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+if ! [ -e "${PLAYIT_LIB}" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
+	exit 1
+fi
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	exit 1
+fi
+. "${PLAYIT_LIB}"
+
+# Set extra variables
+
+PKG_PREFIX_DEFAULT='/usr/local'
+PKG_COMPRESSION_DEFAULT='none'
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+GAME_LANG_DEFAULT=''
+WITH_MOVIES_DEFAULT=''
+
+printf '\n'
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+fetch_args "$@"
+check_deps "${SCRIPT_DEPS_HARD}" "${SCRIPT_DEPS_SOFT}"
+printf '\n'
+set_checksum
+set_compression
+set_prefix
+
+PATH_BIN="${PKG_PREFIX}/games"
+PATH_DESK='/usr/local/share/applications'
+PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
+PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
+PATH_ICON="/usr/local/share/icons/hicolor/${APP1_ICON_RES}/apps"
+
+printf '\n'
+set_target '1' 'humblebundle.com'
+printf '\n'
+
+# Check target file integrity
+
+if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
+	checksum "${GAME_ARCHIVE}" 'defaults' "${GAME_ARCHIVE1_MD5}"
+fi
+
+# Extract game data
+
+build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON}"
+print wait
+extract_data 'tar' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
+for file in ${INSTALLER_JUNK}; do
+	rm -rf "${PKG_TMPDIR}"/${file}
+done
+for file in ${INSTALLER_DOC}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+done
+for file in ${INSTALLER_GAME}; do
+	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+done
+chmod 755 "${PKG1_DIR}${PATH_GAME}"/${APP1_EXE}
+ln -s "${PATH_GAME}/${APP1_ICON}" "${PKG1_DIR}${PATH_ICON}/${APP1_ID}.png"
+rm -rf "${PKG_TMPDIR}"
+print done
+
+# Write launchers
+
+write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' '' "${APP1_NAME}"
+write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}"
+printf '\n'
+
+# Build package
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
+printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
+
+exit 0
