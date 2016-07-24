fea00115fd6b2abed73ee3fcfd2233913a1428aa
vv221 <vv221@dotslashplay.it>
2016-07-24 12:50:59 +0200

    1.14 - Star Wars: Knights of the Old Republic 2 (GOG) - full rewrite

--- play.it-1.14/play-star-wars-knights-of-the-old-republic-2_gog-2.0.0.3.sh
+++ play.it-1.14/play-star-wars-knights-of-the-old-republic-2_gog-2.0.0.3.sh
@@ -28,30 +28,33 @@
 ###
 
 ###
-# conversion script for the Star Wars: Knights of the Old Republic 2 installer sold on GOG.com
+# conversion script for the Star Wars Knights of the Old Republic II installer sold on GOG.com
 # build a .deb package from the Windows installer
-# tested on Debian, should work on any .deb-based distribution
-#
-# script version 20151127.1
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
+
+script_version=20160724.1
 
 # Set game-specific variables
 
+SCRIPT_DEPS_HARD='fakeroot realpath unar'
+SCRIPT_DEPS_SOFT='icotool wrestool'
+
 GAME_ID='star-wars-kotor-2'
 GAME_ID_SHORT='kotor2'
-GAME_NAME='Knights of the Old Republic II: The Sith Lords'
-GAME_NAME_LONG='Star Wars: Knights of the Old Republic 2 - The Sith Lords'
+GAME_NAME='Star Wars Knights of the Old Republic II: The Sith Lords'
 
 GAME_ARCHIVE1='setup_sw_kotor2_2.0.0.3-1.bin'
 GAME_ARCHIVE1_MD5='bbedad0d349a653a1502f2b9f4c207fc'
 GAME_ARCHIVE2='setup_sw_kotor2_french_2.0.0.3-1.bin'
 GAME_ARCHIVE2_MD5='27a4f0ba820bc66f53aa5117684917cf'
 GAME_ARCHIVE_FULLSIZE='4700000'
-PKG_ORIGIN='gog'
-PKG_REVISION='2.0.0.3'
+PKG_REVISION='gog2.0.0.3'
+
+INSTALLER_PATH='game'
+INSTALLER_DOC='./*.pdf ./update1_0b.txt ./links'
+INSTALLER_GAME='./* ../support/app/*.ini'
 
 GAME_CACHE_DIRS=''
 GAME_CACHE_FILES=''
@@ -77,8 +80,8 @@ APP2_ID="${GAME_ID}_config"
 APP2_EXE='./swconfig.exe'
 APP2_ICON='./swconfig.exe'
 APP2_ICON_RES='32x32 48x48'
-APP2_NAME="${GAME_NAME} (settings)"
-APP2_NAME_FR="${GAME_NAME} (réglages)"
+APP2_NAME="${GAME_NAME} - settings"
+APP2_NAME_FR="${GAME_NAME} - réglages"
 APP2_CAT='Settings'
 
 PKG1_ID="${GAME_ID}"
@@ -87,21 +90,27 @@ PKG1_ARCH='i386'
 PKG1_CONFLICTS=''
 PKG1_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
 PKG1_RECS=''
-PKG1_DESC="${GAME_NAME_LONG}"
+PKG1_DESC="${GAME_NAME}
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
 
 # Load common functions
 
-TARGET_LIB_VERSION='1.13'
+TARGET_LIB_VERSION='1.14'
+
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
 fi
+
 if ! [ -e "${PLAYIT_LIB}" ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'play-anything.sh not found.\n'
 	printf 'It must be placed in the same directory than this script.\n\n'
 	exit 1
 fi
+
 LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
 if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
 	printf '\n\033[1;31mError:\033[0m\n'
 	printf 'Wrong version of play-anything.\n'
@@ -109,25 +118,28 @@ if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt
 	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
 	exit 1
 fi
+
 . "${PLAYIT_LIB}"
 
 # Set extra variables
 
-PKG_PREFIX_DEFAULT='/usr/local'
-PKG_COMPRESSION_DEFAULT='none'
-GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-GAME_LANG_DEFAULT=''
-WITH_MOVIES_DEFAULT=''
+NO_ICON='0'
 
-printf '\n'
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+
 fetch_args "$@"
-check_deps 'fakeroot unar' 'icotool wrestool'
-printf '\n'
+
 set_checksum
 set_compression
 set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
@@ -149,16 +161,24 @@ fi
 
 build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}"
 print wait
-extract_data 'unar_passwd' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet,tolower' ''
-for file in '*.pdf' 'update1_0b.txt' 'links'; do
-	mv "${PKG_TMPDIR}/game"/${file} "${PKG1_DIR}${PATH_DOC}"
+
+extract_data 'unar_passwd' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet,tolower'
+
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
+for file in ${INSTALLER_DOC}; do
+	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
 done
-mv "${PKG_TMPDIR}/game"/* "${PKG1_DIR}${PATH_GAME}"
-mv "${PKG_TMPDIR}/support/app"/*.ini "${PKG1_DIR}${PATH_GAME}"
+
+for file in ${INSTALLER_GAME}; do
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
+
 if [ "${NO_ICON}" = '0' ]; then
 	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
 	extract_icons "${APP2_ID}" "${APP2_ICON}" "${APP2_ICON_RES}" "${PKG_TMPDIR}"
 fi
+
 rm -rf "${PKG_TMPDIR}"
 print done
 
@@ -166,16 +186,19 @@ print done
 
 write_bin_wine_common "${PKG1_DIR}${PATH_BIN}/${APP_COMMON_ID}"
 write_bin_wine_cfg "${PKG1_DIR}${PATH_BIN}/${GAME_ID_SHORT}-winecfg"
+
 write_bin_wine "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' "${APP1_NAME}"
 write_bin_wine "${PKG1_DIR}${PATH_BIN}/${APP2_ID}" "${APP2_EXE}" '' '' "${APP2_NAME}"
+
 write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" 'wine'
 write_desktop "${APP2_ID}" "${APP2_NAME}" "${APP2_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP2_ID}.desktop" "${APP2_CAT}" 'wine'
 printf '\n'
 
 # Build package
 
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_ORIGIN}${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
 build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'defaults'
+
 print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
