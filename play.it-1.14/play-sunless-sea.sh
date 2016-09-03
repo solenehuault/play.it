da6971ce5039bda9739a85448a1620ac594b6bcb
vv221 <vv221@dotslashplay.it>
2016-09-03 18:05:36 +0200

    1.14 - Sunless Sea - new installer + multi-arch handling

--- play.it-1.14/play-sunless-sea.sh
+++ play.it-1.14/play-sunless-sea.sh
@@ -29,14 +29,12 @@
 
 ###
 # conversion script for the Sunless Sea installer sold on HumbleBundle.com
-# build a .deb package from the .run installer
-# tested on Debian, should work on any .deb-based distribution
+# build .deb packages from the MojoSetup installer
 #
 # send your bug reports to vv221@dotslashplay.it
-# start the e-mail subject by "./play.it" to avoid it being flagged as spam
 ###
 
-script_version=20160317.1
+script_version=20160903.1
 
 # Set game-specific variables
 
@@ -46,16 +44,20 @@ GAME_ID='sunless-sea'
 GAME_ID_SHORT='sunless'
 GAME_NAME='Sunless Sea'
 
-GAME_ARCHIVE1='Sunless_Sea-StandAlone-Linux-2016-02-22.sh'
-GAME_ARCHIVE1_MD5='91f751a8d4b6c65e50f1e8466350c5e8'
-GAME_ARCHIVE_FULLSIZE='670000'
-PKG_REVISION='humble160222'
+GAME_ARCHIVE1='Sunless_Sea-StandAlone-Linux-2016-03-29.sh'
+GAME_ARCHIVE1_MD5='b82055a5e48ffe7188309ce28a003b98'
+GAME_ARCHIVE_FULLSIZE='690000'
+PKG_REVISION='humble160329'
 
-INSTALLER_DOC='data/noarch/README.linux'
-INSTALLER_GAME='data/noarch/* data/x86/*'
+INSTALLER_PATH='data'
+INSTALLER_DOC='noarch/README.linux'
+INSTALLER_GAME_PKG1='x86/* noarch/*_Data/Plugins/x86 noarch/*_Data/Mono/x86'
+INSTALLER_GAME_PKG2='x86_64/* noarch/*_Data/Plugins/x86_64 noarch/*_Data/Mono/x86_64'
+INSTALLER_GAME_PKG3='noarch/*'
 
 APP1_ID="${GAME_ID}"
-APP1_EXE='./Sunless Sea.x86'
+APP1_EXE_PKG1='./Sunless Sea.x86'
+APP1_EXE_PKG2='./Sunless Sea.x86_64'
 APP1_ICON1='Sunless Sea_Data/Resources/UnityPlayer.png'
 APP1_ICON1_RES='128x128'
 APP1_ICON2='./Icon.png'
@@ -64,19 +66,46 @@ APP1_NAME="${GAME_NAME}"
 APP1_NAME_FR="${GAME_NAME}"
 APP1_CAT='Game'
 
-PKG1_ID="${GAME_ID}"
-PKG1_ARCH='i386'
-PKG1_VERSION='1.0.4.2130'
-PKG1_CONFLICTS=''
-PKG1_DEPS='libc6, libglu1-mesa | libglu1, libxcursor1'
-PKG1_RECS=''
-PKG1_DESC="${GAME_NAME}
+PKG_ID="${GAME_ID}"
+PKG_VERSION='1.0.4.2130'
+PKG_DEPS='libc6, libglu1-mesa | libglu1, libxcursor1'
+PKG_DESC="${GAME_NAME}
  package built from HumbleBundle.com installer
  ./play.it script version ${script_version}"
 
+PKG1_ID="${PKG_ID}"
+PKG1_ARCH='i386'
+PKG1_VERSION="${PKG_VERSION}"
+PKG1_DEPS="${PKG_DEPS}"
+PKG1_RECS=''
+PKG1_DESC="${PKG_DESC}"
+
+PKG2_ID="${PKG_ID}"
+PKG2_ARCH='amd64'
+PKG2_VERSION="${PKG_VERSION}"
+PKG2_DEPS="${PKG_DEPS}"
+PKG2_RECS=''
+PKG2_DESC="${PKG_DESC}"
+
+PKG3_ID="${GAME_ID}-common"
+PKG3_ARCH='all'
+PKG3_VERSION="${PKG_VERSION}"
+PKG3_CONFLICTS=''
+PKG3_DEPS=''
+PKG3_RECS=''
+PKG3_DESC="${GAME_NAME} - arch-independant data
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
+
+PKG1_CONFLICTS="${PKG2_ID}:${PKG2_ARCH}"
+PKG2_CONFLICTS="${PKG1_ID}:${PKG1_ARCH}"
+
+PKG1_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG1_DEPS}"
+PKG2_DEPS="${PKG3_ID} (= ${PKG_VERSION}-${PKG_REVISION}), ${PKG2_DEPS}"
+
 # Load common functions
 
-TARGET_LIB_VERSION='1.13'
+TARGET_LIB_VERSION='1.14'
 
 if [ -z "${PLAYIT_LIB}" ]; then
 	PLAYIT_LIB='./play-anything.sh'
@@ -115,8 +144,9 @@ set_prefix
 
 check_deps_hard ${SCRIPT_DEPS_HARD}
 
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+printf '\n'
+set_target '1' 'humblebundle.com'
+printf '\n'
 
 PATH_BIN="${PKG_PREFIX}/games"
 PATH_DESK='/usr/local/share/applications'
@@ -124,9 +154,10 @@ PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
 PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
 PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 
-printf '\n'
-set_target '1' 'humblebundle.com'
-printf '\n'
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG2_DIR' "${PKG2_ID}_${PKG2_VERSION}-${PKG_REVISION}_${PKG2_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG3_DIR' "${PKG3_ID}_${PKG3_VERSION}-${PKG_REVISION}_${PKG3_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
 
 # Check target files integrity
 
@@ -136,59 +167,91 @@ fi
 
 # Extract game data
 
-build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DESK}" "${PATH_DOC}" "${PATH_GAME}" "${PATH_ICON_BASE}/${APP1_ICON1_RES}/apps" "${PATH_ICON_BASE}/${APP1_ICON2_RES}/apps"
+build_pkg_dirs '2' "${PATH_BIN}" "${PATH_DESK}" "${PATH_GAME}"
+rm -rf "${PKG3_DIR}"
+mkdir -p "${PKG3_DIR}/DEBIAN" "${PKG3_DIR}${PATH_DOC}" "${PKG3_DIR}${PATH_GAME}"
 print wait
 
 extract_data 'mojo' "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'fix_rights,quiet'
 
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
 for file in ${INSTALLER_DOC}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_DOC}"
+	mv "${file}" "${PKG3_DIR}${PATH_DOC}"
 done
 
-for file in ${INSTALLER_GAME}; do
-	mv "${PKG_TMPDIR}"/${file} "${PKG1_DIR}${PATH_GAME}"
+for file in ${INSTALLER_GAME_PKG1}; do
+	target_file="./${file#*/}"
+	mkdir -p "${PKG1_DIR}${PATH_GAME}/${target_file%/*}"
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}/${target_file}"
 done
 
-chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE}"
+for file in ${INSTALLER_GAME_PKG2}; do
+	target_file="./${file#*/}"
+	mkdir -p "${PKG2_DIR}${PATH_GAME}/${target_file%/*}"
+	mv "${file}" "${PKG2_DIR}${PATH_GAME}/${target_file}"
+done
+
+rmdir --parents --ignore-fail-on-non-empty noarch/*_Data/Mono
+rmdir --parents --ignore-fail-on-non-empty noarch/*_Data/Plugins
+
+for file in ${INSTALLER_GAME_PKG3}; do
+	mv "${file}" "${PKG3_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
+
+chmod 755 "${PKG1_DIR}${PATH_GAME}/${APP1_EXE_PKG1}"
+chmod 755 "${PKG2_DIR}${PATH_GAME}/${APP1_EXE_PKG2}"
 
 rm -rf "${PKG_TMPDIR}"
-
 print done
 
 # Write launchers
 
-write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' '' "${APP1_NAME}"
-
+write_bin_native "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_PKG1}" '' '' '' "${APP1_NAME} (${PKG1_ARCH})"
+write_bin_native "${PKG2_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE_PKG2}" '' '' '' "${APP1_NAME} (${PKG2_ARCH})"
 write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}"
-
+cp -l "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${PKG2_DIR}${PATH_DESK}/${APP1_ID}.desktop"
 printf '\n'
 
 # Build packages
 
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+printf '%s…\n' "$(l10n 'build_pkgs')"
+print wait
 
-file="${PKG1_DIR}/DEBIAN/postinst"
-cat > "${file}" <<- EOF
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}" 'arch'
+write_pkg_debian "${PKG2_DIR}" "${PKG2_ID}" "${PKG2_VERSION}-${PKG_REVISION}" "${PKG2_ARCH}" "${PKG2_CONFLICTS}" "${PKG2_DEPS}" "${PKG2_RECS}" "${PKG2_DESC}" 'arch'
+write_pkg_debian "${PKG3_DIR}" "${PKG3_ID}" "${PKG3_VERSION}-${PKG_REVISION}" "${PKG3_ARCH}" "${PKG3_CONFLICTS}" "${PKG3_DEPS}" "${PKG3_RECS}" "${PKG3_DESC}"
+
+file="${PKG3_DIR}/DEBIAN/postinst"
+cat > "${file}" << EOF
 #!/bin/sh -e
+mkdir --parents "${PATH_ICON_BASE}/${APP1_ICON1_RES}/apps"
+mkdir --parents "${PATH_ICON_BASE}/${APP1_ICON2_RES}/apps"
 ln -s "${PATH_GAME}/${APP1_ICON1}" "${PATH_ICON_BASE}/${APP1_ICON1_RES}/apps/${GAME_ID}.png"
 ln -s "${PATH_GAME}/${APP1_ICON2}" "${PATH_ICON_BASE}/${APP1_ICON2_RES}/apps/${GAME_ID}.png"
 exit 0
 EOF
 chmod 755 "${file}"
 
-file="${PKG1_DIR}/DEBIAN/prerm"
-cat > "${file}" <<- EOF
+file="${PKG3_DIR}/DEBIAN/prerm"
+cat > "${file}" << EOF
 #!/bin/sh -e
 rm "${PATH_ICON_BASE}/${APP1_ICON1_RES}/apps/${GAME_ID}.png"
 rm "${PATH_ICON_BASE}/${APP1_ICON2_RES}/apps/${GAME_ID}.png"
+rmdir --parents --ignore-fail-on-non-empty "${PATH_ICON_BASE}/${APP1_ICON1_RES}/apps"
+rmdir --parents --ignore-fail-on-non-empty "${PATH_ICON_BASE}/${APP1_ICON2_RES}/apps"
 exit 0
 EOF
 chmod 755 "${file}"
 
-build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG1_ARCH}"
+build_pkg "${PKG2_DIR}" "${PKG2_DESC}" "${PKG_COMPRESSION}" 'quiet' "${PKG2_ARCH}"
+build_pkg "${PKG3_DIR}" "${PKG3_DESC}" "${PKG_COMPRESSION}" 'quiet'
+print done
 
-print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
-
+print_instructions "$(printf '%s' "${PKG1_DESC}" | head -n1) (${PKG1_ARCH})" "${PKG3_DIR}" "${PKG1_DIR}"
+printf '\n'
+print_instructions "$(printf '%s' "${PKG2_DESC}" | head -n1) (${PKG2_ARCH})" "${PKG3_DIR}" "${PKG2_DIR}"
 printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
 
 exit 0
