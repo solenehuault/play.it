b1d1ca677de9fac3b1481936bd60eceb18e6144d
vv221 <vv221@dotslashplay.it>
2016-09-25 15:27:37 +0200

    2.0 - write_bin() and write_desktop() now accept an arbitrary number of aguments

--- play.it-2.0/src/write_desktop.sh
+++ play.it-2.0/src/write_desktop.sh
@@ -3,21 +3,22 @@
 # NEEDED VARS: $app_ID, $app_NAME, $app_CAT, PKG_PATH, PATH_DESK
 # CALLS: liberror
 write_desktop() {
-local app="$1"
-testvar "$app" 'APP' || liberror 'app' 'write_desktop'
-local app_id=$(eval echo \$${app}_ID)
-local app_name="$(eval echo \$${app}_NAME)"
-local app_cat="$(eval echo \$${app}_CAT)"
-local target="${PKG_PATH}${PATH_DESK}/${app_id}.desktop"
-mkdir --parents "${target%/*}"
-cat > "${target}" << EOF
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=$app_name
-Icon=$app_id
-Exec=$app_id
-Categories=$app_cat
-EOF
+for app in $@; do
+	testvar "$app" 'APP' || liberror 'app' 'write_desktop'
+	local app_id=$(eval echo \$${app}_ID)
+	local app_name="$(eval echo \$${app}_NAME)"
+	local app_cat="$(eval echo \$${app}_CAT)"
+	local target="${PKG_PATH}${PATH_DESK}/${app_id}.desktop"
+	mkdir --parents "${target%/*}"
+	cat > "${target}" <<- EOF
+	[Desktop Entry]
+	Version=1.0
+	Type=Application
+	Name=$app_name
+	Icon=$app_id
+	Exec=$app_id
+	Categories=$app_cat
+	EOF
+done
 }
 
