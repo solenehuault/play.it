66c83fb49fbf50af7af8848fe26031806c6b3b8e
vv221 <vv221@dotslashplay.it>
2016-09-25 16:56:29 +0200

    2.0 - rebuild library

--- play.it-2.0/libplayit2.sh
+++ play.it-2.0/libplayit2.sh
@@ -33,7 +33,7 @@
 ###
 
 library_version=2.0
-library_revision=20160925.6
+library_revision=20160925.7
 
 string_error_en="\n\033[1;31mError:\033[0m"
 string_error_fr="\n\033[1;31mErreur :\033[0m"
@@ -445,9 +445,9 @@ esac
 ARCHIVE_MD5="$(eval echo \$${archive}_MD5)"
 ARCHIVE_TYPE="$(eval echo \$${archive}_TYPE)"
 if [ -z "$ARCHIVE_TYPE" ]; then
-	if [ -n $(echo "${SOURCE_ARCHIVE##*/}" | grep '^gog_.*\.sh$') ]; then
+	if [ -n "$(echo "${SOURCE_ARCHIVE##*/}" | grep '^gog_.*\.sh$')" ]; then
 		ARCHIVE_TYPE='mojosetup'
-	elif [ -n $(echo "${SOURCE_ARCHIVE##*/}" | grep '^setup_.*\.exe$') ]; then
+	elif [ -n "$(echo "${SOURCE_ARCHIVE##*/}" | grep '^setup_.*\.exe$')" ]; then
 		ARCHIVE_TYPE='innosetup'
 	else
 		case ${LANG%_*} in
