c5c5bf57dd868f7b0676077809c1c829da23a428
Mopi <mopi@dotslashplay.it>
2016-10-29 17:54:17 +0200

    1.14 - new game - The Dark Eye: Chains of Satinav

---
+++ play.it-1.14/play-the-dark-eye-chains-of-satinav.sh
@@ -0,0 +1,218 @@
+#!/bin/sh -e
+
+###
+# Copyright (c) 2015-2016, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# conversion script for the The Dark Eye: Chains of Satinav sold on GOG.com
+# build a .deb package from the InnoSetup installer
+#
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20161029.1
+
+# Set game-specific variables
+
+SCRIPT_DEPS_HARD='fakeroot realpath innoextract'
+SCRIPT_DEPS_SOFT='icotool wrestool'
+
+GAME_ID='the-dark-eye-chains-of-satinav'
+GAME_ID_SHORT='darkeye'
+GAME_NAME='The Dark Eye: Chains of Satinav'
+
+GAME_ARCHIVE1='setup_the_dark_eye_chains_of_satinav_2.0.0.4.exe'
+GAME_ARCHIVE1_MD5='d1c375ba007b7ed6574a16cca823258a'
+GAME_ARCHIVE2='setup_the_dark_eye_chains_of_satinav_2.0.0.4-1.bin'
+GAME_ARCHIVE2_MD5='0c9ea69bdb3e2c66d13f2d27812279b6'
+GAME_ARCHIVE3='setup_the_dark_eye_chains_of_satinav_2.0.0.4-2.bin'
+GAME_ARCHIVE3_MD5='d87f0693751554c1d382f770202e8c45'
+GAME_ARCHIVE4='setup_the_dark_eye_chains_of_satinav_2.0.0.4-3.bin'
+GAME_ARCHIVE4_MD5='ef662b59635829ed4505f6d7272e4bb7'
+GAME_ARCHIVE5='setup_the_dark_eye_chains_of_satinav_2.0.0.4-4.bin'
+GAME_ARCHIVE5_MD5='555d8af3bb598ed4c481e3e3d63b0221'
+
+GAME_ARCHIVE_FULLSIZE='5500000'
+ARCHIVE_TYPE='inno'
+PKG_REVISION='gog2.0.0.4'
+
+INSTALLER_PATH='app'
+INSTALLER_JUNK='./gameuxinstallhelper.dll ./*.ico ./goggame.dll'
+INSTALLER_DOC='./documents/licenses ../tmp/*.txt '
+INSTALLER_GAME='./*'
+
+GAME_CACHE_DIRS=''
+GAME_CACHE_FILES=''
+GAME_CACHE_FILES_POST=''
+GAME_CONFIG_DIRS=''
+GAME_CONFIG_FILES='./*.ini ./*.xml'
+GAME_CONFIG_FILES_POST=''
+GAME_DATA_DIRS=''
+GAME_DATA_FILES=''
+GAME_DATA_FILES_POST=''
+
+APP_COMMON_ID="${GAME_ID_SHORT}-common.sh"
+
+APP1_ID="${GAME_ID}"
+APP1_EXE='./satinav.exe'
+APP1_ICON='./satinav.exe'
+APP1_ICON_RES='16x16 24x24 32x32 48x48 256x256'
+APP1_NAME="${GAME_NAME}"
+APP1_NAME_FR="${APP1_NAME}"
+APP1_CAT='Game'
+
+PKG1_ID="${GAME_ID}"
+PKG1_VERSION='1.0'
+PKG1_ARCH='i386'
+PKG1_CONFLICTS=''
+PKG1_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
+PKG1_RECS=''
+PKG1_DESC="${GAME_NAME}
+ package built from GOG.com installer
+ ./play.it script version ${script_version}"
+
+# Load common functions
+
+TARGET_LIB_VERSION='1.14'
+
+if [ -z "${PLAYIT_LIB}" ]; then
+	PLAYIT_LIB='./play-anything.sh'
+fi
+
+if ! [ -e "${PLAYIT_LIB}" ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'play-anything.sh not found.\n'
+	printf 'It must be placed in the same directory than this script.\n\n'
+	exit 1
+fi
+
+LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
+
+if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'Wrong version of play-anything.\n'
+	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
+	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
+	exit 1
+fi
+
+. "${PLAYIT_LIB}"
+
+# Set extra variables
+
+NO_ICON=0
+
+GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
+PKG_COMPRESSION_DEFAULT='none'
+PKG_PREFIX_DEFAULT='/usr/local'
+fetch_args "$@"
+
+set_checksum
+set_compression
+set_prefix
+
+check_deps_hard ${SCRIPT_DEPS_HARD}
+check_deps_soft ${SCRIPT_DEPS_SOFT}
+
+printf '\n'
+set_target '1' 'gog.com'
+set_target_extra 'GAME_ARCHIVE2' '' "${GAME_ARCHIVE2}"
+set_target_extra 'GAME_ARCHIVE3' '' "${GAME_ARCHIVE3}"
+set_target_extra 'GAME_ARCHIVE4' '' "${GAME_ARCHIVE4}"
+set_target_extra 'GAME_ARCHIVE5' '' "${GAME_ARCHIVE5}"
+# QUELQUECHOSE
+printf '\n'
+
+game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
+
+PATH_BIN="${PKG_PREFIX}/games"
+PATH_DESK='/usr/local/share/applications'
+PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
+PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
+PATH_ICON_BASE="/usr/local/share/icons/hicolor"
+
+# Check target files integrity
+
+if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
+	printf '%s…\n' "$(l10n 'checksum_multiple')"
+	print wait
+
+	checksum "${GAME_ARCHIVE}" 'quiet' "${GAME_ARCHIVE1_MD5}"
+	checksum "${GAME_ARCHIVE2}" 'quiet' "${GAME_ARCHIVE2_MD5}"
+	checksum "${GAME_ARCHIVE3}" 'quiet' "${GAME_ARCHIVE3_MD5}"
+	checksum "${GAME_ARCHIVE4}" 'quiet' "${GAME_ARCHIVE4_MD5}"
+	checksum "${GAME_ARCHIVE5}" 'quiet' "${GAME_ARCHIVE5_MD5}"
+	print done
+fi
+
+# Extract game data
+
+build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_GAME}"
+print wait
+
+extract_data "${ARCHIVE_TYPE}" "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
+
+cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
+for file in ${INSTALLER_JUNK}; do
+	rm -rf "${file}"
+done
+for file in ${INSTALLER_DOC}; do
+	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
+done
+
+for file in ${INSTALLER_GAME}; do
+	mv "${file}" "${PKG1_DIR}${PATH_GAME}"
+done
+cd - > /dev/null
+
+if [ "${NO_ICON}" = '0' ]; then
+	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
+fi
+
+rm -rf "${PKG_TMPDIR}"
+print done
+
+# Write launchers
+
+write_bin_wine_common "${PKG1_DIR}${PATH_BIN}/${APP_COMMON_ID}"
+write_bin_wine_cfg "${PKG1_DIR}${PATH_BIN}/${GAME_ID_SHORT}-winecfg"
+write_bin_wine "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' "${APP1_NAME}"
+
+write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" 'wine'
+
+printf '\n'
+
+# Build package
+
+write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
+build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+
+print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
+printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
+
+exit 0
