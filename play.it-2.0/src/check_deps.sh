cf7bd8153c040c38dccbaca8e5c37129451c9728
vv221 <vv221@dotslashplay.it>
2016-11-10 19:46:15 +0100

    2.0 library - check_deps*() - code rewrite

--- play.it-2.0/src/check_deps.sh
+++ play.it-2.0/src/check_deps.sh
@@ -3,21 +3,47 @@
 # NEEDED VARS: ARCHIVE_TYPE, SCRIPT_DEPS, CHECKSUM_METHOD, PACKAGE_TYPE
 # CALLS: check_deps_7z, check_deps_icon, check_deps_failed
 check_deps() {
-[ "$ARCHIVE_TYPE" = 'innosetup' ] && SCRIPT_DEPS="$SCRIPT_DEPS innoextract"
-[ "$ARCHIVE_TYPE" = 'nixstaller' ] && SCRIPT_DEPS="$SCRIPT_DEPS gzip tar unxz"
-[ "$ARCHIVE_TYPE" = 'mojosetup' ] && SCRIPT_DEPS="$SCRIPT_DEPS unzip"
-[ "$ARCHIVE_TYPE" = 'zip' ] && SCRIPT_DEPS="$SCRIPT_DEPS unzip"
-[ "$ARCHIVE_TYPE" = 'rar' ] && SCRIPT_DEPS="$SCRIPT_DEPS unar"
-[ "$CHECKSUM_METHOD" = 'md5sum' ] && SCRIPT_DEPS="$SCRIPT_DEPS md5sum"
-[ "$PACKAGE_TYPE" = 'deb' ] && SCRIPT_DEPS="$SCRIPT_DEPS fakeroot dpkg"
-[ "${APP_MAIN_ICON##*.}" = 'ico' ] && SCRIPT_DEPS="$SCRIPT_DEPS icotool"
-for dep in $SCRIPT_DEPS; do
-case $dep in
-	7z) check_deps_7z ;;
-	convert|icotool|wrestool) check_deps_icon "$dep" ;;
-	*) [ -n "$(which $dep)" ] || check_deps_failed "$dep" ;;
-esac
-done
+	case "$ARCHIVE_TYPE" in
+		('innosetup')
+			SCRIPT_DEPS="$SCRIPT_DEPS innoextract"
+		;;
+		('nixstaller')
+			SCRIPT_DEPS="$SCRIPT_DEPS gzip tar unxz"
+		;;
+		('mojosetup')
+			SCRIPT_DEPS="$SCRIPT_DEPS unzip"
+		;;
+		('zip')
+			SCRIPT_DEPS="$SCRIPT_DEPS unzip"
+		;;
+		('rar')
+			SCRIPT_DEPS="$SCRIPT_DEPS unar"
+		;;
+	esac
+	if [ "$CHECKSUM_METHOD" = 'md5sum' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS md5sum"
+	fi
+	if [ "$PACKAGE_TYPE" = 'deb' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS fakeroot dpkg"
+	fi
+	if [ "${APP_MAIN_ICON##*.}" = 'ico' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS icotool"
+	fi
+	for dep in $SCRIPT_DEPS; do
+		case $dep in
+			('7z')
+				check_deps_7z
+			;;
+			('convert'|'icotool'|'wrestool')
+				check_deps_icon "$dep"
+			;;
+			(*)
+				if [ -z "$(which $dep)" ]; then
+					check_deps_failed "$dep"
+				fi
+			;;
+		esac
+	done
 }
 
 # check presence of a software to handle .7z archives
@@ -25,15 +51,15 @@ done
 # CALLS: check_deps_failed
 # CALLED BY: check_deps
 check_deps_7z() {
-if [ -n "$(which 7zr)" ]; then
-	extract_7z() { 7zr x -o"$PLAYIT_WORKDIR" -y "$file"; }
-elif [ -n "$(which 7za)" ]; then
-	extract_7z() { 7za x -o"$PLAYIT_WORKDIR" -y "$file"; }
-elif [ -n "$(which unar)" ]; then
-	extract_7z() { unar -output-directory "$PLAYIT_WORKDIR" -force-overwrite -no-directory "$file"; }
-else
-	check_deps_failed 'p7zip'
-fi
+	if [ -n "$(which 7zr)" ]; then
+		extract_7z() { 7zr x -o"$2" -y "$1"; }
+	elif [ -n "$(which 7za)" ]; then
+		extract_7z() { 7za x -o"$2" -y "$1"; }
+	elif [ -n "$(which unar)" ]; then
+		extract_7z() { unar -output-directory "$2" -force-overwrite -no-directory "$1"; }
+	else
+		check_deps_failed 'p7zip'
+	fi
 }
 
 # check presence of a software to handle icon extraction
@@ -41,23 +67,31 @@ fi
 # NEEDED VARS: NO_ICON
 # CALLED BY: check_deps
 check_deps_icon() {
-if [ -z "$(which $1)" ] && [ "$NO_ICON" != '1' ]; then
-	NO_ICON='1'
-	case ${LANG%_*} in
-		fr) echo "$1 est introuvable. Les icônes ne seront pas extraites." ;;
-		en|*) echo "$1 not found. Skipping icons extraction." ;;
-	esac
-fi
+	if [ -z "$(which $1)" ] && [ "$NO_ICON" != '1' ]; then
+		NO_ICON='1'
+		case ${LANG%_*} in
+			('fr')
+				echo "$1 est introuvable. Les icônes ne seront pas extraites."
+			;;
+			('en'|*)
+				echo "$1 not found. Skipping icons extraction."
+			;;
+		esac
+	fi
 }
 
 # display a message if a required dependency is missing
 # USAGE: check_deps_failed $command_name
 # CALLED BY: check_deps, check_deps_7z
 check_deps_failed() {
-case ${LANG%_*} in
-	fr) echo "$string_error_fr\n$1 est introuvable. Installez-le avant de lancer ce script." ;;
-	en|*) echo "$string_error_en\n$1 not found. Install it before running this script." ;;
-esac
-return 1
+	case ${LANG%_*} in
+		('fr')
+			echo "$string_error_fr\n$1 est introuvable. Installez-le avant de lancer ce script."
+		;;
+		('en'|*)
+			echo "$string_error_en\n$1 not found. Install it before running this script."
+		;;
+	esac
+	return 1
 }
 
