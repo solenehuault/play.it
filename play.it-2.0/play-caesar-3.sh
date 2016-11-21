d136e477c7437d900c0938347795777cdf38363e
vv221 <vv221@dotslashplay.it>
2016-11-21 09:38:40 +0100

    2.0 - Caesar 3 - update script to 2.0 syntax

--- play.it-2.0/play-caesar-3.sh
+++ play.it-2.0/play-caesar-3.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
+set -o errexit
 
 ###
 # Copyright (c) 2015-2016, Antoine Le Gonidec
@@ -28,194 +29,114 @@
 ###
 
 ###
-# conversion script for the Caesar 3 installer sold on GOG.com
-# build a .deb package from the InnoSetup installer
-#
+# prototype script using libplayit2.sh
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20160924.1
+script_version=20161121.1
 
 # Set game-specific variables
-
-SCRIPT_DEPS_HARD='fakeroot realpath innoextract'
-SCRIPT_DEPS_SOFT='icotool wrestool'
 
 GAME_ID='caesar-3'
 GAME_ID_SHORT='c3'
 GAME_NAME='Caesar III'
 
-GAME_ARCHIVE1='setup_caesar3_2.0.0.9.exe'
-GAME_ARCHIVE1_MD5='2ee16fab54493e1c2a69122fd2e56635'
-GAME_ARCHIVE_FULLSIZE='550000'
-ARCHIVE_TYPE='inno'
-PKG_REVISION='gog2.0.0.9'
+ARCHIVE_GOG='setup_caesar3_2.0.0.9.exe'
+ARCHIVE_GOG_MD5='2ee16fab54493e1c2a69122fd2e56635'
+ARCHIVE_GOG_UNCOMPRESSED_SIZE='550000'
 
-INSTALLER_PATH='app'
-INSTALLER_DOC='./readme.txt ./*.pdf ../tmp/gog_eula.txt ../tmp/eula.txt'
-INSTALLER_GAME='./555 ./smk ./wavs ./*.555 ./*.emp ./*.eng ./*.inf ./*.map ./*.sg2 ./c3.exe ./c3_model.txt ./caesar3.ini ./mission1.pak ./smackw32.dll'
+ARCHIVE_DOC1_PATH='tmp'
+ARCHIVE_DOC1_FILES='./gog_eula.txt ./eula.txt'
+ARCHIVE_DOC2_PATH='app'
+ARCHIVE_DOC2_PATH='./readme.txt ./*.pdf'
+ARCHIVE_GAME_PATH='app'
+ARCHIVE_GAME_FILES='./555 ./smk ./wavs ./*.555 ./*.emp ./*.eng ./*.inf ./*.map ./*.sg2 ./c3.exe ./c3_model.txt ./caesar3.ini ./mission1.pak ./smackw32.dll'
 
-TRADFR_URL='http://wiki.dotslashplay.it/fr/traductions/caesar-3'
-TRADFR_ARCHIVE1='caesar3fr-full.7z'
-TRADFR_ARCHIVE1_MD5='763e4281e35ebddd0a9e658820c3a6c2'
-TRADFR_ARCHIVE2='caesar3fr-light.7z'
-TRADFR_ARCHIVE2_MD5='7f698b7a86735087d6f95dca07f7f8bc'
-TRADFR_ARCHIVE_TYPE='7z'
-TRADFR_DEPS_HARD='7z'
+CACHE_DIRS=''
+CACHE_FILES=''
+CONFIG_DIRS=''
+CONFIG_FILES='./caesar3.ini'
+DATA_DIRS=''
+DATA_FILES='./c3_model.txt ./status.txt ./*.sav'
 
-GAME_CACHE_DIRS=''
-GAME_CACHE_FILES=''
-GAME_CACHE_FILES_POST=''
-GAME_CONFIG_DIRS=''
-GAME_CONFIG_FILES='./caesar3.ini'
-GAME_CONFIG_FILES_POST=''
-GAME_DATA_DIRS=''
-GAME_DATA_FILES='./c3_model.txt'
-GAME_DATA_FILES_POST='./status.txt ./*.sav'
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='./c3.exe'
+APP_MAIN_ICON='./c3.exe'
+APP_MAIN_ICON_RES='16x16 32x32'
 
-APP_COMMON_ID="${GAME_ID_SHORT}-common.sh"
-
-APP1_ID="${GAME_ID}"
-APP1_EXE='./c3.exe'
-APP1_ICON='./c3.exe'
-APP1_ICON_RES='16x16 32x32'
-APP1_NAME='Caesar III'
-APP1_NAME_FR="${APP1_NAME}"
-APP1_CAT='Game'
-
-PKG1_ID="${GAME_ID}"
-PKG1_VERSION='1.1'
-PKG1_ARCH='i386'
-PKG1_CONFLICTS=''
-PKG1_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
-PKG1_RECS=''
-PKG1_DESC="${GAME_NAME}
- package built from GOG.com installer
+PKG_MAIN_VERSION='1.1-gog2.0.0.9'
+PKG_MAIN_ARCH='i386'
+PKG_MAIN_DEPS='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
+PKG_MAIN_DESC="${GAME_NAME}\n
+ package built from GOG.com installer\n
  ./play.it script version ${script_version}"
 
 # Load common functions
 
-TARGET_LIB_VERSION='1.14'
+target_version='2.0'
 
-if [ -z "${PLAYIT_LIB}" ]; then
-	PLAYIT_LIB='./play-anything.sh'
+if [ -z "${PLAYIT_LIB2}" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XGD_DATA_HOME="${HOME}/.local/share"
+	if [ -e "${XDG_DATA_HOME}/play.it/libplayit2.sh" ]; then
+		PLAYIT_LIB2="${XDG_DATA_HOME}/play.it/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		echo '\n\033[1;31mError:\033[0m\nlibplayit2.sh not found.\n'
+		return 1
+	fi
 fi
+. "$PLAYIT_LIB2"
 
-if ! [ -e "${PLAYIT_LIB}" ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'play-anything.sh not found.\n'
-	printf 'It must be placed in the same directory than this script.\n\n'
-	exit 1
+if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
+	echo "\n\033[1;31mError:\033[0m\nwrong version of libplayit2.sh\ntarget version is: ${target_version}"
+	return 1
 fi
-
-LIB_VERSION="$(grep '^# library version' "${PLAYIT_LIB}" | cut -d' ' -f4 | cut -d'.' -f1,2)"
-
-if [ ${LIB_VERSION%.*} -ne ${TARGET_LIB_VERSION%.*} ] || [ ${LIB_VERSION#*.} -lt ${TARGET_LIB_VERSION#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'Wrong version of play-anything.\n'
-	printf 'It must be at least %s ' "${TARGET_LIB_VERSION}"
-	printf 'but lower than %s.\n\n' "$((${TARGET_LIB_VERSION%.*}+1)).0"
-	exit 1
-fi
-
-. "${PLAYIT_LIB}"
 
 # Set extra variables
 
-NO_ICON=0
-
-GAME_ARCHIVE_CHECKSUM_DEFAULT='md5sum'
-PKG_COMPRESSION_DEFAULT='none'
-PKG_PREFIX_DEFAULT='/usr/local'
-GAME_LANG_DEFAULT='en'
-
+set_common_defaults
 fetch_args "$@"
 
-set_checksum
-set_compression
-set_prefix
-set_lang
+# Set source archive
 
-if [ "${GAME_LANG}" = 'fr' ]; then
-	SCRIPT_DEPS_HARD="${SCRIPT_DEPS_HARD} ${TRADFR_DEPS_HARD}"
+set_source_archive 'ARCHIVE_GOG'
+check_deps
+set_common_paths
+if [ -n "$ARCHIVE" ]; then
+	file_checksum "$SOURCE_ARCHIVE" "$ARCHIVE"
+else
+	file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
 fi
-
-check_deps_hard ${SCRIPT_DEPS_HARD}
-check_deps_soft ${SCRIPT_DEPS_SOFT}
-
-printf '\n'
-set_target '1' 'gog.com'
-if [ "${GAME_LANG}" = 'fr' ]; then
-	set_target_extra 'TRADFR_ARCHIVE' "${TRADFR_URL}" "${TRADFR_ARCHIVE1}" "${TRADFR_ARCHIVE2}"
-fi
-printf '\n'
-
-game_mkdir 'PKG_TMPDIR' "$(mktemp -u ${GAME_ID_SHORT}.XXXXX)" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-game_mkdir 'PKG1_DIR' "${PKG1_ID}_${PKG1_VERSION}-${PKG_REVISION}_${PKG1_ARCH}" "$((${GAME_ARCHIVE_FULLSIZE}*2))"
-
-PATH_BIN="${PKG_PREFIX}/games"
-PATH_DESK='/usr/local/share/applications'
-PATH_DOC="${PKG_PREFIX}/share/doc/${GAME_ID}"
-PATH_GAME="${PKG_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON_BASE="/usr/local/share/icons/hicolor"
-
-# Check target files integrity
-
-if [ "${GAME_ARCHIVE_CHECKSUM}" = 'md5sum' ]; then
-	printf '%s…\n' "$(l10n 'checksum_multiple')"
-	print wait
-
-	checksum "${GAME_ARCHIVE}" 'quiet' "${GAME_ARCHIVE1_MD5}"
-	if [ "${GAME_LANG}" = 'fr' ]; then
-		checksum "${TRADFR_ARCHIVE}" 'quiet' "${TRADFR_ARCHIVE1_MD5}" "${TRADFR_ARCHIVE2_MD5}"
-	fi
-
-	print done
-fi
+check_deps
 
 # Extract game data
 
-build_pkg_dirs '1' "${PATH_BIN}" "${PATH_DOC}" "${PATH_DESK}" "${PATH_GAME}"
-print wait
+set_workdir 'PKG_MAIN'
+extract_data_from "$SOURCE_ARCHIVE"
 
-extract_data "${ARCHIVE_TYPE}" "${GAME_ARCHIVE}" "${PKG_TMPDIR}" 'quiet'
-if [ "${GAME_LANG}" = 'fr' ]; then
-	extract_data "${TRADFR_ARCHIVE_TYPE}" "${TRADFR_ARCHIVE}" "${PKG_TMPDIR}/app" 'force,quiet'
-fi
-
-cd "${PKG_TMPDIR}/${INSTALLER_PATH}"
-for file in ${INSTALLER_DOC}; do
-	mv "${file}" "${PKG1_DIR}${PATH_DOC}"
-done
-
-for file in ${INSTALLER_GAME}; do
-	mv "${file}" "${PKG1_DIR}${PATH_GAME}"
-done
-cd - > /dev/null
+organize_data
 
 if [ "${NO_ICON}" = '0' ]; then
-	extract_icons "${APP1_ID}" "${APP1_ICON}" "${APP1_ICON_RES}" "${PKG_TMPDIR}"
+	extract_icon_from "${PKG_MAIN_PATH}${PATH_GAME}/${APP_MAIN_ICON}"
+	sort_icons 'APP_MAIN'
+	rm --recursive "${PLAYIT_WORKDIR}/icons"
 fi
 
-rm -rf "${PKG_TMPDIR}"
-print done
+rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
 # Write launchers
 
-write_bin_wine_common "${PKG1_DIR}${PATH_BIN}/${APP_COMMON_ID}"
-write_bin_wine_cfg "${PKG1_DIR}${PATH_BIN}/${GAME_ID_SHORT}-winecfg"
-write_bin_wine "${PKG1_DIR}${PATH_BIN}/${APP1_ID}" "${APP1_EXE}" '' '' "${APP1_NAME}"
-
-write_desktop "${APP1_ID}" "${APP1_NAME}" "${APP1_NAME_FR}" "${PKG1_DIR}${PATH_DESK}/${APP1_ID}.desktop" "${APP1_CAT}" 'wine'
-
-printf '\n'
+write_bin 'APP_MAIN'
+write_desktop 'APP_MAIN'
 
 # Build package
 
-write_pkg_debian "${PKG1_DIR}" "${PKG1_ID}" "${PKG1_VERSION}-${PKG_REVISION}" "${PKG1_ARCH}" "${PKG1_CONFLICTS}" "${PKG1_DEPS}" "${PKG1_RECS}" "${PKG1_DESC}"
-build_pkg "${PKG1_DIR}" "${PKG1_DESC}" "${PKG_COMPRESSION}"
+write_metadata 'PKG_MAIN'
+build_pkg 'PKG_MAIN'
 
-print_instructions "${PKG1_DESC}" "${PKG1_DIR}"
-printf '\n%s ;)\n\n' "$(l10n 'have_fun')"
+# Clean up
+
+rm --recursive "${PLAYIT_WORKDIR}"
 
 exit 0
