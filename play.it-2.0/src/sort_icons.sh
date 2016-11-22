252a1bc2c6ccd21b8dcf7be0c6731f4be33bb0aa
vv221 <vv221@dotslashplay.it>
2016-11-22 22:49:59 +0100

    2.0 library - sort_icons - add Arch Linux support

--- play.it-2.0/src/sort_icons.sh
+++ play.it-2.0/src/sort_icons.sh
@@ -1,7 +1,7 @@
 # create icons tree
 # USAGE: sort_icons $app
 # NEEDED VARS: $app_ID, $app_ICON_RES, PKG, $PKG_PATH, PACKAGE_TYPE
-# CALLS: sort_icons_deb, sort_icons_tar
+# CALLS: sort_icons_arch, sort_icons_deb, sort_icons_tar
 sort_icons() {
 for app in $@; do
 	testvar "$app" 'APP' || liberror 'app' 'sort_icons'
@@ -10,10 +10,25 @@ for app in $@; do
 	local icon_res="$(eval echo \$${app}_ICON_RES)"
 	local pkg_path="$(eval echo \$${PKG}_PATH)"
 	case $PACKAGE_TYPE in
+		arch) sort_icons_arch ;;
 		deb) sort_icons_deb ;;
 		tar) sort_icons_tar ;;
 		*) liberror 'PACKAGE_TYPE' 'sort_icons'
 	esac
+done
+}
+
+# create icons tree for .pkg.tar.xz package
+# USAGE: sort_icons_arch
+# NEEDED VARS: PATH_ICON_BASE, PLAYIT_WORKDIR
+# CALLED BY: sort_icons
+sort_icons_arch() {
+for res in $icon_res; do
+	path_icon="${PATH_ICON_BASE}/${res}/apps"
+	mkdir -p "${pkg_path}${path_icon}"
+	for file in "${PLAYIT_WORKDIR}"/icons/*${res}x*.png; do
+		mv "${file}" "${pkg_path}${path_icon}/${app_id}.png"
+	done
 done
 }
 
