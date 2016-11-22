9a508e8e47b54fa10ee406c9719432bb49b209a7
vv221 <vv221@dotslashplay.it>
2016-11-22 22:49:25 +0100

    2.0 library - set_common - add Arch Linux support

--- play.it-2.0/src/set_common.sh
+++ play.it-2.0/src/set_common.sh
@@ -16,13 +16,26 @@ NO_ICON='0'
 # set package paths
 # USAGE: set_common_paths
 # NEEDED VARS: PACKAGE_TYPE
-# CALLS: set_common_paths_deb, set_common_paths_tar, liberror
+# CALLS: set_common_paths_arch, set_common_paths_deb, set_common_paths_tar, liberror
 set_common_paths() {
 case $PACKAGE_TYPE in
+	arch) set_common_paths_arch ;;
 	deb) set_common_paths_deb ;;
 	tar) set_common_paths_tar ;;
 	*) liberror 'PACKAGE_TYPE' 'set_common_paths'
 esac
+}
+
+# set .pkg.tar.xz package paths
+# USAGE: set_common_paths_arch
+# NEEDED VARS: INSTALL_PREFIX, GAME_ID
+# CALLED BY: set_common_paths
+set_common_paths_arch() {
+PATH_BIN="${INSTALL_PREFIX}/bin"
+PATH_DESK='/usr/local/share/applications'
+PATH_DOC="${INSTALL_PREFIX}/share/doc/${GAME_ID}"
+PATH_GAME="${INSTALL_PREFIX}/share/${GAME_ID}"
+PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 }
 
 # set .deb package paths
