12d97285cb7ca1ed25ca0813bf8f54c0ce98ef08
vv221 <vv221@dotslashplay.it>
2016-11-22 22:50:15 +0100

    2.0 library - write_metadata - add Arch Linux support

--- play.it-2.0/src/write_metadata.sh
+++ play.it-2.0/src/write_metadata.sh
@@ -1,15 +1,10 @@
 # write package meta-data
 # USAGE: write_metadata $pkg
 # NEEDED VARS: $pkg_ARCH, $pkg_CONFLICTS, $pkg_DEPS, $pkg_DESC, $pkg_ID, $pkg_PATH, $pkg_VERSION, $PACKAGE_TYPE
-# CALLS: testvar, liberror, write_metadata_deb
+# CALLS: testvar, liberror, write_metadata_arch, write_metadata_deb
 write_metadata() {
 for pkg in $@; do
 	testvar "$pkg" 'PKG' || liberror 'pkg' 'write_metadata'
-	local pkg_arch="$(eval echo \$${pkg}_ARCH)"
-	local pkg_conflicts="$(eval echo \$${pkg}_CONFLICTS)"
-	[ -n "$pkg_conflicts" ] || pkg_conflicts=''
-	local pkg_deps="$(eval echo \$${pkg}_DEPS)"
-	[ -n "$pkg_deps" ] || pkg_deps=''
 	local pkg_desc="$(eval echo \$${pkg}_DESC)"
 	local pkg_id="$(eval echo \$${pkg}_ID)"
 	[ -n "$pkg_id" ] || pkg_id="$GAME_ID"
@@ -17,11 +12,52 @@ for pkg in $@; do
 	local pkg_path="$(eval echo \$${pkg}_PATH)"
 	local pkg_version="$(eval echo \$${pkg}_VERSION)"
 	[ -n "$pkg_version" ] || pkg_version='1.0-1'
-	local pkg_size=$(du --total --block-size=1K --summarize "$pkg_path" | tail --lines=1 | cut --fields=1)
 	case $PACKAGE_TYPE in
-		deb) write_metadata_deb ;;
-		tar) return 0 ;;
+		arch)
+			local pkg_arch="$(eval echo \$${pkg}_ARCH_ARCH)"
+			local pkg_conflicts="$(eval echo \$${pkg}_CONFLICTS_ARCH)"
+			local pkg_deps="$(eval echo \$${pkg}_DEPS_ARCH)"
+			local pkg_size=$(du --total --block-size=1 --summarize "$pkg_path" | tail --lines=1 | cut --fields=1)
+			write_metadata_arch
+		;;
+		deb)
+			local pkg_arch="$(eval echo \$${pkg}_ARCH_DEB)"
+			local pkg_conflicts="$(eval echo \$${pkg}_CONFLICTS_DEB)"
+			local pkg_deps="$(eval echo \$${pkg}_DEPS_DEB)"
+			local pkg_size=$(du --total --block-size=1K --summarize "$pkg_path" | tail --lines=1 | cut --fields=1)
+			write_metadata_deb
+		;;
+		tar)
+			return 0
+		;;
 	esac
+done
+}
+
+# write .pkg.tar.xz package meta-data
+# USAGE: write_metadata_arch
+# CALLED BY: write_metadata
+write_metadata_arch() {
+local target="${pkg_path}/.PKGINFO"
+mkdir --parents "${target%/*}"
+cat > "${target}" << EOF
+pkgname = $pkg_id
+pkgver = $pkg_version
+pkgdesc = $pkg_desc
+packager = $pkg_maint
+builddate = $(date +"%m%d%Y")
+size = $pkg_size
+arch = $pkg_arch
+EOF
+for dep in $pkg_deps; do
+	cat >> "${target}" <<- EOF
+	depends = $dep
+	EOF
+done
+for conflict in $pkg_conflicts; do
+	cat >> "${target}" <<- EOF
+	conflicts = $conflict
+	EOF
 done
 }
 
