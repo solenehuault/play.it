e6f9c3938e884687996e93463543be40082a2f9a
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-23 12:27:31 +0100

    2.0 library - liberror - code rewrite

--- play.it-2.0/src/liberror.sh
+++ play.it-2.0/src/liberror.sh
@@ -1,10 +1,19 @@
-# display an error if a function hs been called with invalid arguments
+# display an error if a function has been called with invalid arguments
 # USAGE: liberror $var_name $calling_function
 liberror() {
-case ${LANG%_*} in
-	fr) echo "$string_error_fr\nvaleur incorrecte pour $1 appelée par $2 : $(eval echo \$$1)" ;;
-	en|*) echo "$string_error_en\ninvalid value for $1 called by $2: $(eval echo \$$1)" ;;
-esac
-return 1
+	local var="$1"
+	local value="$(eval echo \$$var)"
+	local func="$2"
+	case ${LANG%_*} in
+		fr)
+			echo "$string_error_fr"
+			echo "valeur incorrecte pour $var appelée par $func : $value"
+		;;
+		en|*)
+			echo "$string_error_en"
+			echo "invalid value for $var called by $func: $value"
+		;;
+	esac
+	return 1
 }
 
