c2eb254657f8ae87a386d7bcde66fef5cec1da0d
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-23 15:24:36 +0100

    2.0 library - set_common - code rewrite

--- play.it-2.0/src/set_common.sh
+++ play.it-2.0/src/set_common.sh
@@ -1,16 +1,16 @@
 # set default values for common vars
 # USAGE: set_common_defaults
 set_common_defaults() {
-DEFAULT_CHECKSUM_METHOD='md5'
-DEFAULT_COMPRESSION_METHOD='none'
-DEFAULT_GAME_LANG='en'
-DEFAULT_GAME_LANG_AUDIO='en'
-DEFAULT_GAME_LANG_TXT='en'
-DEFAULT_INSTALL_PREFIX='/usr/local'
-DEFAULT_ICON_CHOICE='original'
-DEFAULT_MOVIES_SUPPORT='0'
-DEFAULT_PACKAGE_TYPE='deb'
-NO_ICON='0'
+	DEFAULT_CHECKSUM_METHOD='md5'
+	DEFAULT_COMPRESSION_METHOD='none'
+	DEFAULT_GAME_LANG='en'
+	DEFAULT_GAME_LANG_AUDIO='en'
+	DEFAULT_GAME_LANG_TXT='en'
+	DEFAULT_INSTALL_PREFIX='/usr/local'
+	DEFAULT_ICON_CHOICE='original'
+	DEFAULT_MOVIES_SUPPORT='0'
+	DEFAULT_PACKAGE_TYPE='deb'
+	NO_ICON='0'
 }
 
 # set package paths
@@ -18,12 +18,20 @@ NO_ICON='0'
 # NEEDED VARS: PACKAGE_TYPE
 # CALLS: set_common_paths_arch, set_common_paths_deb, set_common_paths_tar, liberror
 set_common_paths() {
-case $PACKAGE_TYPE in
-	arch) set_common_paths_arch ;;
-	deb) set_common_paths_deb ;;
-	tar) set_common_paths_tar ;;
-	*) liberror 'PACKAGE_TYPE' 'set_common_paths'
-esac
+	case $PACKAGE_TYPE in
+		('arch')
+			set_common_paths_arch
+		;;
+		('deb')
+			set_common_paths_deb
+		;;
+		('tar')
+			set_common_paths_tar
+		;;
+		(*)
+			liberror 'PACKAGE_TYPE' 'set_common_paths'
+		;;
+	esac
 }
 
 # set .pkg.tar.xz package paths
@@ -31,11 +39,11 @@ esac
 # NEEDED VARS: INSTALL_PREFIX, GAME_ID
 # CALLED BY: set_common_paths
 set_common_paths_arch() {
-PATH_BIN="${INSTALL_PREFIX}/bin"
-PATH_DESK='/usr/local/share/applications'
-PATH_DOC="${INSTALL_PREFIX}/share/doc/${GAME_ID}"
-PATH_GAME="${INSTALL_PREFIX}/share/${GAME_ID}"
-PATH_ICON_BASE='/usr/local/share/icons/hicolor'
+	PATH_BIN="${INSTALL_PREFIX}/bin"
+	PATH_DESK='/usr/local/share/applications'
+	PATH_DOC="${INSTALL_PREFIX}/share/doc/${GAME_ID}"
+	PATH_GAME="${INSTALL_PREFIX}/share/${GAME_ID}"
+	PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 }
 
 # set .deb package paths
@@ -43,11 +51,11 @@ PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 # NEEDED VARS: INSTALL_PREFIX, GAME_ID
 # CALLED BY: set_common_paths
 set_common_paths_deb() {
-PATH_BIN="${INSTALL_PREFIX}/games"
-PATH_DESK='/usr/local/share/applications'
-PATH_DOC="${INSTALL_PREFIX}/share/doc/${GAME_ID}"
-PATH_GAME="${INSTALL_PREFIX}/share/games/${GAME_ID}"
-PATH_ICON_BASE='/usr/local/share/icons/hicolor'
+	PATH_BIN="${INSTALL_PREFIX}/games"
+	PATH_DESK='/usr/local/share/applications'
+	PATH_DOC="${INSTALL_PREFIX}/share/doc/${GAME_ID}"
+	PATH_GAME="${INSTALL_PREFIX}/share/games/${GAME_ID}"
+	PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 }
 
 # set .tar archive paths
@@ -55,10 +63,10 @@ PATH_ICON_BASE='/usr/local/share/icons/hicolor'
 # NEEDED VARS: INSTALL_PREFIX
 # CALLED BY: set_common_paths
 set_common_paths_tar() {
-PATH_BIN="${INSTALL_PREFIX}/bin"
-PATH_DESK="$INSTALL_PREFIX"
-PATH_DOC="${INSTALL_PREFIX}/doc"
-PATH_GAME="${INSTALL_PREFIX}/data"
-PATH_ICON_BASE="${INSTALL_PREFIX}/icons"
+	PATH_BIN="${INSTALL_PREFIX}/bin"
+	PATH_DESK="$INSTALL_PREFIX"
+	PATH_DOC="${INSTALL_PREFIX}/doc"
+	PATH_GAME="${INSTALL_PREFIX}/data"
+	PATH_ICON_BASE="${INSTALL_PREFIX}/icons"
 }
 
