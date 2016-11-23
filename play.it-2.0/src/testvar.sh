0c58a27d085817cec34690ea6f76fab0b81c240d
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-23 12:28:23 +0100

    2.0 library - testvar - code rewrite

--- play.it-2.0/src/testvar.sh
+++ play.it-2.0/src/testvar.sh
@@ -1,8 +1,8 @@
-
 # test the validity of the argument given to parent function
 # USAGE: testvar $var_name $pattern
 testvar() {
-if [ -z "$(echo "$1" | grep ^${2})" ]; then
-	return 1
-fi
+	if [ -z "$(echo "$1" | grep ^${2})" ]; then
+		return 1
+	fi
 }
+
