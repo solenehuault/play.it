36c89fda788735bac703c6de69145778cfe15ca3
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-23 12:29:15 +0100

    2.0 library - write_bin - code rewrite

--- play.it-2.0/src/write_bin.sh
+++ play.it-2.0/src/write_bin.sh
@@ -3,35 +3,37 @@
 # NEEDED VARS: $app_ID, $app_TYPE, PKG_PATH, PATH_BIN, $app_EXE
 # CALLS: liberror, write_bin_header, write_bin_set_vars, write_bin_set_exe, write_bin_set_prefix, write_bin_build_userdirs, write_bin_build_prefix, write_bin_run
 write_bin() {
-for app in $@; do
-	testvar "$app" 'APP' || liberror 'app' 'write_bin'
-	local app_id="$(eval echo \$${app}_ID)"
-	[ -n "$app_id" ] || app_id="$GAME_ID"
-	local app_type="$(eval echo \$${app}_TYPE)"
-	local file="${PKG_PATH}${PATH_BIN}/${app_id}"
-	mkdir --parents "${file%/*}"
-	write_bin_header
-	write_bin_set_vars
-	if [ "$app_type" != 'scummvm' ]; then
-		local app_exe="$(eval echo \$${app}_EXE)"
-		write_bin_set_exe
-		write_bin_set_prefix
-		write_bin_build_userdirs
-		write_bin_build_prefix
-	fi
-	write_bin_run
-	chmod 755 "$file"
-done
+	for app in $@; do
+		testvar "$app" 'APP' || liberror 'app' 'write_bin'
+		local app_id="$(eval echo \$${app}_ID)"
+		if [ -z "$app_id" ]; then
+			app_id="$GAME_ID"
+		fi
+		local app_type="$(eval echo \$${app}_TYPE)"
+		local file="${PKG_PATH}${PATH_BIN}/${app_id}"
+		mkdir --parents "${file%/*}"
+		write_bin_header
+		write_bin_set_vars
+		if [ "$app_type" != 'scummvm' ]; then
+			local app_exe="$(eval echo \$${app}_EXE)"
+			write_bin_set_exe
+			write_bin_set_prefix
+			write_bin_build_userdirs
+			write_bin_build_prefix
+		fi
+		write_bin_run
+		chmod 755 "$file"
+	done
 }
 
 # write launcher script header
 # USAGE: write_bin_header
 # CALLED BY: write_bin
 write_bin_header() {
-cat > "$file" << EOF
-#!/bin/sh
-set -o errexit
-
-EOF
+	cat > "$file" <<- EOF
+	#!/bin/sh
+	set -o errexit
+	
+	EOF
 }
 
