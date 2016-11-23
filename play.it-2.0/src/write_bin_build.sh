a7f85cc6e42db0423123d0a2fc191efe9f20e65c
vv221 <vv221@dotslashplay.it>
2016-11-23 19:09:44 +0100

    2.0 library - write_bin_build - use long versions of commands parameters

--- play.it-2.0/src/write_bin_build.sh
+++ play.it-2.0/src/write_bin_build.sh
@@ -5,17 +5,17 @@ write_bin_build_userdirs() {
 	# Build user-writable directories
 	
 	if [ ! -e "\$PATH_CACHE" ]; then
-	  mkdir -p "\$PATH_CACHE"
+	  mkdir --parents "\$PATH_CACHE"
 	  init_userdir_dirs "\$PATH_CACHE" \$CACHE_DIRS
 	  init_userdir_files "\$PATH_CACHE" \$CACHE_FILES
 	fi
 	if [ ! -e "\$PATH_CONFIG" ]; then
-	  mkdir -p "\$PATH_CONFIG"
+	  mkdir --parents "\$PATH_CONFIG"
 	  init_userdir_dirs "\$PATH_CONFIG" \$CONFIG_DIRS
 	  init_userdir_files "\$PATH_CONFIG" \$CONFIG_FILES
 	fi
 	if [ ! -e "\$PATH_DATA" ]; then
-	  mkdir -p "\$PATH_DATA"
+	  mkdir --parents "\$PATH_DATA"
 	  init_userdir_dirs "\$PATH_DATA" \$DATA_DIRS
 	  init_userdir_files "\$PATH_DATA" \$DATA_FILES
 	fi
@@ -29,8 +29,8 @@ write_bin_build_userdirs_wine() {
 	cat >> "$file" <<- EOF
 	export WINEPREFIX WINEARCH WINEDEBUG WINEDLLOVERRIDES
 	if ! [ -e "\$WINEPREFIX" ]; then
-	  mkdir -p "\${WINEPREFIX%/*}"
-	  wineboot -i 2>/dev/null
+	  mkdir --parents "\${WINEPREFIX%/*}"
+	  wineboot --init 2>/dev/null
 	  rm "\${WINEPREFIX}/dosdevices/z:"
 	fi
 	EOF
@@ -46,8 +46,8 @@ write_bin_build_prefix() {
 	[ "$app_type" = 'wine' ] && write_bin_build_userdirs_wine
 	cat >> "$file" <<- EOF
 	if [ ! -e "\$PATH_PREFIX" ]; then
-	  mkdir -p "\$PATH_PREFIX"
-	  cp -surf "\${PATH_GAME}"/* "\${PATH_PREFIX}"
+	  mkdir --parents "\$PATH_PREFIX"
+	  cp --force --recursive --symbolic-link --update "\${PATH_GAME}"/* "\${PATH_PREFIX}"
 	fi
 	init_prefix_files "\$PATH_CACHE"
 	init_prefix_files "\$PATH_CONFIG"
