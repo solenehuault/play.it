e5cf525f2bb73c78327091dd4ef36679f18382ec
vv221 <vv221@dotslashplay.it>
2016-11-27 19:00:06 +0100

    2.0 library - organize_data - multiple fixes

--- play.it-2/src/organize_data.sh
+++ play.it-2/src/organize_data.sh
@@ -1,9 +1,7 @@
 # put files from archive in the right package directories (alias)
 # USAGE: organize_data
-# NEEDED VARS: PKG_PATH, PKG, $PKG_PATH
 # CALLS: organize_data_doc, organize_data_game
 organize_data() {
-	[ -n "$PKG_PATH" ] || PKG_PATH="$(eval echo \$${PKG}_PATH)"
 	if [ -n "${ARCHIVE_DOC_PATH}" ]; then
 		organize_data_generic 'DOC' "$PATH_DOC"
 	fi
@@ -20,16 +18,18 @@ organize_data() {
 
 # put files from archive in the right package directories (generic function)
 # USAGE: organize_data_generic $id $path
-# NEEDED VARS: PKG_PATH, PLAYIT_WORKDIR
+# NEEDED VARS: PKG, $PKG_PATH, PLAYIT_WORKDIR
 # CALLED BY: organize_data_doc organize_data_game
 organize_data_generic() {
+	PKG_PATH="$(eval echo \$${PKG}_PATH)"
 	local archive_path="${PLAYIT_WORKDIR}/gamedata/$(eval echo \$ARCHIVE_${1}_PATH)"
 	local archive_files="$(eval echo \"\$ARCHIVE_${1}_FILES\")"
 	local pkg_path="${PKG_PATH}${2}"
 	mkdir --parents "$pkg_path"
 	cd "$archive_path"
 	for file in $archive_files; do
-		mv "$file" "$pkg_path"
+		mkdir --parents "$pkg_path/${file%/*}"
+		mv "$file" "$pkg_path/$file"
 	done
 	cd - > /dev/null
 }
