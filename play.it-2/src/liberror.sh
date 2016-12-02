6f75eb23ec098e426370792bf07a1f1cca850c6f
vv221 <vv221@dotslashplay.it>
2016-12-02 20:48:16 +0100

    2.0 library - liberror - use print_error

--- play.it-2/src/liberror.sh
+++ play.it-2/src/liberror.sh
@@ -4,13 +4,12 @@ liberror() {
 	local var="$1"
 	local value="$(eval echo \$$var)"
 	local func="$2"
+	print_error
 	case ${LANG%_*} in
 		('fr')
-			printf '%s\n' "$string_error_fr"
 			printf 'valeur incorrecte pour %s appelée par %s : %s\n' "$var" "$func" "$value"
 		;;
 		('en'|*)
-			printf '%s\n' "$string_error_en"
 			printf 'invalid value for %s called by %s: %s\n' "$var" "$func" "$value"
 		;;
 	esac
