17b0ef11bca0969fb0cb755c7dcc4ca6aab643d0
vv221 <vv221@dotslashplay.it>
2016-12-02 20:24:40 +0100

    2.0 library - build_pkg - add compression support to 'arch' and 'tar' targets + use printf instead of echo

--- play.it-2/src/build_pkg.sh
+++ play.it-2/src/build_pkg.sh
@@ -1,4 +1,4 @@
-# build .pkg.tar.xz package, .deb package or .tar archive
+# build .pkg.tar package, .deb package or .tar archive
 # USAGE: build_pkg $pkg[…]
 # NEEDED VARS: $pkg_PATH, PACKAGE_TYPE
 # CALLS: testvar, liberror, build_pkg_arch, build_pkg_deb, build_pkg_tar
@@ -23,16 +23,31 @@ build_pkg() {
 	done
 }
 
-# build .pkg.tar.xz package
+# build .pkg.tar package
 # USAGE: build_pkg_arch
 # NEEDED VARS: PLAYIT_WORKDIR, COMPRESSION_METHOD
 # CALLS: build_pkg_print
 # CALLED BY: build_pkg
 build_pkg_arch() {
 	local pkg_filename="${PWD}/${pkg_path##*/}.pkg.tar"
+	local tar_options='--create'
+	case $COMPRESSION_METHOD in
+		('gzip')
+			tar_options="$tar_options --gzip"
+			pkg_filename="${pkg_filename}.gz"
+		;;
+		('xz')
+			tar_options="$tar_options --xz"
+			pkg_filename="${pkg_filename}.xz"
+		;;
+		('none') ;;
+		(*)
+			liberror 'PACKAGE_TYPE' 'build_pkg'
+		;;
+	esac
 	build_pkg_print
 	cd "$pkg_path"
-	tar -cf "$pkg_filename" .PKGINFO *
+	tar $tar_options --file "$pkg_filename" .PKGINFO *
 	cd - > /dev/null
 }
 
@@ -43,8 +58,9 @@ build_pkg_arch() {
 # CALLED BY: build_pkg
 build_pkg_deb() {
 	local pkg_filename="${PWD}/${pkg_path##*/}.deb"
+	local dpkg_options="-Z$COMPRESSION_METHOD --build \"$pkg_path\""
 	build_pkg_print
-	TMPDIR="$PLAYIT_WORKDIR" fakeroot -- dpkg-deb -Z$COMPRESSION_METHOD -b "$pkg_path" "$pkg_filename" 1>/dev/null
+	TMPDIR="$PLAYIT_WORKDIR" fakeroot -- dpkg-deb $dpkg_options "$pkg_filename" 1>/dev/null
 }
 
 # build .tar archive
@@ -53,9 +69,24 @@ build_pkg_deb() {
 # CALLED BY: build_pkg
 build_pkg_tar() {
 	local pkg_filename="${PWD}/${pkg_path##*/}.tar"
+	local tar_options='--create'
+	case $COMPRESSION_METHOD in
+		('gzip')
+			tar_options="$tar_options --gzip"
+			pkg_filename="${pkg_filename}.gz"
+		;;
+		('xz')
+			tar_options="$tar_options --xz"
+			pkg_filename="${pkg_filename}.xz"
+		;;
+		('none') ;;
+		(*)
+			liberror 'PACKAGE_TYPE' 'build_pkg'
+		;;
+	esac
 	build_pkg_print
 	cd "$pkg_path"
-	tar --create --file "$pkg_filename" .
+	tar $tar_options --file "$pkg_filename" .
 	cd - > /dev/null
 }
 
@@ -65,10 +96,10 @@ build_pkg_tar() {
 build_pkg_print() {
 	case ${LANG%_*} in
 		('fr')
-			echo "Construction de $pkg_filename"
+			printf 'Construction de %s\n' "$pkg_filename"
 		;;
 		('en'|*)
-			echo "Building $pkg_filename"
+			printf 'Building %s\n' "$pkg_filename"
 		;;
 	esac
 }
