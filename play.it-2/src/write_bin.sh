7d1b334328bd53626ee342c4c3ece5e6eeef26bd
vv221 <vv221@dotslashplay.it>
2016-12-09 15:01:49 +0100

    2.0 library - write_bin - improve rights management

--- play.it-2/src/write_bin.sh
+++ play.it-2/src/write_bin.sh
@@ -1,8 +1,9 @@
 # write launcher script
 # USAGE: write_bin $app
-# NEEDED VARS: $app_ID, $app_TYPE, PKG_PATH, PATH_BIN, $app_EXE
+# NEEDED VARS: $app_ID, $app_TYPE, PKG, PATH_BIN, $app_EXE
 # CALLS: liberror, write_bin_header, write_bin_set_vars, write_bin_set_exe, write_bin_set_prefix, write_bin_build_userdirs, write_bin_build_prefix, write_bin_run
 write_bin() {
+	PKG_PATH="$(eval echo \$${PKG}_PATH)"
 	for app in $@; do
 		testvar "$app" 'APP' || liberror 'app' 'write_bin'
 		local app_id="$(eval echo \$${app}_ID)"
@@ -16,6 +17,7 @@ write_bin() {
 		write_bin_set_vars
 		if [ "$app_type" != 'scummvm' ]; then
 			local app_exe="$(eval echo \$${app}_EXE)"
+			chmod +x "${PKG_PATH}${PATH_GAME}/$app_exe"
 			write_bin_set_exe
 			write_bin_set_prefix
 			write_bin_build_userdirs
