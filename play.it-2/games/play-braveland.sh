02a522c11e959fa759d4be4a2bc3a712e2c43e05
Mopi <mopi@dotslashplay.it>
2016-12-12 16:38:41 +0100

    2.0 - Braveland - Fix icon handling

--- play.it-2/games/play-braveland.sh
+++ play.it-2/games/play-braveland.sh
@@ -29,7 +29,8 @@ set -o errexit
 ###
 
 ###
-# prototype script using libplayit2.sh
+# Braveland
+# build native Linux packages from the original installers
 # send your bug reports to vv221@dotslashplay.it
 ###
 
@@ -103,11 +104,8 @@ fetch_args "$@"
 set_source_archive 'ARCHIVE_GOG'
 check_deps
 set_common_paths
-if [ -n "$ARCHIVE" ]; then
-	file_checksum "$SOURCE_ARCHIVE" "$ARCHIVE"
-else
-	file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-fi
+PATH_ICON="$PKG_MAIN_PATH$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
+file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
 check_deps
 
 # Extract game data
@@ -116,12 +114,6 @@ set_workdir 'PKG_MAIN'
 extract_data_from "$SOURCE_ARCHIVE"
 
 organize_data
-
-PATH_ICON="$PKG_MAIN_PATH$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-mkdir --parents "${PATH_ICON}"
-cd "$PATH_ICON"
-ln --symbolic "$PATH_GAME/$APP_MAIN_ICON" "./$GAME_ID.png"
-cd - > /dev/null
 
 rm --recursive "${PLAYIT_WORKDIR}/gamedata"
 
@@ -132,11 +124,25 @@ write_desktop 'APP_MAIN'
 
 # Build package
 
+cat > "$postinst" << EOF
+mkdir --parents "$PATH_ICON"
+ln --symbolic "$PATH_GAME"/$APP_MAIN_ICON "$PATH_ICON/$GAME_ID.png"
+EOF
+
+cat > "$prerm" << EOF
+rm "$PATH_ICON/$GAME_ID.png"
+rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON"
+EOF
+
 write_metadata 'PKG_MAIN'
 build_pkg 'PKG_MAIN'
 
 # Clean up
 
 rm --recursive "${PLAYIT_WORKDIR}"
+
+# Print instructions
+
+print_instructions "$PKG_MAIN_PKG"
 
 exit 0
