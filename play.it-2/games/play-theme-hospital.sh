1e1716aee803b47d8134629dcdb2eb10d919f651
vv221 <vv221@dotslashplay.it>
2016-12-17 16:25:48 +0100

    2.0 - Theme Hospital - split $PKG_DESC into $PKG_DESC_ARCH & $PKG_DESC_DEB

--- play.it-2/games/play-theme-hospital.sh
+++ play.it-2/games/play-theme-hospital.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161215.2
+script_version=20161217.1
 
 # Set game-specific variables
 
@@ -65,9 +65,9 @@ PKG_MAIN_ARCH_DEB='all'
 PKG_MAIN_ARCH_ARCH='any'
 PKG_MAIN_DEPS_DEB='dosbox'
 PKG_MAIN_DEPS_ARCH='dosbox'
-PKG_MAIN_DESC="$GAME_NAME\n
- package built from GOG.com installer\n
+PKG_MAIN_DESC_DEB="$GAME_NAME\n
  ./play.it script version $script_version"
+PKG_MAIN_DESC_ARCH="$GAME_NAME - ./play.it script version $script_version"
 
 # Load common functions
 
