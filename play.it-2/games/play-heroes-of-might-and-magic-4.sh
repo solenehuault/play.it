67d977e705986d42008ee45715844951946ff273
vv221 <vv221@dotslashplay.it>
2016-12-17 16:21:40 +0100

    2.0 - Heroes of Might and Magic 4 - split $PKG_DESC into $PKG_DESC_ARCH & $PKG_DESC_DEB

--- play.it-2/games/play-heroes-of-might-and-magic-4.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-4.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161215.1
+script_version=20161217.1
 
 # Set game-specific variables
 
@@ -77,9 +77,9 @@ PKG_MAIN_ARCH_DEB='i386'
 PKG_MAIN_ARCH_ARCH='any'
 PKG_MAIN_DEPS_DEB='winetricks, wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
 PKG_MAIN_DEPS_ARCH='winetricks wine'
-PKG_MAIN_DESC="$GAME_NAME\n
- package built from GOG.com installer\n
+PKG_MAIN_DESC_DEB="$GAME_NAME\n
  ./play.it script version $script_version"
+PKG_MAIN_DESC_ARCH="$GAME_NAME - ./play.it script version $script_version"
 
 # Load common functions
 
