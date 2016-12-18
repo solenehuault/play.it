3715086172f49240516932eb23a9212cab0aa7ce
vv221 <vv221@dotslashplay.it>
2016-12-18 16:32:43 +0100

    2.0 - Lovers in a Dangerous Spacetime - store individual logs for each game session

--- play.it-2/games/play-lovers-in-a-dangerous-spacetime.sh
+++ play.it-2/games/play-lovers-in-a-dangerous-spacetime.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161218.2
+script_version=20161218.3
 
 # Set game-specific variables
 
@@ -53,9 +53,12 @@ ARCHIVE_GAME_64_FILES='./*.x86_64 ./*_Data/*/x86_64'
 ARCHIVE_GAME_MAIN_PATH='.'
 ARCHIVE_GAME_MAIN_FILES='./*_Data'
 
+DATA_DIRS='./logs'
+
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE_32='./LoversInADangerousSpacetime.x86'
 APP_MAIN_EXE_64='./LoversInADangerousSpacetime.x86_64'
+APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
 APP_MAIN_ICON='*_Data/Resources/UnityPlayer.png'
 APP_MAIN_ICON_RES='128x128'
 
