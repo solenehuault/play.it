5abf885a06f741666bc2eb47bbf9dc3def7cdc10
vv221 <vv221@dotslashplay.it>
2016-12-18 16:32:34 +0100

    2.0 - HuniePop - store individual logs for each game session

--- play.it-2/games/play-huniepop.sh
+++ play.it-2/games/play-huniepop.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161217.1
+script_version=20161218.1
 
 # Set game-specific variables
 
@@ -51,8 +51,11 @@ ARCHIVE_DOC_FILES='./*'
 ARCHIVE_GAME_PATH='data/noarch/game'
 ARCHIVE_GAME_FILES='./*'
 
+DATA_DIRS='./logs'
+
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='./HuniePop.x86'
+APP_MAIN_OPTIONS='-logFile ./logs/$(date +%F-%R).log'
 APP_MAIN_ICON='*_Data/Resources/UnityPlayer.png'
 APP_MAIN_ICON_RES='128x128'
 
