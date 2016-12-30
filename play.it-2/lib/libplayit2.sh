2eecbba60706e175ebe7c20dc9d0964b876d3aa2
vv221 <vv221@dotslashplay.it>
2016-12-30 14:49:49 +0100

    2.0 library - New build

--- play.it-2/lib/libplayit2.sh
+++ play.it-2/lib/libplayit2.sh
@@ -33,7 +33,7 @@
 ###
 
 library_version=2.0
-library_revision=20161229.1
+library_revision=20161230.1
 
 # build .pkg.tar package, .deb package or .tar archive
 # USAGE: build_pkg $pkg[…]
@@ -1466,29 +1466,37 @@ write_desktop_winecfg() {
 
 # write package meta-data
 # USAGE: write_metadata $pkg
-# NEEDED VARS: $pkg_ARCH_ARCH, $pkg_ARCH_DEB, $pkg_CONFLICTS_ARCH, $pkg_CONFLICTS_DEB, $pkg_DEPS_ARCH, $pkg_DEPS_DEB, $pkg_DESC_ARCH, $pkg_DESC_DEB, $pkg_ID, $pkg_PATH, $pkg_PROVIDES_ARCH, $pkg_PROVIDES_DEB, $pkg_VERSION, $PACKAGE_TYPE
-# CALLS: testvar, liberror, write_metadata_arch, write_metadata_deb
+# NEEDED VARS: $PKG_ARCH_ARCH $PKG_ARCH_DEB $PKG_CONFLICTS_ARCH
+# 	$PKG_CONFLICTS_DEB $PKG_DEPS_ARCH $PKG_DEPS_DEB $PKG_DESCRIPTION
+# 	$PKG_ID $PKG_PATH $PKG_PROVIDES_ARCH $PKG_PROVIDES_DEB $PKG_VERSION
+# 	$PACKAGE_TYPE
+# CALLS: testvar liberror write_metadata_arch write_metadata_deb
 write_metadata() {
 	for pkg in $@; do
+
 		testvar "$pkg" 'PKG' || liberror 'pkg' 'write_metadata'
+
 		local pkg_id="$(eval echo \$${pkg}_ID)"
-		if [ -z "$pkg_id" ]; then
-			pkg_id="$GAME_ID"
-		fi
+		local pkg_description="$(eval echo \$${pkg}_DESCRIPTION)"
 		local pkg_maint="$(whoami)@$(hostname)"
 		local pkg_path="$(eval echo \$${pkg}_PATH)"
 		local pkg_version="$(eval echo \$${pkg}_VERSION)"
-		if [ -z "$pkg_version" ]; then
+
+		if [ ! "$pkg_id" ]; then
+			pkg_id="$GAME_ID"
+		fi
+
+		if [ ! "$pkg_version" ]; then
 			pkg_version="$PKG_VERSION"
 		fi
-		if [ -z "$pkg_version" ]; then
+		if [ ! "$pkg_version" ]; then
 			pkg_version='1.0-1'
 		fi
+
 		case $PACKAGE_TYPE in
 			('arch')
 				local pkg_arch="$(eval echo \$${pkg}_ARCH_ARCH)"
 				local pkg_conflicts="$(eval echo \$${pkg}_CONFLICTS_ARCH)"
-				local pkg_desc="$(eval echo \$${pkg}_DESC_ARCH)"
 				local pkg_deps="$(eval echo \$${pkg}_DEPS_ARCH)"
 				local pkg_provides="$(eval echo \$${pkg}_PROVIDES_ARCH)"
 				local pkg_size=$(du --total --block-size=1 --summarize "$pkg_path" | tail --lines=1 | cut --fields=1)
@@ -1498,7 +1506,6 @@ write_metadata() {
 				local pkg_arch="$(eval echo \$${pkg}_ARCH_DEB)"
 				local pkg_conflicts="$(eval echo \$${pkg}_CONFLICTS_DEB)"
 				local pkg_deps="$(eval echo \$${pkg}_DEPS_DEB)"
-				local pkg_desc="$(eval echo \$${pkg}_DESC_DEB)"
 				local pkg_provides="$(eval echo \$${pkg}_PROVIDES_DEB)"
 				local pkg_size=$(du --total --block-size=1K --summarize "$pkg_path" | tail --lines=1 | cut --fields=1)
 				write_metadata_deb
@@ -1507,40 +1514,57 @@ write_metadata() {
 				return 0
 			;;
 		esac
+
 	done
 }
 
-# write .pkg.tar.xz package meta-data
+# write .pkg.tar package meta-data
 # USAGE: write_metadata_arch
 # CALLED BY: write_metadata
 write_metadata_arch() {
 	local target="${pkg_path}/.PKGINFO"
+
 	mkdir --parents "${target%/*}"
+
 	cat > "${target}" <<- EOF
 	pkgname = $pkg_id
 	pkgver = $pkg_version
-	pkgdesc = $pkg_desc
 	packager = $pkg_maint
 	builddate = $(date +"%m%d%Y")
 	size = $pkg_size
 	arch = $pkg_arch
 	EOF
+
+	if [ "$pkg_description" ]; then
+		cat >> "${target}" <<- EOF
+		pkgdesc = $GAME_NAME - $pkg_description - ./play.it script version $script_version
+		EOF
+	else
+		cat >> "${target}" <<- EOF
+		pkgdesc = $GAME_NAME - ./play.it script version $script_version
+		EOF
+	fi
+
 	for dep in $pkg_deps; do
 		cat >> "${target}" <<- EOF
 		depend = $dep
 		EOF
 	done
+
 	for conflict in $pkg_conflicts; do
 		cat >> "${target}" <<- EOF
 		conflict = $conflict
 		EOF
 	done
+
 	for provide in $pkg_provides; do
 		cat >> "${target}" <<- EOF
 		provide = $provide
 		EOF
 	done
-	local target="${pkg_path}/.INSTALL"
+
+	target="${pkg_path}/.INSTALL"
+
 	if [ -e "$postinst" ]; then
 		cat >> "$target" <<- EOF
 		post_install() {
@@ -1553,6 +1577,7 @@ write_metadata_arch() {
 		}
 		EOF
 	fi
+
 	if [ -e "$prerm" ]; then
 		cat >> "$target" <<- EOF
 		pre_remove() {
@@ -1572,38 +1597,54 @@ write_metadata_arch() {
 # CALLED BY: write_metadata
 write_metadata_deb() {
 	local target="$pkg_path/DEBIAN/control"
+
 	mkdir --parents "${target%/*}"
+
 	cat > "$target" <<- EOF
 	Package: $pkg_id
 	Version: $pkg_version
 	Architecture: $pkg_arch
 	Maintainer: $pkg_maint
 	Installed-Size: $pkg_size
+	Section: non-free/games
 	EOF
-	if [ -n "$pkg_conflicts" ]; then
+
+	if [ "$pkg_conflicts" ]; then
 		cat >> "$target" <<- EOF
 		Conflicts: $pkg_conflicts
 		EOF
 	fi
-	if [ -n "$pkg_provides" ]; then
+
+	if [ "$pkg_provides" ]; then
 		cat >> "$target" <<- EOF
 		Provides: $pkg_provides
 		EOF
 	fi
-	if [ -n "$pkg_deps" ]; then
+
+	if [ "$pkg_deps" ]; then
 		cat >> "$target" <<- EOF
 		Depends: $pkg_deps
 		EOF
 	fi
-	cat >> "$target" <<- EOF
-	Section: non-free/games
-	Description: $pkg_desc
-	EOF
+
+	if [ "$pkg_description" ]; then
+		cat >> "${target}" <<- EOF
+		Description: $GAME_NAME - $pkg_description
+		 ./play.it script version $script_version
+		EOF
+	else
+		cat >> "${target}" <<- EOF
+		Description: $GAME_NAME
+		 ./play.it script version $script_version
+		EOF
+	fi
+
 	if [ "$pkg_arch" = 'all' ]; then
 		sed -i 's/Architecture: all/&\nMulti-Arch: foreign/' "$target"
 	fi
+
 	if [ -e "$postinst" ]; then
-		local target="$pkg_path/DEBIAN/postinst"
+		target="$pkg_path/DEBIAN/postinst"
 		cat > "$target" <<- EOF
 		#!/bin/sh -e
 		EOF
@@ -1613,8 +1654,9 @@ write_metadata_deb() {
 		EOF
 		chmod 755 "$target"
 	fi
+
 	if [ -e "$prerm" ]; then
-		local target="$pkg_path/DEBIAN/prerm"
+		target="$pkg_path/DEBIAN/prerm"
 		cat > "$target" <<- EOF
 		#!/bin/sh -e
 		EOF
