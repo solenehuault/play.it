365c0268e94b223bc50a15ca45f1abec5a607a21
vv221 <vv221@dotslashplay.it>
2017-01-01 18:41:19 +0100

    2.0 - The Witcher 2 - Use unzip for archive extraction

--- play.it-2/games/play-the-witcher-2.sh
+++ play.it-2/games/play-the-witcher-2.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161230.2
+script_version=20170101.1
 
 # Set game-specific variables
 
@@ -45,6 +45,7 @@ ARCHIVE_GOG='gog_the_witcher_2_assassins_of_kings_enhanced_edition_2.2.0.8.sh'
 ARCHIVE_GOG_MD5='3fff5123677a7be2023ecdb6af3b82b6'
 ARCHIVE_GOG_UNCOMPRESSED_SIZE='24000000'
 ARCHIVE_GOG_VERSION='1release3-gog2.2.0.8'
+ARCHIVE_GOG_TYPE='mojosetup_unzip'
 
 ARCHIVE_DOC1_PATH='data/noarch/docs'
 ARCHIVE_DOC1_FILES='./*'
