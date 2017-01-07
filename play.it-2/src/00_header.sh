cd5ce21c887878ce5b2ed5d3faa0da48e8dc7d8c
vv221 <vv221@dotslashplay.it>
2017-01-07 19:57:04 +0100

    2.0 library - Update copyright year in license

--- play.it-2/src/00_header.sh
+++ play.it-2/src/00_header.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ###
-# Copyright (c) 2015-2016, Antoine Le Gonidec
+# Copyright (c) 2015-2017, Antoine Le Gonidec
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
