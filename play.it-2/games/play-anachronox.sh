9497491980979f968e69e1a1870129b0b0d64af7
vv221 <vv221@dotslashplay.it>
2017-01-08 20:15:44 +0100

    2.0 - Anachronox - Use new alias function extract_and_sort_icons_from()

--- play.it-2/games/play-anachronox.sh
+++ play.it-2/games/play-anachronox.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161230.2
+script_version=20170108.1
 
 # Set game-specific variables
 
@@ -110,11 +110,7 @@ set_workdir 'PKG_MAIN'
 extract_data_from "$SOURCE_ARCHIVE"
 organize_data
 
-if [ "$NO_ICON" = '0' ]; then
-	extract_icon_from "${PKG_MAIN_PATH}${PATH_GAME}/$APP_MAIN_ICON"
-	sort_icons 'APP_MAIN'
-	rm --recursive "$PLAYIT_WORKDIR/icons"
-fi
+extract_and_sort_icons_from 'APP_MAIN'
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
