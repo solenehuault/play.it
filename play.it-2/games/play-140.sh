5d62cac225872a1ab51bb275612d4b9e3fdddc62
vv221 <vv221@dotslashplay.it>
2017-02-09 12:47:35 +0100

    2.0 - 140 - Code clean-up

--- play.it-2/games/play-140.sh
+++ play.it-2/games/play-140.sh
@@ -34,81 +34,64 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170902.2
+script_version=20170902.3
 
 # Set game-specific variables
 
 GAME_ID='140-game'
-GAME_ID_SHORT='140'
 GAME_NAME='140'
 
 ARCHIVE_HUMBLE='140_Linux_1389820765.zip'
 ARCHIVE_HUMBLE_MD5='e78c09a2a9f47d89a4bb1e4e97911e79'
 ARCHIVE_HUMBLE_UNCOMPRESSED_SIZE='92000'
+ARCHIVE_HUMBLE_VERSION='1.0-humble1389820765'
 
-ARCHIVE_GAME_32_PATH='.'
-ARCHIVE_GAME_32_FILES='./140.x86 140_Data/Mono/x86 140_Data/Plugins/x86'
-ARCHIVE_GAME_64_PATH='.'
-ARCHIVE_GAME_64_FILES='./140.x86_64 140_Data/Mono/x86_64 140_Data/Plugins/x86_64'
-ARCHIVE_GAME_MAIN_PATH='.'
-ARCHIVE_GAME_MAIN_FILES='./140_Data'
-
-CACHE_DIRS=''
-CACHE_FILES=''
-CONFIG_DIRS=''
-CONFIG_FILES=''
-DATA_DIRS=''
-DATA_FILES=''
+ARCHIVE_GAME_BIN32_PATH='.'
+ARCHIVE_GAME_BIN32_FILES='./140.x86 140_Data/*/x86'
+ARCHIVE_GAME_BIN64_PATH='.'
+ARCHIVE_GAME_BIN64_FILES='./140.x86_64 140_Data/*/x86_64'
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='./140_Data'
 
 APP_MAIN_TYPE='native'
-APP_MAIN_EXE_32='./140.x86'
-APP_MAIN_EXE_64='./140.x86_64'
-APP_MAIN_ICON='./140_Data/Resources/UnityPlayer.png'
+APP_MAIN_EXE_32='140.x86'
+APP_MAIN_EXE_64='140.x86_64'
+APP_MAIN_ICON='140_Data/Resources/UnityPlayer.png'
 APP_MAIN_ICON_RES='128x128'
 
-PKG_MAIN_ID="${GAME_ID}-common"
-PKG_MAIN_VERSION='1.0-humble1389820765'
-PKG_MAIN_DEPS_DEB=''
-PKG_MAIN_DEPS_ARCH=''
-PKG_MAIN_DESC="${GAME_NAME} - arch-independant data\n
- package built from HUMBLE.com installer\n
- ./play.it script version ${script_version}"
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRITPION='data'
 
-PKG_32_VERSION="${PKG_MAIN_VERSION}"
-PKG_32_ARCH='32'
-PKG_32_DEPS_DEB="$PKG_MAIN_ID, libc6, libstdc++6, libglu1-mesa | libglu1, libasound2-plugins, libxcursor1"
-PKG_32_DEPS_ARCH="$PKG_MAIN_ID glu alsa-lib libxcursor"
-PKG_32_DESC="${GAME_NAME}\n
- package built from HUMBLE.com installer\n
- ./play.it script version ${script_version}"
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libglu1-mesa | libglu1, libasound2-plugins, libxcursor1"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID glu alsa-lib libxcursor"
 
-PKG_64_VERSION="${PKG_MAIN_VERSION}"
-PKG_64_ARCH='64'
-PKG_64_DEPS_DEB="$PKG_MAIN_ID, libc6, libstdc++6, libglu1-mesa | libglu1, libasound2-plugins, libxcursor1"
-PKG_64_DEPS_ARCH="$PKG_MAIN_ID glu alsa-lib libxcursor"
-PKG_64_DESC="${GAME_NAME}\n
- package built from HUMBLE.com installer\n
- ./play.it script version ${script_version}"
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_ARCH="$PKG_BIN32_DEPS_ARCH"
 
 # Load common functions
 
 target_version='2.0'
 
-if [ -z "${PLAYIT_LIB2}" ]; then
-	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="${HOME}/.local/share"
-	if [ -e "${XDG_DATA_HOME}/play.it/libplayit2.sh" ]; then
-		PLAYIT_LIB2="${XDG_DATA_HOME}/play.it/libplayit2.sh"
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
 	elif [ -e './libplayit2.sh' ]; then
 		PLAYIT_LIB2='./libplayit2.sh'
 	else
-		echo '\n\033[1;31mError:\033[0m\nlibplayit2.sh not found.\n'
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
 		return 1
 	fi
 fi
 . "$PLAYIT_LIB2"
 
 if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	echo "\n\033[1;31mError:\033[0m\nwrong version of libplayit2.sh\ntarget version is: ${target_version}"
+	printf '\n\033[1;31mError:\033[0m\n'
+	printf 'wrong version of libplayit2.sh\n'
+	printf 'target version is: %s\n' "$target_version"
 	return 1
 fi
 
@@ -122,42 +105,45 @@ fetch_args "$@"
 set_source_archive 'ARCHIVE_HUMBLE'
 check_deps
 set_common_paths
-PATH_ICON="$PKG_MAIN_PATH$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
 file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_HUMBLE'
 check_deps
 
 # Extract game data
 
-set_workdir 'PKG_MAIN' 'PKG_32' 'PKG_64'
+set_workdir 'PKG_BIN32' 'PKG_BIN64' 'PKG_DATA'
 extract_data_from "$SOURCE_ARCHIVE"
 
-PKG='PKG_32'
-organize_data_generic 'GAME_32' "$PATH_GAME"
-PKG='PKG_64'
-organize_data_generic 'GAME_64' "$PATH_GAME"
-PKG='PKG_MAIN'
-organize_data_generic 'GAME_MAIN' "$PATH_GAME"
-organize_data_generic 'DOC' "$PATH_DOC"
+PKG='PKG_BIN32'
+organize_data_generic 'GAME_BIN32' "$PATH_GAME"
 
-rm --recursive "${PLAYIT_WORKDIR}/gamedata"
+PKG='PKG_BIN64'
+organize_data_generic 'GAME_BIN64' "$PATH_GAME"
+
+PKG='PKG_DATA'
+organize_data_generic 'GAME_DATA' "$PATH_GAME"
+organize_data_generic 'DOC'       "$PATH_DOC"
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-PKG='PKG_32'
+PKG='PKG_BIN32'
 APP_MAIN_EXE="$APP_MAIN_EXE_32"
-write_bin 'APP_MAIN'
+write_bin     'APP_MAIN'
 write_desktop 'APP_MAIN'
 
-PKG='PKG_64'
+PKG='PKG_BIN64'
 APP_MAIN_EXE="$APP_MAIN_EXE_64"
-write_bin 'APP_MAIN'
+write_bin     'APP_MAIN'
 write_desktop 'APP_MAIN'
 
 # Build package
 
+PATH_ICON="${PKG_DATA_PATH}${PATH_ICON_BASE}/$APP_MAIN_ICON_RES/apps"
+
 cat > "$postinst" << EOF
 mkdir --parents "$PATH_ICON"
-ln --symbolic "$PATH_GAME"/$APP_MAIN_ICON "$PATH_ICON/$GAME_ID.png"
+ln --symbolic "$PATH_GAME/$APP_MAIN_ICON" "$PATH_ICON/$GAME_ID.png"
 EOF
 
 cat > "$prerm" << EOF
@@ -165,10 +151,10 @@ rm "$PATH_ICON/$GAME_ID.png"
 rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON"
 EOF
 
-write_metadata 'PKG_MAIN'
+write_metadata 'PKG_DATA'
 rm "$postinst" "$prerm"
-write_metadata 'PKG_32' 'PKG_64'
-build_pkg 'PKG_MAIN' 'PKG_32' 'PKG_64'
+write_metadata 'PKG_BIN32' 'PKG_BIN64'
+build_pkg      'PKG_BIN32' 'PKG_BIN64' 'PKG_DATA'
 
 # Clean up
 
@@ -178,8 +164,8 @@ rm --recursive "${PLAYIT_WORKDIR}"
 
 printf '\n'
 printf '32-bit:'
-print_instructions "$PKG_MAIN_PKG" "$PKG_32_PKG"
+print_instructions "$PKG_DATA_PKG" "$PKG_BIN32_PKG"
 printf '64-bit:'
-print_instructions "$PKG_MAIN_PKG" "$PKG_64_PKG"
+print_instructions "$PKG_DATA_PKG" "$PKG_BIN64_PKG"
 
 exit 0
