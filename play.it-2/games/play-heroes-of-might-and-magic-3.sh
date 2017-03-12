35899ed5d4d2a0c8251185a7f3355dda7f24892d
vv221 <vv221@dotslashplay.it>
2017-03-12 14:09:52 +0100

    2.0 - Heroes of Might and Magic III - Drop '32on64' architecture keyword

--- play.it-2/games/play-heroes-of-might-and-magic-3.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-3.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170116.1
+script_version=20170312.1
 
 # Set game-specific variables
 
@@ -95,7 +95,7 @@ PKG_MUSIC_DESCRIPTION='music'
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
-PKG_BIN_ARCH='32on64'
+PKG_BIN_ARCH='32'
 PKG_BIN_DEPS_DEB="$PKG_DATA_ID, $PKG_MUSIC_ID, winetricks, wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386"
 PKG_BIN_DEPS_ARCH="$PKG_DATA_ID $PKG_MUSIC_ID winetricks wine"
 
