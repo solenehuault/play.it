3620a48cd286b9570291465d4b69e747d9b36274
vv221 <vv221@dotslashplay.it>
2017-03-12 14:00:41 +0100

    2.0 - Legen of Grimrock - Update dependencies list to reflect new Arch Linux architecture management

--- play.it-2/games/play-legend-of-grimrock.sh
+++ play.it-2/games/play-legend-of-grimrock.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170219.2
+script_version=20170312.1
 
 # Set game-specific variables
 
@@ -72,11 +72,11 @@ PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
 PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libgl1-mesa-glx | libgl1, libopenal1, libsdl2-2.0-0, libfreeimage3, libminizip1"
-PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID libgl openal sdl2 freeimage minizip"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-libgl lib32-openal lib32-sdl2 freeimage minizip"
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
-PKG_BIN64_DEPS_ARCH="$PKG_BIN32_DEPS_ARCH"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID libgl openal sdl2 freeimage minizip"
 
 # Load common functions
 
