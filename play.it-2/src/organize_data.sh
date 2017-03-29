d5c20155acc5daa4dd238566b8d097fedfb9a00f
vv221 <vv221@dotslashplay.it>
2017-03-29 21:15:33 +0200

    2.0 library - organize_data_generic - Fix shell globbing behaviour

--- play.it-2/src/organize_data.sh
+++ play.it-2/src/organize_data.sh
@@ -21,13 +21,14 @@ organize_data() {
 # NEEDED VARS: $PKG, $PKG_PATH, $PLAYIT_WORKDIR
 # CALLED BY: organize_data
 organize_data_generic() {
-	local archive_path="$(eval echo \$ARCHIVE_${1}_PATH)"
+	local archive_path="$(eval echo \"\$ARCHIVE_${1}_PATH\")"
+	local archive_files="$(eval echo \"\$ARCHIVE_${1}_FILES\")"
 	if [ "$archive_path" ] && [ -e "$PLAYIT_WORKDIR/gamedata/$archive_path" ]; then
 		local pkg_path="$(eval echo \$${PKG}_PATH)${2}"
 		mkdir --parents "$pkg_path"
 		(
 			cd "$PLAYIT_WORKDIR/gamedata/$archive_path"
-			for file in $(eval echo \$ARCHIVE_${1}_FILES); do
+			for file in $archive_files; do
 				if [ -e "$file" ]; then
 					cp --recursive --link --parents "$file" "$pkg_path"
 					rm --recursive "$file"
