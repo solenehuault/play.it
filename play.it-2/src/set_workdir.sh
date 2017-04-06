e7fbc79827a626400aa152213a741d77f2fc4cc2
vv221 <vv221@dotslashplay.it>
2017-04-06 19:31:33 +0200

    2.0 library - set_workdir_workdir - Use current value of $XDG_RUNTIME_DIR if set instead of hardcoded default value

--- play.it-2/src/set_workdir.sh
+++ play.it-2/src/set_workdir.sh
@@ -25,12 +25,13 @@ set_workdir_workdir() {
 	local workdir_name=$(mktemp --dry-run ${GAME_ID}.XXXXX)
 	local archive_size=$(eval echo \$${ARCHIVE}_SIZE)
 	local needed_space=$(($archive_size * 2))
+	[ "$XDG_RUNTIME_DIR" ] || XDG_RUNTIME_DIR="/run/user/$(id -u)"
 	[ "$XDG_CACHE_HOME" ] || XDG_CACHE_HOME="$HOME/.cache"
-	local free_space_run=$(df --output=avail /run/user/$(id -u) | tail --lines=1)
+	local free_space_run=$(df --output=avail "$XDG_RUNTIME_DIR" | tail --lines=1)
 	local free_space_tmp=$(df --output=avail /tmp | tail --lines=1)
 	local free_space_cache=$(df --output=avail "$XDG_CACHE_HOME" | tail --lines=1)
 	if [ $free_space_run -ge $needed_space ]; then
-		export PLAYIT_WORKDIR="/run/user/$(id -u)/play.it/$workdir_name"
+		export PLAYIT_WORKDIR="$XDG_RUNTIME_DIR/play.it/$workdir_name"
 	elif [ $free_space_tmp -ge $needed_space ]; then
 		export PLAYIT_WORKDIR="/tmp/play.it/$workdir_name"
 	elif [ $free_space_cache -ge $needed_space ]; then
