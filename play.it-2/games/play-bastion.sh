56997fef2c5b82d2a66b2b7b55d28f8a4a2f50c8
vv221 <vv221@dotslashplay.it>
2017-04-11 09:51:33 +0200

    2.0 - Bastion - Release version

--- play.it-2/games/play-bastion.sh
+++ play.it-2/games/play-bastion.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170410.1
 
 # Set game-specific variables
 
@@ -47,18 +47,21 @@ ARCHIVE_HUMBLE_SIZE='1100000'
 ARCHIVE_HUMBLE_VERSION='1.2.20120620-humble120620'
 ARCHIVE_HUMBLE_TYPE='nix_stage1'
 
-ARCHIVE_ALL='instarchive_all'
-ARCHIVE_32='instarchive_linux_x86'
-ARCHIVE_64='instarchive_linux_x86_64'
+ARCHIVE_BIN32='instarchive_linux_x86'
+ARCHIVE_BIN64='instarchive_linux_x86_64'
+ARCHIVE_DATA='instarchive_all'
 
 ARCHIVE_DOC_PATH='.'
 ARCHIVE_DOC_FILES='./README.linux'
-ARCHIVE_GAME_32_PATH='.'
-ARCHIVE_GAME_32_FILES='./Bastion.bin.x86 ./lib'
-ARCHIVE_GAME_64_PATH='.'
-ARCHIVE_GAME_64_FILES='./Bastion.bin.x86_64 ./lib64'
-ARCHIVE_GAME_MAIN_PATH='.'
-ARCHIVE_GAME_MAIN_FILES='./Bastion.exe ./Bastion.png ./*.dll ./Content ./mono ./*.config'
+
+ARCHIVE_GAME_BIN32_PATH='.'
+ARCHIVE_GAME_BIN32_FILES='./Bastion.bin.x86 ./lib'
+
+ARCHIVE_GAME_BIN64_PATH='.'
+ARCHIVE_GAME_BIN64_FILES='./Bastion.bin.x86_64 ./lib64'
+
+ARCHIVE_GAME_DATA_PATH='.'
+ARCHIVE_GAME_DATA_FILES='./Bastion.exe ./Bastion.png ./*.dll ./Content ./mono ./*.config'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE_32='Bastion.bin.x86'
@@ -66,18 +69,16 @@ APP_MAIN_EXE_64='Bastion.bin.x86_64'
 APP_MAIN_ICON='Bastion.png'
 APP_MAIN_ICON_RES='256x256'
 
-PKG_MAIN_ID="${GAME_ID}-common"
-PKG_MAIN_DESCRIPTION='arch-independant data'
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
 
-PKG_32_ARCH='32'
-PKG_32_CONFLICTS_DEB="$GAME_ID"
-PKG_32_DEPS_DEB="$PKG_MAIN_ID, libc6, libstdc++6, libgcc1, libsdl1.2debian"
-PKG_32_DEPS_ARCH="$PKG_MAIN_ID lib32-glu lib32-gcc-libs"
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libgcc1, libsdl1.2debian"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-glu lib32-gcc-libs"
 
-PKG_64_ARCH='64'
-PKG_64_CONFLICTS_DEB="$GAME_ID"
-PKG_64_DEPS_DEB="$PKG_32_DEPS_DEB"
-PKG_64_DEPS_ARCH="$PKG_MAIN_ID glu gcc-libs"
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID glu gcc-libs"
 
 # Load common functions
 
@@ -114,45 +115,49 @@ fetch_args "$@"
 set_source_archive 'ARCHIVE_HUMBLE'
 check_deps
 set_common_paths
-PATH_ICON="$PKG_MAIN_PATH$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_HUMBLE'
+file_checksum "$SOURCE_ARCHIVE"
 
 # Extract game data
 
-set_workdir 'PKG_MAIN' 'PKG_32' 'PKG_64'
+set_workdir 'PKG_DATA' 'PKG_BIN32' 'PKG_BIN64'
 extract_data_from "$SOURCE_ARCHIVE"
-
 ARCHIVE='ARCHIVE'
 ARCHIVE_TYPE='nix_stage2'
-extract_data_from "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_ALL" "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_32" "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_64"
+extract_data_from "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_DATA"
+extract_data_from "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_BIN32"
+extract_data_from "$PLAYIT_WORKDIR/gamedata/$ARCHIVE_BIN64"
 
-PKG='PKG_32'
+PKG='PKG_BIN32'
 organize_data 'GAME_32' "$PATH_GAME"
-PKG='PKG_64'
+
+PKG='PKG_BIN64'
 organize_data 'GAME_64' "$PATH_GAME"
-PKG='PKG_MAIN'
-organize_data 'GAME_MAIN' "$PATH_GAME"
-organize_data 'DOC' "$PATH_DOC"
+
+PKG='PKG_DATA'
+organize_data 'DOC'       "$PATH_DOC"
+organize_data 'GAME_DATA' "$PATH_GAME"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-PKG='PKG_32'
+PKG='PKG_BIN32'
 APP_MAIN_EXE="$APP_MAIN_EXE_32"
-write_bin 'APP_MAIN'
+write_bin     'APP_MAIN'
 write_desktop 'APP_MAIN'
 
-PKG='PKG_64'
+PKG='PKG_BIN64'
 APP_MAIN_EXE="$APP_MAIN_EXE_64"
-write_bin 'APP_MAIN'
+write_bin     'APP_MAIN'
 write_desktop 'APP_MAIN'
 
 # Build package
 
+PATH_ICON="$PKG_MAIN_PATH$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
+
 cat > "$postinst" << EOF
 mkdir --parents "$PATH_ICON"
-ln --symbolic "$PATH_GAME"/$APP_MAIN_ICON "$PATH_ICON/$GAME_ID.png"
+ln --symbolic "$PATH_GAME/$APP_MAIN_ICON" "$PATH_ICON/$GAME_ID.png"
 EOF
 
 cat > "$prerm" << EOF
@@ -160,10 +165,10 @@ rm "$PATH_ICON/$GAME_ID.png"
 rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON"
 EOF
 
-write_metadata 'PKG_MAIN'
+write_metadata 'PKG_DATA'
 rm "$postinst" "$prerm"
-write_metadata 'PKG_32' 'PKG_64'
-build_pkg 'PKG_MAIN' 'PKG_32' 'PKG_64'
+write_metadata 'PKG_BIN32' 'PKG_BIN64'
+build_pkg      'PKG_BIN32' 'PKG_BIN64' 'PKG_DATA'
 
 # Clean up
 
@@ -171,9 +176,10 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-printf '\n32-bit:'
-print_instructions "$PKG_MAIN_PKG" "$PKG_32_PKG"
-printf '\n64-bit:'
-print_instructions "$PKG_MAIN_PKG" "$PKG_64_PKG"
+printf '\n'
+printf '32-bit:'
+print_instructions "$PKG_DATA_PKG" "$PKG_BIN32_PKG"
+printf '64-bit:'
+print_instructions "$PKG_DATA_PKG" "$PKG_BIN64_PKG"
 
 exit 0
