cade1f5438caea67377ff65fa4dfec41de5a95f7
vv221 <vv221@dotslashplay.it>
2017-04-18 20:43:31 +0200

    2.0 library - Merge multiple small source files into src/common.sh

--- play.it-2/src/common.sh
+++ play.it-2/src/common.sh
@@ -33,3 +33,70 @@ set_arch() {
 	esac
 }
 
+# test the validity of the argument given to parent function
+# USAGE: testvar $var_name $pattern
+testvar() {
+	if [ -z "$(echo "$1" | grep ^${2})" ]; then
+		return 1
+	fi
+}
+
+# set defaults rights on files (755 for dirs & 644 for regular files)
+# USAGE: fix_rights $dir[…]
+fix_rights() {
+	for dir in "$@"; do
+		if [ ! -d "$dir" ]; then
+			return 1
+		fi
+		find "$dir" -type d -exec chmod 755 '{}' +
+		find "$dir" -type f -exec chmod 644 '{}' +
+	done
+}
+
+# print a localized error message
+# USAGE: print_error
+print_error() {
+	case ${LANG%_*} in
+		('fr')
+			printf '\n\033[1;31mErreur :\033[0m\n'
+		;;
+		('en'|*)
+			printf '\n\033[1;31mError:\033[0m\n'
+		;;
+	esac
+}
+
+# convert files name to lower case
+# USAGE: tolower $dir[…]
+tolower() {
+	for dir in "$@"; do
+		if [ ! -d "$dir" ]; then
+			return 1
+		fi
+		find "$dir" -depth | while read file; do
+			newfile="${file%/*}/$(echo "${file##*/}" | tr [:upper:] [:lower:])"
+			if [ ! -e "$newfile" ] && [ "$file" != "$dir" ]; then
+				mv "$file" "$newfile"
+			fi
+		done
+	done
+}
+
+# display an error if a function has been called with invalid arguments
+# USAGE: liberror $var_name $calling_function
+liberror() {
+	local var="$1"
+	local value="$(eval echo \$$var)"
+	local func="$2"
+	print_error
+	case ${LANG%_*} in
+		('fr')
+			printf 'valeur incorrecte pour %s appelée par %s : %s\n' "$var" "$func" "$value"
+		;;
+		('en'|*)
+			printf 'invalid value for %s called by %s: %s\n' "$var" "$func" "$value"
+		;;
+	esac
+	return 1
+}
+
