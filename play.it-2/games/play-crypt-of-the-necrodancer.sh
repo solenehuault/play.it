5768495b92cb0422d30cdb3224153f6a89e2c5b5
vv221 <vv221@dotslashplay.it>
2017-05-02 20:29:55 +0200

    2.0 - Crypt of the NecroDancer - Release version

--- play.it-2/games/play-crypt-of-the-necrodancer.sh
+++ play.it-2/games/play-crypt-of-the-necrodancer.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170502.1
 
 # Set game-specific variables
 
@@ -48,8 +48,10 @@ ARCHIVE_GOG_VERSION='1.27-gog2.3.0.5'
 
 ARCHIVE_DOC1_PATH='data/noarch/docs'
 ARCHIVE_DOC1_FILES='./*'
+
 ARCHIVE_DOC2_PATH='data/noarch/game/'
 ARCHIVE_DOC2_FILES='./license.txt'
+
 ARCHIVE_GAME_PATH='data/noarch/game'
 ARCHIVE_GAME_FILES='./*'
 
@@ -59,11 +61,11 @@ DATA_FILES='./NecroDancer'
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='NecroDancer'
 APP_MAIN_ICON='data/noarch/support/icon.png'
-APP_MAIN_ICON_RES='256x256'
+APP_MAIN_ICON_RES='256'
 
 PKG_MAIN_ARCH='32'
-PKG_MAIN_DEPS_DEB="libglu1-mesa | libglu1, libopenal1, libfftw3-single3, libglfw2, libgsm1, libsamplerate0, libschroedinger-1.0-0, libtag1v5-vanilla | libtag1-vanilla, libyaml-0-2, libvorbis0a"
-PKG_MAIN_DEPS_ARCH="lib32-glibc lib32-libogg lib32-libvorbis lib32-libx11 lib32-libxau lib32-libxcb lib32-libxdmcp lib32-libxext lib32-libgl lib32-openal"
+PKG_MAIN_DEPS_DEB='libglu1-mesa | libglu1, libopenal1, libfftw3-single3, libglfw2, libgsm1, libsamplerate0, libschroedinger-1.0-0, libtag1v5-vanilla | libtag1-vanilla, libyaml-0-2, libvorbis0a'
+PKG_MAIN_DEPS_ARCH='lib32-glibc lib32-libogg lib32-libvorbis lib32-libx11 lib32-libxau lib32-libxcb lib32-libxdmcp lib32-libxext lib32-libgl lib32-openal'
 
 # Load common functions
 
@@ -83,26 +85,9 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
 # Set source archive
 
 set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
 
 # Extract game data
 
@@ -113,22 +98,21 @@ organize_data 'DOC1' "$PATH_DOC"
 organize_data 'DOC2' "$PATH_DOC"
 organize_data 'GAME' "$PATH_GAME"
 
-mkdir --parents "$PKG_MAIN_PATH/$PATH_ICON"
+PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
+mkdir --parents "${PKG_MAIN_PATH}${PATH_ICON}"
 mv "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON" "$PKG_MAIN_PATH/$PATH_ICON/$GAME_ID.png"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-PKG='PKG_MAIN'
-write_bin 'APP_MAIN'
+write_bin     'APP_MAIN'
 write_desktop 'APP_MAIN'
 
 # Build package
 
 write_metadata 'PKG_MAIN'
-
-build_pkg 'PKG_MAIN'
+build_pkg      'PKG_MAIN'
 
 # Clean up
 
