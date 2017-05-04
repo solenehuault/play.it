2ca0d7486f3018ba548fccbe37f837ded91f4d32
vv221 <vv221@dotslashplay.it>
2017-05-04 22:50:39 +0200

    2.0 library - New alias function write_launcher() calling write_bin() and write_desktop() + Rename launchers-related source files

--- play.it-2/src/launchers.sh
+++ play.it-2/src/launchers.sh
@@ -1,3 +1,11 @@
+# alias calling write_bin() and write_desktop()
+# USAGE: write_launcher $app[…]
+# CALLS: write_bin write_dekstop
+write_launcher() {
+	write_bin $@
+	write_desktop $@
+}
+
 # write launcher script
 # USAGE: write_bin $app
 # NEEDED VARS: $PKG $APP_ID $APP_TYPE $PATH_BIN $APP_EXE $APP_OPTIONS $APP_LIBS
@@ -266,5 +274,56 @@ write_bin_run() {
 
 	exit 0
 	EOF
+}
+
+# write menu entry
+# USAGE: write_desktop $app
+# NEEDED VARS: $app_TYPE, $app_ID, $app_NAME, $app_CAT, PKG_PATH, PATH_DESK
+# CALLS: liberror
+write_desktop() {
+	local app
+	for app in $@; do
+		testvar "$app" 'APP' || liberror 'app' 'write_desktop'
+		local type="$(eval echo \$${app}_TYPE)"
+		if [ "$winecfg_desktop" != 'done' ] && [ "$type" = 'wine' ]; then
+			winecfg_desktop='done'
+			write_desktop_winecfg
+		fi
+		local id="$(eval echo \$${app}_ID)"
+		if [ -z "$id" ]; then
+			id="$GAME_ID"
+		fi
+		local name="$(eval echo \$${app}_NAME)"
+		if [ -z "$name" ]; then
+			name="$GAME_NAME"
+		fi
+		local cat="$(eval echo \$${app}_CAT)"
+		if [ -z "$cat" ]; then
+			cat='Game'
+		fi
+		local target="${PKG_PATH}${PATH_DESK}/${id}.desktop"
+		mkdir --parents "${target%/*}"
+		cat > "${target}" <<- EOF
+		[Desktop Entry]
+		Version=1.0
+		Type=Application
+		Name=$name
+		Icon=$id
+		Exec=$id
+		Categories=$cat
+		EOF
+	done
+}
+
+# write winecfg launcher script
+# USAGE: write_desktop_winecfg
+# NEEDED VARS: GAME_ID
+# CALLS: write_desktop
+write_desktop_winecfg() {
+	APP_WINECFG_ID="${GAME_ID}_winecfg"
+	APP_WINECFG_NAME="$GAME_NAME - WINE configuration"
+	APP_WINECFG_CAT='Settings'
+	write_desktop 'APP_WINECFG'
+	sed --in-place 's/Icon=.\+/Icon=winecfg/' "${PKG_PATH}${PATH_DESK}/${APP_WINECFG_ID}.desktop"
 }
 
