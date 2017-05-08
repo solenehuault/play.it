02aba52f0bacc7bb5b615336f27197b9c06526a5
vv221 <vv221@dotslashplay.it>
2017-05-08 17:07:47 +0200

    2.0 - Arena - Drop obsolete modification of launcher script

--- play.it-2/games/play-arena.sh
+++ play.it-2/games/play-arena.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170508.1
+script_version=20170508.2
 
 # Set game-specific variables
 
@@ -123,7 +123,6 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 PKG='PKG_BIN'
 write_launcher 'APP_MAIN'
-sed -i "s/imgmount d $GAME_IMAGE -t iso -fs iso/mount d $GAME_IMAGE -t cdrom/" "${PKG_BIN_PATH}${PATH_BIN}/$GAME_ID"
 
 # Build package
 
