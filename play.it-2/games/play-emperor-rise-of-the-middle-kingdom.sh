af2e9ab3bb251ff074b357c48995f497430e3f41
vv221 <vv221@dotslashplay.it>
2017-05-14 15:57:22 +0200

    2.0 - Emperor: Rise of the Middle Kingdom - Release version

--- play.it-2/games/play-emperor-rise-of-the-middle-kingdom.sh
+++ play.it-2/games/play-emperor-rise-of-the-middle-kingdom.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170514.1
 
 # Set game-specific variables
 
 GAME_ID='emperor-rise-of-the-middle-kingdom'
 GAME_NAME='Emperor: Rise of the Middle Kingdom'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='setup_emperor_rise_of_the_middle_kingdom_2.0.0.2.exe'
 ARCHIVE_GOG_MD5='5e50e84c028a85eafe5dd5f2aa277fea'
@@ -48,10 +50,12 @@ ARCHIVE_GOG_VERSION='1.0.1.0-gog2.0.0.2'
 
 ARCHIVE_DOC_PATH='app'
 ARCHIVE_DOC_FILES='./*.txt ./*.pdf'
+
 ARCHIVE_GAME_BIN_PATH='app'
-ARCHIVE_GAME_BIN_FILES='./*.exe ./binkw32.dll ./ijl10.dll ./mss32.dll ./sierrapt.dll'
+ARCHIVE_GAME_BIN_FILES='./*.exe ./*.cfg ./*.inf ./emperor.ini ./binkw32.dll ./ijl10.dll ./mss32.dll ./sierrapt.dll'
+
 ARCHIVE_GAME_DATA_PATH='app'
-ARCHIVE_GAME_DATA_FILES='./*.cfg ./*.eng ./*.inf ./audio ./binks ./campaigns ./cities ./data ./dragon.ico ./emperor.ini ./model ./mp3dec.asi ./mssds3dh.m3d ./mssrsx.m3d ./res ./save'
+ARCHIVE_GAME_DATA_FILES='./*.eng ./audio ./binks ./campaigns ./cities ./data ./dragon.ico ./model ./mp3dec.asi ./mssds3dh.m3d ./mssrsx.m3d ./res ./save'
 
 CONFIG_FILES='./*.cfg ./*.ini'
 DATA_DIRS='./save'
@@ -60,12 +64,14 @@ APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='emperor.exe'
 APP_MAIN_ICON1='emperor.exe'
 APP_MAIN_ICON2='dragon.ico'
-APP_MAIN_ICON_RES='16x16 32x32'
+APP_MAIN_ICON_RES='16 32'
 
 APP_EDIT_ID="${GAME_ID}_editor"
 APP_EDIT_NAME="$GAME_NAME - Editor"
 APP_EDIT_TYPE='wine'
 APP_EDIT_EXE='emperoredit.exe'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN'
 
 PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
@@ -92,29 +98,8 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_BIN' 'PKG_DATA'
 extract_data_from "$SOURCE_ARCHIVE"
 
 PKG='PKG_BIN'
@@ -124,41 +109,37 @@ PKG='PKG_DATA'
 organize_data 'GAME_DATA' "$PATH_GAME"
 organize_data 'DOC'       "$PATH_DOC"
 
-if [ "$NO_ICON" = '0' ]; then
-	extract_icon_from "${PKG_BIN_PATH}${PATH_GAME}/$APP_MAIN_ICON1"
-	extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
-	extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
-	rm "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
-	rm "$PLAYIT_WORKDIR/icons/$APP_MAIN_ICON1"*32x32*.png
-	sort_icons 'APP_MAIN'
-	rm --recursive "$PLAYIT_WORKDIR/icons"
-fi
+extract_icon_from "${PKG_BIN_PATH}${PATH_GAME}/$APP_MAIN_ICON1"
+extract_icon_from "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
+extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
+rm "${PKG_DATA_PATH}${PATH_GAME}/$APP_MAIN_ICON2"
+rm "$PLAYIT_WORKDIR/icons/$APP_MAIN_ICON1"*32x32*.png
+sort_icons 'APP_MAIN'
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-write_bin     'APP_MAIN' 'APP_EDIT'
-write_desktop 'APP_MAIN' 'APP_EDIT'
+write_launcher 'APP_MAIN' 'APP_EDIT'
 
 # Build package
 
 cat > "$postinst" << EOF
 for res in $APP_MAIN_ICON_RES; do
-	ln --symbolic "$GAME_ID.png" "$PATH_ICON_BASE/\$res/apps/$APP_EDIT_ID.png"
+	ln --symbolic "$GAME_ID.png" "$PATH_ICON_BASE/\${res}x\${res}/apps/$APP_EDIT_ID.png"
 done
 EOF
 
 cat > "$prerm" << EOF
 for res in $APP_MAIN_ICON_RES; do
-	rm "$PATH_ICON_BASE/\$res/apps/$APP_EDIT_ID.png"
+	rm "$PATH_ICON_BASE/\${res}x\${res}/apps/$APP_EDIT_ID.png"
 done
 EOF
 
 write_metadata 'PKG_DATA'
 rm "$postinst" "$prerm"
 write_metadata 'PKG_BIN'
-build_pkg      'PKG_BIN' 'PKG_DATA'
+build_pkg
 
 # Clean up
 
