a31ce04f91abfaa18250cd531b799ff486312590
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-16 17:17:13 +0200

    2.0 - Heroes of Might and Magic 4 - Release version

--- play.it-2/games/play-heroes-of-might-and-magic-4.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-4.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170516.1
 
 # Set game-specific variables
 
 GAME_ID='heroes-of-might-and-magic-4'
 GAME_NAME='Heroes of Might and Magic IV'
+
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
 
 ARCHIVE_GOG_EN='setup_homm4_complete_2.0.0.12.exe'
 ARCHIVE_GOG_EN_MD5='74de66eb408bb2916dd0227781ba96dc'
@@ -53,14 +55,19 @@ ARCHIVE_GOG_FR_SIZE='1100000'
 
 ARCHIVE_DOC1_PATH='tmp'
 ARCHIVE_DOC1_FILES='./*eula.txt'
+
 ARCHIVE_DOC2_PATH='app'
 ARCHIVE_DOC2_FILES='./*.chm ./*.pdf ./*.txt'
+
 ARCHIVE_GAME_BIN_PATH='app'
 ARCHIVE_GAME_BIN_FILES='./*.exe ./binkw32.dll ./drvmgt.dll ./mss32.dll ./mp3dec.asi data/*.dll'
+
 ARCHIVE_GAME_STORM_PATH='app'
 ARCHIVE_GAME_STORM_FILES='data/storm_override.h4r data/storm.aop'
+
 ARCHIVE_GAME_MUSIC_PATH='app'
 ARCHIVE_GAME_MUSIC_FILES='data/music.h4r'
+
 ARCHIVE_GAME_DATA_PATH='app'
 ARCHIVE_GAME_DATA_FILES='./data ./maps'
 
@@ -72,14 +79,16 @@ APP_WINETRICKS='ddr=gdi vd=1280x1024'
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='./heroes4.exe'
 APP_MAIN_ICON='./heroes4.exe'
-APP_MAIN_ICON_RES='16x16 32x32'
+APP_MAIN_ICON_RES='16 32'
 
 APP_EDITOR_TYPE='wine'
 APP_EDITOR_ID="${GAME_ID}_edit"
 APP_EDITOR_EXE='./campaign_editor.exe'
 APP_EDITOR_ICON='./campaign_editor.exe'
-APP_EDITOR_ICON_RES='48x48 64x64'
+APP_EDITOR_ICON_RES='48 64'
 APP_EDITOR_NAME="$GAME_NAME - campaign editor"
+
+PACKAGES_LIST='PKG_STORM PKG_MUSIC PKG_DATA PKG_BIN'
 
 PKG_STORM_ID="${GAME_ID}-storm"
 PKG_STORM_DESCRIPTION='Gathering Storm'
@@ -112,29 +121,8 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG_EN' 'ARCHIVE_GOG_FR'
-check_deps
-set_common_paths
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG_EN' 'ARCHIVE_GOG_FR'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_BIN' 'PKG_STORM' 'PKG_MUSIC' 'PKG_DATA'
 extract_data_from "$SOURCE_ARCHIVE"
 
 PKG='PKG_BIN'
@@ -147,38 +135,30 @@ PKG='PKG_MUSIC'
 organize_data 'GAME_MUSIC' "$PATH_GAME"
 
 PKG='PKG_DATA'
-organize_data 'GAME_DATA' "$PATH_GAME"
 organize_data 'DOC1'      "$PATH_DOC"
 organize_data 'DOC2'      "$PATH_DOC"
+organize_data 'GAME_DATA' "$PATH_GAME"
 
-if [ "$NO_ICON" = '0' ]; then
-	(
-		cd "${PKG_BIN_PATH}${PATH_GAME}"
-
-		extract_icon_from "$APP_MAIN_ICON"
-		extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
-		sort_icons 'APP_MAIN'
-		rm --recursive "$PLAYIT_WORKDIR/icons"
-
-		extract_icon_from "$APP_EDITOR_ICON"
-		extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
-		sort_icons 'APP_EDITOR'
-		rm --recursive "$PLAYIT_WORKDIR/icons"
-	)
-fi
+extract_and_sort_icons_from 'APP_MAIN'
+extract_and_sort_icons_from 'APP_EDITOR'
+(
+        cd "$PKG_BIN_PATH"
+	cp --link --parents --recursive "./$PATH_ICON_BASE" "$PKG_DATA_PATH"
+        rm --recursive "./$PATH_ICON_BASE"
+        rmdir --ignore-fail-on-non-empty --parents "./${PATH_ICON_BASE%/*}"
+)
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_bin     'APP_MAIN' 'APP_EDITOR'
-write_desktop 'APP_MAIN' 'APP_EDITOR'
+write_launcher 'APP_MAIN' 'APP_EDITOR'
 
 # Build package
 
-write_metadata 'PKG_BIN' 'PKG_STORM' 'PKG_MUSIC' 'PKG_DATA'
-build_pkg      'PKG_BIN' 'PKG_STORM' 'PKG_MUSIC' 'PKG_DATA'
+write_metadata
+build_pkg
 
 # Clean up
 
