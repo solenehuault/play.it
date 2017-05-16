d01a3eac64336a02cac7f59428b14cc6b3493367
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-16 17:15:39 +0200

    2.0 - Heroes of Might and Magic 3 - Release version

--- play.it-2/games/play-heroes-of-might-and-magic-3.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-3.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170516.1
 
 # Set game-specific variables
 
 GAME_ID='heroes-of-might-and-magic-3'
 GAME_NAME='Heroes of Might and Magic III'
+
+ARCHIVES_LIST='ARCHIVE_GOG_EN ARCHIVE_GOG_FR'
 
 ARCHIVE_GOG_EN='setup_homm3_complete_2.0.0.16.exe'
 ARCHIVE_GOG_EN_MD5='263d58f8cc026dd861e9bbcadecba318'
@@ -55,14 +57,19 @@ ARCHIVE_GOG_FR_SIZE='1100000'
 
 ARCHIVE_DOC1_PATH='tmp'
 ARCHIVE_DOC1_FILES='./*eula.txt'
+
 ARCHIVE_DOC2_PATH='app'
 ARCHIVE_DOC2_FILES='./eula ./*.cnt ./*.hlp ./*.pdf ./*.txt'
+
 ARCHIVE_GAME_BIN_PATH='app'
 ARCHIVE_GAME_BIN_FILES='./*.exe ./binkw32.dll ./ifc20.dll ./ifc21.dll ./mcp.dll ./mp3dec.asi ./mss32.dll ./smackw32.dll'
+
 ARCHIVE_GAME_DATA_PATH='app'
 ARCHIVE_GAME_DATA_FILES='./data ./maps'
+
 ARCHIVE_GAME_MUSIC_PATH='app'
 ARCHIVE_GAME_MUSIC_FILES='./mp3'
+
 ARCHIVE_GAME_PATCH_PATH='tmp'
 ARCHIVE_GAME_PATCH_FILES='./heroes3.exe'
 
@@ -70,24 +77,28 @@ CONFIG_DIRS='./config'
 DATA_DIRS='./games ./maps ./random_maps'
 DATA_FILES='data/h3ab_bmp.lod data/h3ab_spr.lod data/h3bitmap.lod data/h3sprite.lod'
 
+APP_WINETRICKS='vd=800x600'
+
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='./heroes3.exe'
 APP_MAIN_ICON='./heroes3.exe'
-APP_MAIN_ICON_RES='16x16 32x32 48x48 64x64'
+APP_MAIN_ICON_RES='16 32 48 64'
 
 APP_EDITOR_MAP_TYPE='wine'
 APP_EDITOR_MAP_ID="${GAME_ID}_map-editor"
 APP_EDITOR_MAP_EXE='./h3maped.exe'
 APP_EDITOR_MAP_ICON='./h3maped.exe'
-APP_EDITOR_MAP_ICON_RES='16x16 32x32 48x48 64x64'
+APP_EDITOR_MAP_ICON_RES='16 32 48 64'
 APP_EDITOR_MAP_NAME="$GAME_NAME - map editor"
 
 APP_EDITOR_CAMPAIGN_TYPE='wine'
 APP_EDITOR_CAMPAIGN_ID="${GAME_ID}_campaign-editor"
 APP_EDITOR_CAMPAIGN_EXE='./h3ccmped.exe'
 APP_EDITOR_CAMPAIGN_ICON='./h3ccmped.exe'
-APP_EDITOR_CAMPAIGN_ICON_RES='16x16 32x32 48x48 64x64'
+APP_EDITOR_CAMPAIGN_ICON_RES='16 32 48 64'
 APP_EDITOR_CAMPAIGN_NAME="$GAME_NAME - campaign editor"
+
+PACKAGES_LIST='PKG_MUSIC PKG_DATA PKG_BIN'
 
 PKG_MUSIC_ID="${GAME_ID}-music"
 PKG_MUSIC_DESCRIPTION='music'
@@ -117,42 +128,20 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
+# Load patch if using GOG English archive
 
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG_EN' 'ARCHIVE_GOG_FR'
 if [ "${SOURCE_ARCHIVE##*/}" = "$ARCHIVE_GOG_EN" ]; then
 	MAIN_ARCHIVE="$SOURCE_ARCHIVE"
-	unset SOURCE_ARCHIVE
+	unset ARCHIVE SOURCE_ARCHIVE
 	set_source_archive 'ARCHIVE_GOG_EN_PATCH'
+	file_checksum "$SOURCE_ARCHIVE"
 	PATCH_ARCHIVE="$SOURCE_ARCHIVE"
 	SOURCE_ARCHIVE="$MAIN_ARCHIVE"
-	unset ARCHIVE
-fi
-check_deps
-set_common_paths
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG_EN' 'ARCHIVE_GOG_FR'
-if [ "${SOURCE_ARCHIVE##*/}" = "$ARCHIVE_GOG_EN" ]; then
-	unset ARCHIVE
-	file_checksum "$PATCH_ARCHIVE" 'ARCHIVE_GOG_EN_PATCH'
 	ARCHIVE='ARCHIVE_GOG_EN'
 fi
-check_deps
 
 # Extract game data
 
-set_workdir 'PKG_BIN' 'PKG_MUSIC' 'PKG_DATA'
 extract_data_from "$SOURCE_ARCHIVE"
 if [ "${SOURCE_ARCHIVE##*/}" = "$ARCHIVE_GOG_EN" ]; then
 	extract_data_from "$PATCH_ARCHIVE"
@@ -170,43 +159,27 @@ organize_data 'DOC1'      "$PATH_DOC"
 organize_data 'DOC2'      "$PATH_DOC"
 organize_data 'GAME_DATA' "$PATH_GAME"
 
-if [ "$NO_ICON" = '0' ]; then
-	(
-		cd "${PKG_BIN_PATH}${PATH_GAME}"
-
-		extract_icon_from "$APP_MAIN_ICON"
-		extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
-		sort_icons 'APP_MAIN'
-		rm --recursive "$PLAYIT_WORKDIR/icons"
-
-		extract_icon_from "$APP_EDITOR_MAP_ICON"
-		extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
-		sort_icons 'APP_EDITOR_MAP'
-		rm --recursive "$PLAYIT_WORKDIR/icons"
-
-		extract_icon_from "$APP_EDITOR_CAMPAIGN_ICON"
-		extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
-		sort_icons 'APP_EDITOR_CAMPAIGN'
-		rm --recursive "$PLAYIT_WORKDIR/icons"
-	)
-fi
+extract_and_sort_icons_from 'APP_MAIN'
+extract_and_sort_icons_from 'APP_EDITOR_MAP'
+extract_and_sort_icons_from 'APP_EDITOR_CAMPAIGN'
+(
+        cd "$PKG_BIN_PATH"
+        cp --link --parents --recursive "./$PATH_ICON_BASE" "$PKG_DATA_PATH"
+        rm --recursive "./$PATH_ICON_BASE"
+        rmdir --ignore-fail-on-non-empty --parents "./${PATH_ICON_BASE%/*}"
+)
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
 PKG='PKG_BIN'
-write_bin     'APP_MAIN' 'APP_EDITOR_MAP' 'APP_EDITOR_CAMPAIGN'
-write_desktop 'APP_MAIN' 'APP_EDITOR_MAP' 'APP_EDITOR_CAMPAIGN'
-
-for file in "${PKG_BIN_PATH}${PATH_BIN}"/*; do
-	sed -i 's|\trm "${WINEPREFIX}/dosdevices/z:"|&\n\twinetricks vd=800x600|' "$file"
-done
+write_launcher 'APP_MAIN' 'APP_EDITOR_MAP' 'APP_EDITOR_CAMPAIGN'
 
 # Build package
 
-write_metadata 'PKG_BIN' 'PKG_MUSIC' 'PKG_DATA'
-build_pkg      'PKG_BIN' 'PKG_MUSIC' 'PKG_DATA'
+write_metadata
+build_pkg
 
 # Clean up
 
