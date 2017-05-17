e59b15b274bab5e0329389d7efe6dac85ea6d6da
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-17 12:53:23 +0200

    2.0 library - New build

--- play.it-2/lib/libplayit2.sh
+++ play.it-2/lib/libplayit2.sh
@@ -33,7 +33,7 @@
 ###
 
 library_version=2.0
-library_revision=20170516.3
+library_revision=20170517.1
 
 # set package distribution-specific architecture
 # USAGE: set_arch
@@ -137,108 +137,27 @@ liberror() {
 	return 1
 }
 
-# check script dependencies
-# USAGE: check_deps
-# NEEDED VARS: ARCHIVE_TYPE, SCRIPT_DEPS, CHECKSUM_METHOD, PACKAGE_TYPE
-# CALLS: check_deps_7z, check_deps_icon, check_deps_failed
-check_deps() {
-	case "$ARCHIVE_TYPE" in
-		('innosetup')
-			SCRIPT_DEPS="$SCRIPT_DEPS innoextract"
-		;;
-		('nixstaller')
-			SCRIPT_DEPS="$SCRIPT_DEPS gzip tar unxz"
-		;;
-		('mojosetup')
-			SCRIPT_DEPS="$SCRIPT_DEPS bsdtar"
-		;;
-		('zip')
-			SCRIPT_DEPS="$SCRIPT_DEPS unzip"
-		;;
-		('rar')
-			SCRIPT_DEPS="$SCRIPT_DEPS unar"
-		;;
-		('tar.gz')
-			SCRIPT_DEPS="$SCRIPT_DEPS gzip tar"
-		;;
-	esac
-	if [ "$CHECKSUM_METHOD" = 'md5sum' ]; then
-		SCRIPT_DEPS="$SCRIPT_DEPS md5sum"
-	fi
-	if [ "$PACKAGE_TYPE" = 'deb' ]; then
-		SCRIPT_DEPS="$SCRIPT_DEPS fakeroot dpkg"
-	fi
-	if [ "${APP_MAIN_ICON##*.}" = 'bmp' ]; then
-		SCRIPT_DEPS="$SCRIPT_DEPS convert"
-	fi
-	if [ "${APP_MAIN_ICON##*.}" = 'ico' ]; then
-		SCRIPT_DEPS="$SCRIPT_DEPS icotool"
-	fi
-	for dep in $SCRIPT_DEPS; do
-		case $dep in
-			('7z')
-				check_deps_7z
-			;;
-			(*)
-				if ! which $dep >/dev/null 2>&1; then
-					check_deps_failed "$dep"
-				fi
-			;;
-		esac
-	done
-}
-
-# check presence of a software to handle .7z archives
-# USAGE: check_deps_7z
-# CALLS: check_deps_failed
-# CALLED BY: check_deps
-check_deps_7z() {
-	if which 7zr >/dev/null 2>&1; then
-		extract_7z() { 7zr x -o"$2" -y "$1"; }
-	elif which 7za >/dev/null 2>&1; then
-		extract_7z() { 7za x -o"$2" -y "$1"; }
-	elif which unar >/dev/null 2>&1; then
-		extract_7z() { unar -output-directory "$2" -force-overwrite -no-directory "$1"; }
-	else
-		check_deps_failed 'p7zip'
-	fi
-}
-
-# display a message if a required dependency is missing
-# USAGE: check_deps_failed $command_name
-# CALLED BY: check_deps, check_deps_7z
-check_deps_failed() {
-	print_error
-	case ${LANG%_*} in
-		('fr')
-			printf '%s est introuvable. Installez-le avant de lancer ce script.\n' "$1"
-		;;
-		('en'|*)
-			printf '%s not found. Install it before running this script.\n' "$1"
-		;;
-	esac
-	return 1
-}
-
 # set archive for data extraction
 # USAGE: set_archive $name $archive[…]
 # NEEDED_VARS: SOURCE_ARCHIVE
-# CALLS: set_archive_print
+# CALLS: check_deps file_checksum set_archive_print
 set_archive() {
 	local name=$1
 	shift 1
+	unset $name
 	for archive in "$@"; do
 		if [ -f "$archive" ]; then
 			export $name="$archive"
 			set_archive_print "$archive"
-			return 0
 		elif [ -f "${SOURCE_ARCHIVE%/*}/$archive" ]; then
 			export $name="${SOURCE_ARCHIVE%/*}/$archive"
 			set_archive_print "${SOURCE_ARCHIVE%/*}/$archive"
-			return 0
 		fi
 	done
-	unset $name
+	if [ -n "$(eval echo \$$name)" ]; then
+		check_deps
+		file_checksum "$(eval echo \$$name)"
+	fi
 }
 
 # set source archive for data extraction
@@ -430,6 +349,89 @@ file_checksum_error() {
 	esac
 	printf "$string1" "$1"
 	printf "$string2"
+}
+
+# check script dependencies
+# USAGE: check_deps
+# NEEDED VARS: ARCHIVE_TYPE, SCRIPT_DEPS, CHECKSUM_METHOD, PACKAGE_TYPE
+# CALLS: check_deps_7z, check_deps_icon, check_deps_failed
+check_deps() {
+	case "$ARCHIVE_TYPE" in
+		('innosetup')
+			SCRIPT_DEPS="$SCRIPT_DEPS innoextract"
+		;;
+		('nixstaller')
+			SCRIPT_DEPS="$SCRIPT_DEPS gzip tar unxz"
+		;;
+		('mojosetup')
+			SCRIPT_DEPS="$SCRIPT_DEPS bsdtar"
+		;;
+		('zip')
+			SCRIPT_DEPS="$SCRIPT_DEPS unzip"
+		;;
+		('rar')
+			SCRIPT_DEPS="$SCRIPT_DEPS unar"
+		;;
+		('tar.gz')
+			SCRIPT_DEPS="$SCRIPT_DEPS gzip tar"
+		;;
+	esac
+	if [ "$CHECKSUM_METHOD" = 'md5sum' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS md5sum"
+	fi
+	if [ "$PACKAGE_TYPE" = 'deb' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS fakeroot dpkg"
+	fi
+	if [ "${APP_MAIN_ICON##*.}" = 'bmp' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS convert"
+	fi
+	if [ "${APP_MAIN_ICON##*.}" = 'ico' ]; then
+		SCRIPT_DEPS="$SCRIPT_DEPS icotool"
+	fi
+	for dep in $SCRIPT_DEPS; do
+		case $dep in
+			('7z')
+				check_deps_7z
+			;;
+			(*)
+				if ! which $dep >/dev/null 2>&1; then
+					check_deps_failed "$dep"
+				fi
+			;;
+		esac
+	done
+}
+
+# check presence of a software to handle .7z archives
+# USAGE: check_deps_7z
+# CALLS: check_deps_failed
+# CALLED BY: check_deps
+check_deps_7z() {
+	if which 7zr >/dev/null 2>&1; then
+		extract_7z() { 7zr x -o"$2" -y "$1"; }
+	elif which 7za >/dev/null 2>&1; then
+		extract_7z() { 7za x -o"$2" -y "$1"; }
+	elif which unar >/dev/null 2>&1; then
+		extract_7z() { unar -output-directory "$2" -force-overwrite -no-directory "$1"; }
+	else
+		check_deps_failed 'p7zip'
+	fi
+}
+
+# display a message if a required dependency is missing
+# USAGE: check_deps_failed $command_name
+# CALLED BY: check_deps, check_deps_7z
+check_deps_failed() {
+	print_error
+	case ${LANG%_*} in
+		('fr')
+			printf '%s est introuvable. Installez-le avant de lancer ce script.\n' "$1"
+		;;
+		('en'|*)
+			printf '%s not found. Install it before running this script.\n' "$1"
+		;;
+	esac
+	return 1
 }
 
 # set working directories
