0f869802b1de65db514cb597a0d892d6b87a7b88
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-18 16:24:36 +0200

    2.0 - Trine - Release version

--- play.it-2/games/play-trine.sh
+++ play.it-2/games/play-trine.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170514.1
+script_version=20170518.1
 
 # Set game-specific variables
 
 GAME_ID='trine'
 GAME_NAME='Trine'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='gog_trine_enchanted_edition_2.0.0.2.sh'
 ARCHIVE_GOG_MD5='0e8d2338b568222b28cf3c31059b4960'
@@ -48,15 +50,19 @@ ARCHIVE_GOG_VERSION='2.12.508-gog2.0.0.2'
 
 ARCHIVE_DOC1_PATH='data/noarch/docs'
 ARCHIVE_DOC1_FILES='./*'
+
 ARCHIVE_DOC2_PATH='data/noarch/game'
 ARCHIVE_DOC2_FILES='./*.txt'
+
 ARCHIVE_GAME_PATH='data/noarch/game'
 ARCHIVE_GAME_FILES='./*'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='bin/trine1_linux_launcher_32bit'
 APP_MAIN_ICON='trine1.png'
-APP_MAIN_ICON_RES='64x64'
+APP_MAIN_ICON_RES='64'
+
+PACKAGES_LIST='PKG_MAIN'
 
 PKG_MAIN_ARCH='32'
 PKG_MAIN_DEPS_DEB='libc6, libstdc++6, libglu1-mesa | libglu1, libgtk2.0-0, libpng12-0, libasound2-plugins, libopenal1, libvorbisfile3'
@@ -80,30 +86,8 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_MAIN'
 extract_data_from "$SOURCE_ARCHIVE"
 
 organize_data 'DOC1' "$PATH_DOC"
@@ -114,12 +98,14 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-write_bin 'APP_MAIN'
-chmod 755 "$PKG_MAIN_PATH/$PATH_GAME/bin/trine1_bin_starter.sh"
-chmod 755 "$PKG_MAIN_PATH/$PATH_GAME/bin/trine1_linux_32bit"
-write_desktop 'APP_MAIN'
+write_launcher 'APP_MAIN'
+chmod 755 "${PKG_MAIN_PATH}${PATH_GAME}/bin/trine1_bin_starter.sh"
+chmod 755 "${PKG_MAIN_PATH}${PATH_GAME}/bin/trine1_linux_32bit"
 
 # Build package
+
+res="$APP_MAIN_ICON_RES"
+PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
 
 cat > "$postinst" << EOF
 mkdir --parents "$PATH_ICON"
@@ -131,8 +117,8 @@ rm "$PATH_ICON/$GAME_ID.png"
 rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON"
 EOF
 
-write_metadata 'PKG_MAIN'
-build_pkg 'PKG_MAIN'
+write_metadata
+build_pkg
 
 # Clean up
 
