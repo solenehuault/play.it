fb1e9f81a659195cfc9b2eccd09ca90e8b125fc6
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-18 14:46:52 +0200

    2.0 - The Jungle Book - Release version

--- play.it-2/games/play-the-jungle-book.sh
+++ play.it-2/games/play-the-jungle-book.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170518.1
 
 # Set game-specific variables
 
 GAME_ID='the-jungle-book'
 GAME_NAME='The Jungle Book'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='gog_disney_s_the_jungle_book_2.0.0.2.sh'
 ARCHIVE_GOG_MD5='bcb57f4ff5cb1662ba3d4a9e34f263ad'
@@ -48,6 +50,7 @@ ARCHIVE_GOG_VERSION='1.0-gog2.0.0.2'
 
 ARCHIVE_DOC_PATH='data/noarch/docs'
 ARCHIVE_DOC_FILES='./*.txt ./*.pdf'
+
 ARCHIVE_GAME_PATH='data/noarch/data'
 ARCHIVE_GAME_FILES='./*'
 
@@ -56,8 +59,11 @@ CONFIG_FILES='./*.cfg'
 APP_MAIN_TYPE='dosbox'
 APP_MAIN_EXE='jungle.exe'
 APP_MAIN_ICON='data/noarch/support/icon.png'
-APP_MAIN_ICON_RES='256x256'
+APP_MAIN_ICON_RES='256'
 
+PACKAGES_LIST='PKG_MAIN'
+
+PKG_MAIN_ARCH='32'
 PKG_MAIN_DEPS_DEB='dosbox'
 PKG_MAIN_DEPS_ARCH='dosbox'
 
@@ -79,49 +85,28 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_MAIN'
 extract_data_from "$SOURCE_ARCHIVE"
 
 organize_data 'DOC'  "$PATH_DOC"
 organize_data 'GAME' "$PATH_GAME"
 
-mkdir --parents "$PKG_MAIN_PATH/$PATH_ICON"
-mv "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON" "$PKG_MAIN_PATH/$PATH_ICON/$GAME_ID.png"
+res="$APP_MAIN_ICON_RES"
+PATH_ICON="$PATH_ICON_BASE/${res}x${res}/apps"
+mkdir --parents "${PKG_MAIN_PATH}${PATH_ICON}"
+mv "$PLAYIT_WORKDIR/gamedata/$APP_MAIN_ICON" "${PKG_MAIN_PATH}${PATH_ICON}/$GAME_ID.png"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-write_bin 'APP_MAIN'
-write_desktop 'APP_MAIN'
+write_launcher 'APP_MAIN'
 
 # Build package
 
-write_metadata 'PKG_MAIN'
-build_pkg 'PKG_MAIN'
+write_metadata
+build_pkg
 
 # Clean up
 
