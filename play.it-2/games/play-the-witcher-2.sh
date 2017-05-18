553268dac434ac8f7c5e61831d79c3e639e71136
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-18 16:09:18 +0200

    2.0 - The Witcher 2 - Release version

--- play.it-2/games/play-the-witcher-2.sh
+++ play.it-2/games/play-the-witcher-2.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170518.1
 
 # Set game-specific variables
 
 GAME_ID='the-witcher-2'
 GAME_NAME='The Witcher 2: Assassins Of Kings'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='gog_the_witcher_2_assassins_of_kings_enhanced_edition_2.2.0.8.sh'
 ARCHIVE_GOG_MD5='3fff5123677a7be2023ecdb6af3b82b6'
@@ -49,29 +51,36 @@ ARCHIVE_GOG_TYPE='mojosetup_unzip'
 
 ARCHIVE_DOC1_PATH='data/noarch/docs'
 ARCHIVE_DOC1_FILES='./*'
+
 ARCHIVE_DOC2_PATH='data/noarch/game'
 ARCHIVE_DOC2_FILES='./*.rtf ./*.txt'
+
 ARCHIVE_GAME_MAIN_PATH='data/noarch/game'
 ARCHIVE_GAME_MAIN_FILES='./*'
+
 ARCHIVE_GAME_PACK1_PATH='data/noarch/game'
 ARCHIVE_GAME_PACK1_FILES='./CookedPC/pack0.dzip.split00'
+
 ARCHIVE_GAME_PACK2_PATH='data/noarch/game'
 ARCHIVE_GAME_PACK2_FILES='./CookedPC/pack0.dzip.split01 ./CookedPC/pack0.dzip.split02'
+
 ARCHIVE_GAME_MOVIES_PATH='data/noarch/game'
 ARCHIVE_GAME_MOVIES_FILES='./CookedPC/movies'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE='./witcher2'
 APP_MAIN_ICON='linux/icons/witcher2-icon.png'
-APP_MAIN_ICON_RES='256x256'
+APP_MAIN_ICON_RES='256'
 
 APP_CONFIG_ID="${GAME_ID}_config"
 APP_CONFIG_TYPE='native'
 APP_CONFIG_EXE='./configurator'
 APP_CONFIG_NAME="$GAME_NAME - configuration"
 APP_CONFIG_ICON='linux/icons/witcher2-configurator.png'
-APP_CONFIG_ICON_RES='256x256'
+APP_CONFIG_ICON_RES='256'
 APP_CONFIG_CAT='Settings'
+
+PACKAGES_LIST='PKG_PACK1 PKG_PACK2 PKG_MOVIES PKG_MAIN'
 
 PKG_PACK1_ID="${GAME_ID}-pack1"
 PKG_PACK1_DESCRIPTION='pack0, part 1'
@@ -104,30 +113,8 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-PATH_ICON="$PATH_ICON_BASE/$APP_MAIN_ICON_RES/apps"
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_MAIN' 'PKG_PACK1' 'PKG_PACK2' 'PKG_MOVIES'
 extract_data_from "$SOURCE_ARCHIVE"
 
 PKG='PKG_PACK1'
@@ -140,40 +127,46 @@ PKG='PKG_MOVIES'
 organize_data 'GAME_MOVIES' "$PATH_GAME"
 
 PKG='PKG_MAIN'
-organize_data 'DOC1' "$PATH_DOC"
-organize_data 'DOC2' "$PATH_DOC"
+organize_data 'DOC1'      "$PATH_DOC"
+organize_data 'DOC2'      "$PATH_DOC"
 organize_data 'GAME_MAIN' "$PATH_GAME"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-write_bin 'APP_MAIN' 'APP_CONFIG'
-write_desktop 'APP_MAIN' 'APP_CONFIG'
+PKG='PKG_MAIN'
+write_launcher
 
 # Build package
 
+res_main="$APP_MAIN_ICON_RES"
+res_config="$APP_CONFIG_ICON_RES"
+PATH_ICON_MAIN="$PATH_ICON_BASE/${res_main}x${res_main}/apps"
+PATH_ICON_CONFIG="$PATH_ICON_BASE/${res_config}x${res_config}/apps"
+
 cat > "$postinst" << EOF
-mkdir --parents "$PATH_ICON"
-ln --symbolic "$PATH_GAME/$APP_MAIN_ICON" "$PATH_ICON/$GAME_ID.png"
-ln --symbolic "$PATH_GAME/$APP_CONFIG_ICON" "$PATH_ICON/$APP_CONFIG_ID.png"
+mkdir --parents "$PATH_ICON_MAIN"
+mkdir --parents "$PATH_ICON_CONFIG"
+ln --symbolic "$PATH_GAME/$APP_MAIN_ICON" "$PATH_ICON_MAIN/$GAME_ID.png"
+ln --symbolic "$PATH_GAME/$APP_CONFIG_ICON" "$PATH_ICON_CONFIG/$APP_CONFIG_ID.png"
 printf 'Building pack0.dzip, this might take a while…\n'
 cat "$PATH_GAME/CookedPC/pack0.dzip.split"* > "$PATH_GAME/CookedPC/pack0.dzip"
 rm "$PATH_GAME/CookedPC/pack0.dzip.split"*
 EOF
 
 cat > "$prerm" << EOF
-rm "$PATH_ICON/$GAME_ID.png"
-rm "$PATH_ICON/$APP_CONFIG_ID.png"
-rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON"
+rm "$PATH_ICON_MAIN/$GAME_ID.png"
+rm "$PATH_ICON_CONFIG/$APP_CONFIG_ID.png"
+rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON_MAIN"
+rmdir --parents --ignore-fail-on-non-empty "$PATH_ICON_CONFIG"
 rm "$PATH_GAME/CookedPC/pack0.dzip"
 EOF
 
 write_metadata 'PKG_MAIN'
 rm "$postinst" "$prerm"
 write_metadata 'PKG_PACK1' 'PKG_PACK2' 'PKG_MOVIES'
-
-build_pkg 'PKG_MAIN' 'PKG_PACK1' 'PKG_PACK2' 'PKG_MOVIES'
+build_pkg
 
 # Clean up
 
