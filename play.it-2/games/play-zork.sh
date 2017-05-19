5f323afe8aca6d89c9284fb2a05a9bc9ad436a25
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-19 10:05:28 +0200

    2.0 - Zork - Release version

--- play.it-2/games/play-zork.sh
+++ play.it-2/games/play-zork.sh
@@ -34,12 +34,14 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170405.1
+script_version=20170519.1
 
 # Set game-specific variables
 
 GAME_ID='zork'
 GAME_NAME='Zork I: The Great Underground Empire'
+
+ARCHIVES_LIST='ARCHIVE_GOG'
 
 ARCHIVE_GOG='setup_zork_2.1.0.17.exe'
 ARCHIVE_GOG_MD5='6bd42788d696f154ce363830909259d3'
@@ -48,8 +50,10 @@ ARCHIVE_GOG_VERSION='1.0-gog2.1.0.17'
 
 ARCHIVE_DOC1_PATH='app'
 ARCHIVE_DOC1_FILES='./*.htm ./*.pdf ./*.txt'
+
 ARCHIVE_DOC2_PATH='tmp'
 ARCHIVE_DOC2_FILES='./gog_eula.txt ./eula.txt'
+
 ARCHIVE_GAME_PATH='app'
 ARCHIVE_GAME_FILES='./goggame-1207661493.ico ./*.com ./*.inf ./data ./save'
 
@@ -58,8 +62,11 @@ DATA_DIRS='./save'
 APP_MAIN_TYPE='dosbox'
 APP_MAIN_EXE='_zork1.com'
 APP_MAIN_ICON='./goggame-1207661493.ico'
-APP_MAIN_ICON_RES='16x16 32x32 48x48 256x256'
+APP_MAIN_ICON_RES='16 32 48 256'
 
+PACKAGES_LIST='PKG_MAIN'
+
+PKG_MAIN_ARCH='32'
 PKG_MAIN_DEPS_DEB='dosbox'
 PKG_MAIN_DEPS_ARCH='dosbox'
 
@@ -81,29 +88,8 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_MAIN'
 extract_data_from "$SOURCE_ARCHIVE"
 
 organize_data 'DOC1' "$PATH_DOC"
@@ -116,13 +102,12 @@ rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-write_bin 'APP_MAIN'
-write_desktop 'APP_MAIN'
+write_launcher 'APP_MAIN'
 
 # Build package
 
-write_metadata 'PKG_MAIN'
-build_pkg 'PKG_MAIN'
+write_metadata
+build_pkg
 
 # Clean up
 
