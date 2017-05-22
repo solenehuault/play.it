8ffd561831af956b7d0658cb3cb09c0952c31d9c
vv221 <vv221@dotslashplay.it>
2017-05-22 22:40:14 +0200

    2.0 - print_instructions - Takes 'PKG' variable names as argument instead of $PKG_PATH values + Can be called without argument (defaults to $PACKAGES_LIST)

--- play.it-2/src/installation-instructions.sh
+++ play.it-2/src/installation-instructions.sh
@@ -1,33 +1,39 @@
 # print installation instructions
 # USAGE: print_instructions $pkg[…]
-# NEEDED VARS: PKG
+# NEEDED VARS: (GAME_NAME) (PACKAGE_TYPE) (PACKAGES_LIST)
 print_instructions() {
-	case ${LANG%_*} in
+	[ $GAME_NAME ] || return 1
+	if [ $# = 0 ]; then
+		print_instructions $PACKAGES_LIST
+		return 0
+	fi
+	case "${LANG%_*}" in
 		('fr')
-			printf '\nInstallez %s en lançant la série de commandes suivantes en root :\n' "$GAME_NAME"
+			string='\nInstallez %s en lançant la série de commandes suivantes en root :\n'
 		;;
 		('en'|*)
-			printf '\nInstall %s by running the following commands as root:\n' "$GAME_NAME"
+			string='\nInstall %s by running the following commands as root:\n'
 		;;
 	esac
+	printf "$string" "$GAME_NAME"
 	case $PACKAGE_TYPE in
 		('arch')
 			printf 'pacman -U'
 			for pkg in $@; do
-				printf ' %s' "$pkg"
+				printf ' %s' "$(eval echo ${pkg}_PATH)"
 			done
 			printf '\n'
 		;;
 		('deb')
 			printf 'dpkg -i'
 			for pkg in $@; do
-				printf ' %s' "$pkg"
+				printf ' %s' "$(eval echo ${pkg}_PATH)"
 			done
 			printf '\n'
 			printf 'apt-get install -f\n'
 		;;
 		(*)
-			liberror 'PACKAGE_TYPE' 'build_pkg'
+			liberror 'PACKAGE_TYPE' 'print_instructions'
 		;;
 	esac
 	printf '\n'
