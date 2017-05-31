55b612425758b161e345b9c5441f816a05ee4fd1
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-05-31 11:40:07 +0200

    2.0 library - Small tweaks in icons-related functions

--- play.it-2/src/icons.sh
+++ play.it-2/src/icons.sh
@@ -1,12 +1,12 @@
 # extract .png or .ico files from given file
 # USAGE: extract_icon_from $file[…]
-# NEEDED VARS: $PLAYIT_WORKDIR
+# NEEDED VARS: PLAYIT_WORKDIR
 # CALLS: liberror
 extract_icon_from() {
 	for file in "$@"; do
 		local destination="$PLAYIT_WORKDIR/icons"
 		mkdir --parents "$destination"
-		case ${file##*.} in
+		case "${file##*.}" in
 			('exe')
 				if [ "$WRESTOOL_NAME" ]; then
 					WRESTOOL_OPTIONS="--name=$WRESTOOL_NAME"
@@ -21,91 +21,66 @@ extract_icon_from() {
 				convert "$file" "$destination/${filename%.bmp}.png"
 			;;
 			(*)
-				liberror 'file_ext' 'extract_icon_from'
+				liberror 'file extension' 'extract_icon_from'
 			;;
 		esac
 	done
 }
 
-# create icons tree
-# USAGE: sort_icons $app
-# NEEDED VARS: $app_ID, $app_ICON_RES, PKG, $PKG_PATH, PACKAGE_TYPE
-# CALLS: sort_icons_arch, sort_icons_deb, sort_icons_tar
+# create icons layout
+# USAGE: sort_icons $app[…]
+# NEEDED VARS: APP_ICON_RES (APP_ID) GAME_ID PKG PKG_PATH
 sort_icons() {
 for app in $@; do
 	testvar "$app" 'APP' || liberror 'app' 'sort_icons'
-	local app_id="$(eval echo \$${app}_ID)"
-	if [ -z "$app_id" ]; then
+
+	local app_id
+	if [ -n "$(eval echo \$${app}_ID)" ]; then
+		app_id="$(eval echo \$${app}_ID)"
+	else
 		app_id="$GAME_ID"
 	fi
+
 	local icon_res="$(eval echo \$${app}_ICON_RES)"
 	local pkg_path="$(eval echo \$${PKG}_PATH)"
-	case $PACKAGE_TYPE in
-		('arch')
-			sort_icons_arch
-		;;
-		('deb')
-			sort_icons_deb
-		;;
-		(*)
-			liberror 'PACKAGE_TYPE' 'sort_icons'
-		;;
-	esac
+	for res in $icon_res; do
+		path_icon="$PATH_ICON_BASE/${res}x${res}/apps"
+		mkdir --parents "${pkg_path}${path_icon}"
+		for file in "$PLAYIT_WORKDIR"/icons/*${res}x${res}x*.png; do
+			mv "$file" "${pkg_path}${path_icon}/${app_id}.png"
+		done
+	done
 done
-}
-
-# create icons tree for .pkg.tar.xz package
-# USAGE: sort_icons_arch
-# NEEDED VARS: PATH_ICON_BASE, PLAYIT_WORKDIR
-# CALLED BY: sort_icons
-sort_icons_arch() {
-	for res in $icon_res; do
-		path_icon="${PATH_ICON_BASE}/${res}x${res}/apps"
-		mkdir -p "${pkg_path}${path_icon}"
-		for file in "${PLAYIT_WORKDIR}"/icons/*${res}x${res}x*.png; do
-			mv "${file}" "${pkg_path}${path_icon}/${app_id}.png"
-		done
-	done
-}
-
-# create icons tree for .deb package
-# USAGE: sort_icons_deb
-# NEEDED VARS: PATH_ICON_BASE, PLAYIT_WORKDIR
-# CALLED BY: sort_icons
-sort_icons_deb() {
-	for res in $icon_res; do
-		path_icon="${PATH_ICON_BASE}/${res}x${res}/apps"
-		mkdir -p "${pkg_path}${path_icon}"
-		for file in "${PLAYIT_WORKDIR}"/icons/*${res}x${res}x*.png; do
-			mv "${file}" "${pkg_path}${path_icon}/${app_id}.png"
-		done
-	done
 }
 
 # extract and sort icons from given .ico or .exe file
 # USAGE: extract_and_sort_icons_from $app[…]
-# NEEDED VARS: $NO_ICON $PLAYIT_WORKDIR $APP_ID $APP_ICON $APP_ICON_RES $PKG
-# 	$PKG_PATH $PACKAGE_TYPE $PATH_GAME
-# CALLS: liberror extract_icon_from sort_icons
+# NEEDED VARS: APP_ICON APP_ICON_RES (APP_ID) GAME_ID PKG PKG_PATH PLAYIT_WORKDIR
+# CALLS: extract_icon_from liberror sort_icons
 extract_and_sort_icons_from() {
 	local app_icon
 	local pkg_path="$(eval echo \$${PKG}_PATH)"
 	for app in $@; do
 		testvar "$app" 'APP' || liberror 'app' 'sort_icons'
+
 		if [ "$ARCHIVE" ] && [ -n "$(eval echo \$${app}_ICON_${ARCHIVE#ARCHIVE_})" ]; then
 			app_icon="$(eval echo \$${app}_ICON_${ARCHIVE#ARCHIVE_})"
 			export ${app}_ICON="$app_icon"
 		else
 			app_icon="$(eval echo \$${app}_ICON)"
 		fi
+
 		if [ ! "$WRESTOOL_NAME" ] && [ -n "$(eval echo \$${app}_ICON_ID)" ]; then
 			WRESTOOL_NAME="$(eval echo \$${app}_ICON_ID)"
 		fi
+
 		extract_icon_from "${pkg_path}${PATH_GAME}/$app_icon"
 		unset WRESTOOL_NAME
+
 		if [ "${app_icon##*.}" = 'exe' ]; then
 			extract_icon_from "$PLAYIT_WORKDIR/icons"/*.ico
 		fi
+
 		sort_icons "$app"
 		rm --recursive "$PLAYIT_WORKDIR/icons"
 	done
