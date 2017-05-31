cb41c3ae985a67c7ad20a9bbff17f8ff6246a6ce
vv221 <vv221@dotslashplay.it>
2017-05-31 22:40:25 +0200

    2.0 - Memoria - Release version

--- play.it-2/games/play-memoria.sh
+++ play.it-2/games/play-memoria.sh
@@ -34,31 +34,55 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20161226.2
+script_version=20170531.1
 
 # Set game-specific variables
 
 GAME_ID='memoria'
 GAME_NAME='Memoria'
 
+ARCHIVES_LIST='ARCHIVE_GOG'
+
 ARCHIVE_GOG='setup_memoria_2.0.0.3.exe'
 ARCHIVE_GOG_MD5='847c7b5e27a287d6e0e17e63bfb14fff'
-ARCHIVE_GOG_UNCOMPRESSED_SIZE='9100000'
+ARCHIVE_GOG_SIZE='9100000'
 ARCHIVE_GOG_VERSION='1.36.0053-gog2.0.0.3'
+ARCHIVE_GOG_PART1='setup_memoria_2.0.0.3-1.bin'
+ARCHIVE_GOG_PART1_MD5='e656464607e4d8599d599ed5b6b29fca'
+ARCHIVE_GOG_PART1_TYPE='innosetup'
+ARCHIVE_GOG_PART2='setup_memoria_2.0.0.3-2.bin'
+ARCHIVE_GOG_PART2_MD5='593d57e8022c65660394c5bc5a333fe8'
+ARCHIVE_GOG_PART2_TYPE='innosetup'
+ARCHIVE_GOG_PART3='setup_memoria_2.0.0.3-3.bin'
+ARCHIVE_GOG_PART3_MD5='0f8ef0abab77f3885aa4f8f9e58611eb'
+ARCHIVE_GOG_PART3_TYPE='innosetup'
+ARCHIVE_GOG_PART4='setup_memoria_2.0.0.3-4.bin'
+ARCHIVE_GOG_PART4_MD5='0935149a66284bdc13659beafed2575f'
+ARCHIVE_GOG_PART4_TYPE='innosetup'
+ARCHIVE_GOG_PART5='setup_memoria_2.0.0.3-5.bin'
+ARCHIVE_GOG_PART5_MD5='5b85fb7fcb51599ee89b5d7371b87ee2'
+ARCHIVE_GOG_PART5_TYPE='innosetup'
+ARCHIVE_GOG_PART6='setup_memoria_2.0.0.3-6.bin'
+ARCHIVE_GOG_PART6_MD5='c8712354bbd093b706f551e75b549061'
+ARCHIVE_GOG_PART6_TYPE='innosetup'
 
 ARCHIVE_DOC1_PATH='app/documents/licenses'
 ARCHIVE_DOC1_FILES='./*.txt'
+
 ARCHIVE_DOC2_PATH='tmp'
 ARCHIVE_DOC2_FILES='./*eula.txt'
+
 ARCHIVE_GAME_PATH='app'
 ARCHIVE_GAME_FILES='./avcodec-54.dll ./avformat-54.dll ./avutil-52.dll ./banner.jpg ./characters ./config.ini ./data.vis ./documents ./folder.jpg ./gfw_high.ico ./goggame.dll ./languages.xml ./libsndfile-1.dll ./lua ./memoria.exe ./openal32.dll ./scenes ./sdl2.dll ./swresample-0.dll ./swscale-2.dll ./videos ./visionaireconfigurationtool.exe ./zlib1.dll'
 
 APP_MAIN_TYPE='wine'
 APP_MAIN_EXE='memoria.exe'
 APP_MAIN_ICON='gfw_high.ico'
-APP_MAIN_ICON_RES='16x16 32x32 48x48 256x256'
+APP_MAIN_ICON_RES='16 32 48 256'
 
-PKG_MAIN_ARCH='32on64'
+PACKAGES_LIST='PKG_MAIN'
+
+PKG_MAIN_ARCH='32'
 PKG_MAIN_DEPS_DEB='wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386'
 PKG_MAIN_DEPS_ARCH='wine'
 
@@ -80,50 +104,43 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
+# Check that all parts of the installer are present
 
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_GOG'
-check_deps
-set_common_paths
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_GOG'
-check_deps
+set_archive 'ARCHIVE_PART1' 'ARCHIVE_GOG_PART1'
+[ "$ARCHIVE_PART1" ] || set_archive_error_not_found 'ARCHIVE_GOG_PART1'
+set_archive 'ARCHIVE_PART2' 'ARCHIVE_GOG_PART2'
+[ "$ARCHIVE_PART2" ] || set_archive_error_not_found 'ARCHIVE_GOG_PART2'
+set_archive 'ARCHIVE_PART3' 'ARCHIVE_GOG_PART3'
+[ "$ARCHIVE_PART3" ] || set_archive_error_not_found 'ARCHIVE_GOG_PART3'
+set_archive 'ARCHIVE_PART4' 'ARCHIVE_GOG_PART4'
+[ "$ARCHIVE_PART4" ] || set_archive_error_not_found 'ARCHIVE_GOG_PART4'
+set_archive 'ARCHIVE_PART5' 'ARCHIVE_GOG_PART5'
+[ "$ARCHIVE_PART5" ] || set_archive_error_not_found 'ARCHIVE_GOG_PART5'
+set_archive 'ARCHIVE_PART6' 'ARCHIVE_GOG_PART6'
+[ "$ARCHIVE_PART6" ] || set_archive_error_not_found 'ARCHIVE_GOG_PART6'
+ARCHIVE='ARCHIVE_GOG'
 
 # Extract game data
 
-set_workdir 'PKG_MAIN'
 extract_data_from "$SOURCE_ARCHIVE"
-organize_data
 
-if [ "$NO_ICON" = '0' ]; then
-	extract_icon_from "${PKG_MAIN_PATH}${PATH_GAME}/$APP_MAIN_ICON"
-	sort_icons 'APP_MAIN'
-	rm --recursive "$PLAYIT_WORKDIR/icons"
-fi
+organize_data 'DOC1' "$PATH_DOC"
+organize_data 'DOC2' "$PATH_DOC"
+organize_data 'GAME' "$PATH_GAME"
+
+extract_and_sort_icons_from 'APP_MAIN'
+rm "${PKG_MAIN_PATH}${PATH_GAME}/$APP_MAIN_ICON"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-write_bin 'APP_MAIN'
-write_desktop 'APP_MAIN'
+write_launcher 'APP_MAIN'
 
 # Build package
 
-write_metadata 'PKG_MAIN'
-
-build_pkg 'PKG_MAIN'
+write_metadata
+build_pkg
 
 # Clean up
 
@@ -131,6 +148,6 @@ rm --recursive "$PLAYIT_WORKDIR"
 
 # Print instructions
 
-print_instructions "$PKG_MAIN_PKG"
+print_instructions
 
 exit 0
