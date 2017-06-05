29442bcc56b655f0e0fde544eea362a55eee6f69
Mopi <mopi@dotslashplay.it>
2017-06-05 11:17:24 +0200

    2.0 - Greedcorp - Add missing dependencie

--- play.it-2/games/play-greedcorp.sh
+++ play.it-2/games/play-greedcorp.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170523.1
+script_version=20170605.1
 
 # Set game-specific variables
 
@@ -72,12 +72,12 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN32_ARCH='32'
-PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libgl1-mesa-glx | libgl1"
-PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-libgl"
+PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libgl1-mesa-glx | libgl1, libglu1-mesa | libglu1"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-libgl lib32-glu "
 
 PKG_BIN64_ARCH='64'
 PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
-PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID libgl"
+PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID libgl glu"
 
 # Load common functions
 
