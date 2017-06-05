ceff7e69bd351488180f9060f46b7aa9d674ff05
Mopi <mopi@dotslashplay.it>
2017-06-05 17:59:17 +0200

    2.0 - Infinum Strike - Add missing dependencie

--- play.it-2/games/play-infinium-strike.sh
+++ play.it-2/games/play-infinium-strike.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170523.1
+script_version=20170605.1
 
 # Set game-specific variables
 
@@ -75,8 +75,8 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, libgl1-mesa-glx | libgl1, pulseaudio:amd64 | pulseaudio"
-PKG_BIN_DEPS_ARCH="$PKG_DATA_ID lib32-libgl pulseaudio"
+PKG_BIN_DEPS_DEB="$PKG_DATA_ID, libgl1-mesa-glx | libgl1, pulseaudio:amd64 | pulseaudio, libxcursor1, libxrandr2"
+PKG_BIN_DEPS_ARCH="$PKG_DATA_ID lib32-libgl pulseaudio lib32-libxcursor lib32-libxrandr"
 
 # Load common functions
 
