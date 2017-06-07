1378d5144a935e0f1ff4d6dac02a79ae0fc0d363
vv221 <vv221@dotslashplay.it>
2017-06-07 14:35:13 +0200

    2.0 - Jazzpunk - Release version

--- play.it-2/games/play-jazzpunk.sh
+++ play.it-2/games/play-jazzpunk.sh
@@ -34,43 +34,45 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170329.1
+script_version=20170607.1
 
 # Set game-specific variables
 
 GAME_ID='jazzpunk'
 GAME_NAME='Jazzpunk'
 
+ARCHIVES_LIST='ARCHIVE_HUMBLE'
+
 ARCHIVE_HUMBLE='Jazzpunk-July6-2014-Linux.zip'
 ARCHIVE_HUMBLE_MD5='50ad5722cafe16dc384e83a4a4e19480'
-ARCHIVE_HUMBLE_UNCOMPRESSED_SIZE='1600000'
+ARCHIVE_HUMBLE_SIZE='1600000'
 ARCHIVE_HUMBLE_VERSION='1.0-humble'
 
-ARCHIVE_GAME_32_PATH='./'
-ARCHIVE_GAME_32_FILES='./*.x86 ./*_Data/*/x86'
-ARCHIVE_GAME_64_PATH='./'
-ARCHIVE_GAME_64_FILES='./*.x86_64 ./*_Data/*/x86_64'
-ARCHIVE_GAME_MAIN_PATH='./'
-ARCHIVE_GAME_MAIN_FILES='./*_Data'
+ARCHIVE_GAME_BIN32_PATH='./'
+ARCHIVE_GAME_BIN32_FILES='./*.x86 ./*_Data/*/x86'
+
+ARCHIVE_GAME_BIN64_PATH='./'
+ARCHIVE_GAME_BIN64_FILES='./*.x86_64 ./*_Data/*/x86_64'
+
+ARCHIVE_GAME_DATA_PATH='./'
+ARCHIVE_GAME_DATA_FILES='./*_Data'
 
 APP_MAIN_TYPE='native'
 APP_MAIN_EXE_32='./Jazzpunk.x86'
 APP_MAIN_EXE_64='./Jazzpunk.x86_64'
 
-PKG_MAIN_ID="${GAME_ID}-common"
-PKG_MAIN_ARCH_DEB='all'
-PKG_MAIN_ARCH_ARCH='any'
-PKG_MAIN_DESCRIPTION='arch-independant data'
+PACKAGES_LIST='PKG_DATA PKG_BIN32 PKG_BIN64'
 
-PKG_32_ARCH='32'
-PKG_32_CONFLICTS_DEB="$GAME_ID"
-PKG_32_DEPS_DEB="$PKG_MAIN_ID, libc6, libstdc++6, libglu1-mesa | libglu1"
-PKG_32_DEPS_ARCH="$PKG_MAIN_ID lib32-glu"
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='arch-independant data'
 
-PKG_64_ARCH='64'
-PKG_64_CONFLICTS_DEB="$GAME_ID"
-PKG_64_DEPS_DEB="$PKG_32_DEPS_DEB"
-PKG_64_DEPS_ARCH="$PKG_MAIN_ID glu"
+PKG_BIN32_ARCH='32'
+PKG_BIN32_DEPS_DEB="$PKG_DATA_ID, libc6, libstdc++6, libglu1-mesa | libglu1"
+PKG_BIN32_DEPS_ARCH="$PKG_DATA_ID lib32-glu"
+
+PKG_BIN64_ARCH='64'
+PKG_BIN64_DEPS_DEB="$PKG_BIN32_DEPS_DEB"
+PKG_BIN64_DEPS_ARCH="$PKG_DATA_ID glu"
 
 # Load common functions
 
@@ -90,58 +92,32 @@ if [ -z "$PLAYIT_LIB2" ]; then
 fi
 . "$PLAYIT_LIB2"
 
-if [ ${library_version%.*} -ne ${target_version%.*} ] || [ ${library_version#*.} -lt ${target_version#*.} ]; then
-	printf '\n\033[1;31mError:\033[0m\n'
-	printf 'wrong version of libplayit2.sh\n'
-	printf 'target version is: %s\n' "$target_version"
-	return 1
-fi
-
-# Set extra variables
-
-set_common_defaults
-fetch_args "$@"
-
-# Set source archive
-
-set_source_archive 'ARCHIVE_HUMBLE'
-check_deps
-set_common_paths
-file_checksum "$SOURCE_ARCHIVE" 'ARCHIVE_HUMBLE'
-check_deps
-
 # Extract game data
 
-set_workdir 'PKG_MAIN' 'PKG_32' 'PKG_64'
 extract_data_from "$SOURCE_ARCHIVE"
 
-PKG='PKG_32'
-organize_data_generic 'GAME_32' "$PATH_GAME"
-PKG='PKG_64'
-organize_data_generic 'GAME_64' "$PATH_GAME"
-PKG='PKG_MAIN'
-organize_data_generic 'GAME_MAIN' "$PATH_GAME"
-organize_data_generic 'DOC' "$PATH_DOC"
+PKG='PKG_BIN32'
+organize_data 'GAME_BIN32' "$PATH_GAME"
+
+PKG='PKG_BIN64'
+organize_data 'GAME_BIN64' "$PATH_GAME"
+
+PKG='PKG_DATA'
+organize_data 'DOC'       "$PATH_DOC"
+organize_data 'GAME_DATA' "$PATH_GAME"
 
 rm --recursive "$PLAYIT_WORKDIR/gamedata"
 
 # Write launchers
 
-PKG='PKG_32'
-APP_MAIN_EXE="$APP_MAIN_EXE_32"
-write_bin 'APP_MAIN'
-write_desktop 'APP_MAIN'
-
-PKG='PKG_64'
-APP_MAIN_EXE="$APP_MAIN_EXE_64"
-write_bin 'APP_MAIN'
-write_desktop 'APP_MAIN'
+for PKG in 'PKG_BIN32' 'PKG_BIN64'; do
+	write_launcher 'APP_MAIN'
+done
 
 # Build package
 
-write_metadata 'PKG_MAIN'
-write_metadata 'PKG_32' 'PKG_64'
-build_pkg 'PKG_MAIN' 'PKG_32' 'PKG_64'
+write_metadata
+build_pkg
 
 # Clean up
 
@@ -149,9 +125,10 @@ rm --recursive "${PLAYIT_WORKDIR}"
 
 # Print instructions
 
-printf '\n32-bit:'
-print_instructions "$PKG_MAIN_PKG" "$PKG_32_PKG"
-printf '\n64-bit:'
-print_instructions "$PKG_MAIN_PKG" "$PKG_64_PKG"
+printf '\n'
+printf '32-bit:'
+print_instructions "$PKG_DATA_PKG" "$PKG_BIN32_PKG"
+printf '64-bit:'
+print_instructions "$PKG_DATA_PKG" "$PKG_BIN64_PKG"
 
 exit 0
