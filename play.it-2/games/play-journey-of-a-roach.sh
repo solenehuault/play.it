3e0534b6c98c9991bafd14726a740d513afb482b
Mopi <mopi@dotslashplay.it>
2017-06-13 17:39:41 +0200

    2.0 - Journey of a Roach - New Game

---
+++ play.it-2/games/play-journey-of-a-roach.sh
@@ -0,0 +1,132 @@
+#!/bin/sh -e
+set -o errexit
+
+###
+# Copyright (c) 2015-2017, Antoine Le Gonidec
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# This software is provided by the copyright holders and contributors "as is"
+# and any express or implied warranties, including, but not limited to, the
+# implied warranties of merchantability and fitness for a particular purpose
+# are disclaimed. In no event shall the copyright holder or contributors be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of the
+# possibility of such damage.
+###
+
+###
+# Journey of a Roach
+# build native Linux packages from the original installers
+# send your bug reports to vv221@dotslashplay.it
+###
+
+script_version=20170613.1
+
+# Set game-specific variables
+
+GAME_ID='journey-of-a-roach'
+GAME_NAME='Journey of a Roach'
+
+ARCHIVES_LIST='ARCHIVE_HUMBLE'
+
+ARCHIVE_HUMBLE='JoaR_1.3_PC_Full_Multi_Daedalic_ESD.exe'
+ARCHIVE_HUMBLE_MD5='e349a84f7b8ac095b06edea521cade8f'
+ARCHIVE_HUMBLE_VERSION='1.3-humble160915'
+ARCHIVE_HUMBLE_SIZE='1600000'
+ARCHIVE_HUMBLE_TYPE='innosetup'
+
+ARCHIVE_DOC_PATH='app/documents'
+ARCHIVE_DOC_FILES='./*'
+
+ARCHIVE_GAME_BIN_PATH='app'
+ARCHIVE_GAME_BIN_FILES='./joar.exe ./launcher.exe'
+
+ARCHIVE_GAME_DATA_PATH='app'
+ARCHIVE_GAME_DATA_FILES='./joar_data'
+
+APP_MAIN_TYPE='wine'
+APP_MAIN_EXE='./joar.exe'
+APP_MAIN_ICON='./launcher.exe'
+APP_MAIN_ICON_RES='16 32 48 128'
+
+PACKAGES_LIST='PKG_DATA PKG_BIN'
+
+PKG_DATA_ID="${GAME_ID}-data"
+PKG_DATA_DESCRIPTION='data'
+
+PKG_BIN_ARCH='32'
+PKG_BIN_DEPS_DEB="$PKG_DATA_ID, wine:amd64 | wine, wine32 | wine-bin | wine1.6-i386 | wine1.4-i386 | wine-staging-i386"
+PKG_BIN_DEPS_ARCH="$PKG_DATA_ID wine"
+
+# Load common functions
+
+target_version='2.0'
+
+if [ -z "$PLAYIT_LIB2" ]; then
+	[ -n "$XDG_DATA_HOME" ] || XDG_DATA_HOME="$HOME/.local/share"
+	if [ -e "$XDG_DATA_HOME/play.it/libplayit2.sh" ]; then
+		PLAYIT_LIB2="$XDG_DATA_HOME/play.it/libplayit2.sh"
+	elif [ -e './libplayit2.sh' ]; then
+		PLAYIT_LIB2='./libplayit2.sh'
+	else
+		printf '\n\033[1;31mError:\033[0m\n'
+		printf 'libplayit2.sh not found.\n'
+		return 1
+	fi
+fi
+. "$PLAYIT_LIB2"
+
+# Extract game data
+
+extract_data_from "$SOURCE_ARCHIVE"
+
+PKG='PKG_BIN'
+organize_data 'GAME_BIN' "$PATH_GAME"
+
+PKG='PKG_DATA'
+organize_data 'DOC'      "$PATH_DOC"
+organize_data 'GAME_DATA' "$PATH_GAME"
+
+PKG='PKG_BIN'
+extract_and_sort_icons_from 'APP_MAIN'
+(
+        cd "$PKG_BIN_PATH"
+        cp --link --parents --recursive "./$PATH_ICON_BASE" "$PKG_DATA_PATH"
+        rm --recursive "./$PATH_ICON_BASE"
+        rmdir --ignore-fail-on-non-empty --parents "./${PATH_ICON_BASE%/*}"
+)
+
+rm --recursive "$PLAYIT_WORKDIR/gamedata"
+
+# Write launchers
+
+write_launcher 'APP_MAIN'
+
+# Build package
+
+write_metadata
+build_pkg
+
+# Clean up
+
+rm --recursive "$PLAYIT_WORKDIR"
+
+# Print instructions
+
+print_instructions
+
+exit 0
