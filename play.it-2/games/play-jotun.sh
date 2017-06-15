0d4024a3739532aecc35cd928ed1b579d7d38fb2
Mopi <mopi@dotslashplay.it>
2017-06-15 20:41:56 +0200

    2.0 - Jotun - Fixed $ARCHIVE_GOG_VERSION

--- play.it-2/games/play-jotun.sh
+++ play.it-2/games/play-jotun.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170615.1
+script_version=20170615.2
 
 # Set game-specific variables
 
@@ -46,7 +46,7 @@ ARCHIVES_LIST='ARCHIVE_GOG'
 ARCHIVE_GOG='gog_jotun_2.1.0.2.sh'
 ARCHIVE_GOG_MD5='3f6976f56a4da1d6f274407b1342de7f'
 ARCHIVE_GOG_SIZE='4200000'
-ARCHIVE_GOG_VERSION='valhalla.edition-gog1'
+ARCHIVE_GOG_VERSION='1.0-gog2.1.0.2'
 
 ARCHIVE_DOC_PATH='data/noarch/docs'
 ARCHIVE_DOC_FILES='./*'
