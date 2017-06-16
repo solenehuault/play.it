1eb3a99943f7d00f7ef000d9afb6120ef12b6dc0
vv221 <vv221@dotslashplay.it>
2017-06-16 12:51:21 +0200

    2.0 - Improve Debian dependencies list for WINE games (again)

--- play.it-2/games/play-heroes-of-might-and-magic-4.sh
+++ play.it-2/games/play-heroes-of-might-and-magic-4.sh
@@ -34,7 +34,7 @@ set -o errexit
 # send your bug reports to vv221@dotslashplay.it
 ###
 
-script_version=20170614.1
+script_version=20170616.1
 
 # Set game-specific variables
 
@@ -88,7 +88,7 @@ PKG_DATA_ID="${GAME_ID}-data"
 PKG_DATA_DESCRIPTION='data'
 
 PKG_BIN_ARCH='32'
-PKG_BIN_DEPS_DEB="$PKG_DATA_ID, winetricks, wine:amd64 | wine, wine32 | wine-bin | wine-i386 | wine-staging-i386"
+PKG_BIN_DEPS_DEB="$PKG_DATA_ID, winetricks, wine:amd64 | wine, wine32-development | wine32 | wine-bin | wine-i386 | wine-staging-i386"
 PKG_BIN_DEPS_ARCH="$PKG_DATA_ID winetricks wine"
 
 # Load common functions
